00a5a0f04e3967883028402e9fe2fa2de20ae286
Coda Hale <coda.hale@gmail.com>
2011-03-30 12:35:25 -0700

    Only use the filename for the JAR, not the full path.

--- src/main/scala/com/yammer/dropwizard/util/JarAware.scala
+++ src/main/scala/com/yammer/dropwizard/util/JarAware.scala
@@ -1,8 +1,11 @@
 package com.yammer.dropwizard.util
 
+import java.io.File
+
 trait JarAware {
   def jarSyntax = {
-    val thisJar = this.getClass.getProtectionDomain.getCodeSource.getLocation.getFile
+    val location = this.getClass.getProtectionDomain.getCodeSource.getLocation
+    val thisJar = new File(location.getFile).getName
     "java -jar %s".format(
       if (thisJar.endsWith(".jar")) {
         thisJar
