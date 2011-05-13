440e0aeeea1d933f1970def6d1b92c10ba97564e
Coda Hale <coda.hale@gmail.com>
2011-05-12 23:43:10 -0700

    Switch to AsyncRequestLog.

--- src/main/scala/com/yammer/dropwizard/config/RequestLogHandlerFactory.scala
+++ src/main/scala/com/yammer/dropwizard/config/RequestLogHandlerFactory.scala
@@ -1,22 +1,16 @@
 package com.yammer.dropwizard.config
 
 import com.codahale.fig.Configuration
-import org.eclipse.jetty.server.NCSARequestLog
 import org.eclipse.jetty.server.handler.RequestLogHandler
+import com.yammer.dropwizard.jetty.AsyncRequestLog
 
 object RequestLogHandlerFactory {
   def buildHandler(implicit config: Configuration) = {
-    val log = new NCSARequestLog
-    log.setIgnorePaths(config("request_log.ignore_paths").asList[String].toArray)
-    config("request_log.append").asOption[Boolean].foreach(log.setAppend)
-    config("request_log.filename").asOption[String].foreach(log.setFilename)
-    config("request_log.extended").asOption[Boolean].foreach(log.setExtended)
-    config("request_log.include_cookies").asOption[Boolean].foreach(log.setLogCookies)
-    config("request_log.include_latency").asOption[Boolean].foreach(log.setLogLatency)
-    config("request_log.include_server").asOption[Boolean].foreach(log.setLogServer)
-    config("request_log.timezone").asOption[String].foreach(log.setLogTimeZone)
-    config("request_log.retain_days").asOption[Int].foreach(log.setRetainDays)
-
+    val log = new AsyncRequestLog(
+      config("request_log.filename").asOption[String],
+      config("request_log.retain_days").asOption[Int]
+    )
+    
     val handler = new RequestLogHandler()
     handler.setRequestLog(log)
     handler
---
+++ src/test/scala/com/yammer/dropwizard/experiments/LogThroughputRunner.scala
@@ -0,0 +1,57 @@
+package com.yammer.dropwizard.experiments
+
+import java.util.concurrent.{TimeUnit, Executors}
+import com.yammer.metrics.Instrumented
+import com.yammer.metrics.reporting.ConsoleReporter
+import org.eclipse.jetty.server._
+import org.mockito.Mockito._
+import org.eclipse.jetty.http.{HttpFields, HttpURI}
+import com.yammer.dropwizard.jetty.AsyncRequestLog
+
+object LogThroughputRunner extends Instrumented {
+  val timer = metrics.timer("log", durationUnit = TimeUnit.MICROSECONDS)
+
+  class LogWriter(log: RequestLog) extends Runnable {
+    val fields = new HttpFields
+
+    val conn = mock(classOf[HttpConnection])
+    when(conn.getRequestFields).thenReturn(fields)
+
+    val request = new Request(conn)
+    request.setRemoteAddr("127.0.0.1")
+    request.setUri(new HttpURI("/one/two/three"))
+    request.setMethod("GET")
+    request.setProtocol("HTTP/1.1")
+
+    val response = new Response(conn)
+    response.setStatus(200)
+
+    def run() {
+      try {
+        for (i <- 1 to 10000) {
+          timer.time { log.log(request, response) }
+        }
+      } catch {
+        case e => e.printStackTrace()
+      }
+    }
+  }
+
+  def newLog() = {
+//    new NCSARequestLog("./logs/ncsa_request_log_yyyy_mm_dd.log")
+    new AsyncRequestLog(Some("./logs/async_request_log_yyyy_mm_dd.log"), Some(1))
+  }
+
+  def main(args: Array[String]) {
+    ConsoleReporter.enable(1, TimeUnit.SECONDS)
+    val log = newLog()
+    log.start()
+    val pool = Executors.newFixedThreadPool(1000)
+    for (i <- 1 to 1000) {
+      pool.execute(new LogWriter(log))
+    }
+    pool.shutdown()
+    pool.awaitTermination(10, TimeUnit.MINUTES)
+    log.stop()
+  }
+}
