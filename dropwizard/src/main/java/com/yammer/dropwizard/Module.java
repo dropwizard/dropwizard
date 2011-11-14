5495d4a7eea80121e788d755ec605d16f8ec5b07
Coda Hale <coda.hale@gmail.com>
2011-11-14 11:12:10 -0800

    Added docs for Module.

--- dropwizard/src/main/java/com/yammer/dropwizard/Module.java
+++ dropwizard/src/main/java/com/yammer/dropwizard/Module.java
@@ -2,6 +2,14 @@ package com.yammer.dropwizard;
 
 import com.yammer.dropwizard.config.Environment;
 
+/**
+ * A reusable module, used to define blocks of service behavior.
+ */
 public interface Module {
+    /**
+     * Initializes the environment.
+     *
+     * @param environment    the service's {@link Environment}
+     */
     public void initialize(Environment environment);
 }
