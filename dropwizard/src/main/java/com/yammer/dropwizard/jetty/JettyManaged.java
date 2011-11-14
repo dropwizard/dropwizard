52ea7ef1a4797c8d8950909578967bb47d2574e4
Coda Hale <coda.hale@gmail.com>
2011-11-14 10:04:03 -0800

    Tests and docs for JettyManaged.

--- dropwizard/src/main/java/com/yammer/dropwizard/jetty/JettyManaged.java
+++ dropwizard/src/main/java/com/yammer/dropwizard/jetty/JettyManaged.java
@@ -3,12 +3,18 @@ package com.yammer.dropwizard.jetty;
 import com.yammer.dropwizard.lifecycle.Managed;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
 
-// TODO: 10/12/11 <coda> -- write tests for JettyManaged
-// TODO: 10/12/11 <coda> -- write docs for JettyManaged
-
+/**
+ * A wrapper for {@link Managed} instances which ties them to a Jetty
+ * {@link org.eclipse.jetty.util.component.LifeCycle}.
+ */
 public class JettyManaged extends AbstractLifeCycle implements Managed {
     private final Managed managed;
 
+    /**
+     * Creates a new {@link JettyManaged} wrapping {@code managed}.
+     *
+     * @param managed a {@link Managed} instance to be wrapped
+     */
     public JettyManaged(Managed managed) {
         this.managed = managed;
     }
