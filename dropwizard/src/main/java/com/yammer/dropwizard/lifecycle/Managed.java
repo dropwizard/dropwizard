4449587cad6ad091c88a0511f5b6291fda47b967
Coda Hale <coda.hale@gmail.com>
2011-11-14 11:12:20 -0800

    Added docs for Managed.

--- dropwizard/src/main/java/com/yammer/dropwizard/lifecycle/Managed.java
+++ dropwizard/src/main/java/com/yammer/dropwizard/lifecycle/Managed.java
@@ -1,9 +1,21 @@
 package com.yammer.dropwizard.lifecycle;
 
-// TODO: 10/12/11 <coda> -- write docs for Managed
-
+/**
+ * An interface for objects which need to be started and stopped as the service is started or
+ * stopped.
+ */
 public interface Managed {
+    /**
+     * Starts the object. Called <i>before</i> the service becomes available.
+     *
+     * @throws Exception if something goes wrong; this will halt the service startup.
+     */
     public void start() throws Exception;
 
+    /**
+     * Stops the object. Called <i>after</i> the service is no longer accepting requests.
+     *
+     * @throws Exception if something goes wrong.
+     */
     public void stop() throws Exception;
 }
