9eb7f551c149d7ec361cf8a02cb35ddc4b67b5ab
Coda Hale <coda.hale@gmail.com>
2011-11-15 18:12:09 -0800

    Extract HTTP error messages into a resource bundle.

--- dropwizard/src/main/java/com/yammer/dropwizard/jetty/QuietErrorHandler.java
+++ dropwizard/src/main/java/com/yammer/dropwizard/jetty/QuietErrorHandler.java
@@ -1,5 +1,6 @@
 package com.yammer.dropwizard.jetty;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableSet;
 import org.eclipse.jetty.http.HttpGenerator;
 import org.eclipse.jetty.http.HttpHeaders;
@@ -9,18 +10,23 @@ import org.eclipse.jetty.server.HttpConnection;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.handler.ErrorHandler;
-import org.eclipse.jetty.util.ByteArrayISO8859Writer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import static javax.servlet.http.HttpServletResponse.*;
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 /**
  * An {@link ErrorHandler} subclass which returns concise, {@code text/plain} error messages.
  */
 public class QuietErrorHandler extends ErrorHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuietErrorHandler.class);
+
     /*
      * Sadly, this class is basically untestable.
      */
@@ -44,87 +50,35 @@ public class QuietErrorHandler extends ErrorHandler {
             return;
         }
 
-        response.setContentType(MimeTypes.TEXT_HTML_8859_1);
+        response.setContentType(MimeTypes.TEXT_PLAIN_UTF_8);
         if (getCacheControl() != null) {
             response.setHeader(HttpHeaders.CACHE_CONTROL, getCacheControl());
         }
-        final ByteArrayISO8859Writer writer = new ByteArrayISO8859Writer(4096);
+
+        final StringBuilder builder = new StringBuilder(4096);
+        builder.append(errorMessage(request, jettyResponse.getStatus())).append('\n').append('\n');
+        final byte[] bytes = builder.toString().getBytes(Charsets.UTF_8);
+        response.setContentLength(bytes.length);
+        final ServletOutputStream output = response.getOutputStream();
         try {
-            writer.append(errorMessage(request, jettyResponse.getStatus())).append("\n\n");
-            writer.flush();
-            response.setContentLength(writer.size());
-            writer.writeTo(response.getOutputStream());
+            output.write(bytes);
         } finally {
-            writer.close();
-            writer.destroy();
+            output.close();
         }
     }
 
     private static String errorMessage(HttpServletRequest request, int status) {
-        switch (status) {
-            case SC_BAD_REQUEST:
-                return "Your HTTP client sent a request that this server could " +
-                        "not understand.";
-            case SC_CONFLICT:
-                return "The request could not be completed due to a conflict" +
-                        " with the return current state of the resource.";
-            case SC_EXPECTATION_FAILED:
-                return "The server could not meet the expectation given in the " +
-                        "Expect return request header.";
-            case SC_FORBIDDEN:
-                return "You don't have permission to access the requested" +
-                        " resource.";
-            case SC_GONE:
-                return "The requested resource used to exist but no longer" +
-                        " does.";
-            case SC_INTERNAL_SERVER_ERROR:
-                return "The server encountered an internal error and was" +
-                        " unable to complete your request.";
-            case SC_LENGTH_REQUIRED:
-                return "A request with the " + request.getMethod() + " method " +
-                        "requires a valid Content-Length header.";
-            case SC_METHOD_NOT_ALLOWED:
-                return "The " + request.getMethod() + " method is not allowed" +
-                        "for the requested resource";
-            case SC_NOT_ACCEPTABLE:
-                return "The resource identified by the request is only capable" +
-                        " of generating response entities which have content" +
-                        " characteristics not acceptable according to the" +
-                        " accept headers sent in the request.";
-            case SC_NOT_FOUND:
-                return "The requested resource could not be found on this" +
-                        " server.";
-            case SC_OK:
-                return "";
-            case SC_PRECONDITION_FAILED:
-                return "The precondition on the request for the resource failed" +
-                        " positive evaluation.";
-            case SC_REQUEST_ENTITY_TOO_LARGE:
-                return "The " + request.getMethod() + " method does not allow" +
-                        "the data transmitted, or the data volume exceeds the" +
-                        "capacity limit.";
-            case SC_REQUEST_TIMEOUT:
-                return "The server closed the network connection because your HTTP client" +
-                        " didn't finish the request within the specified time.";
-            case SC_REQUEST_URI_TOO_LONG:
-                return "The length of the requested URL exceeds the capacity limit for this" +
-                        " server. The request cannot be processed.";
-            case SC_REQUESTED_RANGE_NOT_SATISFIABLE:
-                return "The server cannot serve the requested byte range.";
-            case SC_SERVICE_UNAVAILABLE:
-                return "The server is temporarily unable to service your request due to" +
-                        " maintenance downtime or capacity problems. Please try again later.";
-            case SC_UNAUTHORIZED:
-                return "This server could not verify that you are authorized to access" +
-                        " this resource.\n" +
-                        "You either supplied the wrong credentials (e.g., bad password)," +
-                        " or your HTTP client doesn't understand how to supply the" +
-                        " required credentials.";
-            case SC_UNSUPPORTED_MEDIA_TYPE:
-                return "The server does not support the media type transmitted in the request.";
-            default:
-                return "Your request could not be processed: " +
-                        HttpGenerator.getReasonBuffer(status);
+        try {
+            final ResourceBundle bundle = ResourceBundle.getBundle("HttpErrorMessages",
+                                                                   request.getLocale());
+            final String message = bundle.getString(Integer.toString(status));
+            if (message != null) {
+                final MessageFormat format = new MessageFormat(message, request.getLocale());
+                return format.format(new Object[]{request.getMethod()});
+            }
+        } catch (MissingResourceException e) {
+            LOGGER.error("Unable to load HttpErrorMessages.properties", e);
         }
+        return "Your request could not be processed: " + HttpGenerator.getReasonBuffer(status);
     }
 }
