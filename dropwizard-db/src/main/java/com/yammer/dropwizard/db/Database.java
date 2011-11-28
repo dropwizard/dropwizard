8b7221e1588bc130ade4003a5450c9b99dcf052f
Coda Hale <coda.hale@gmail.com>
2011-11-28 15:45:09 -0800

    Add statement logging for Database.
    All SQL will be logged to com.yammer.dropwizard.db.Database at TRACE level.

--- dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
+++ dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
@@ -1,20 +1,26 @@
 package com.yammer.dropwizard.db;
 
 import com.yammer.dropwizard.lifecycle.Managed;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.tomcat.dbcp.pool.ObjectPool;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.logging.Log4JLog;
 import org.skife.jdbi.v2.util.IntegerMapper;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class Database extends DBI implements Managed {
+    private static final Logger LOGGER = Logger.getLogger(Database.class);
+
     private final ObjectPool pool;
 
     public Database(DataSource dataSource, ObjectPool pool) {
         super(dataSource);
         this.pool = pool;
+        setSQLLog(new Log4JLog(LOGGER, Level.TRACE));
     }
 
     @Override
