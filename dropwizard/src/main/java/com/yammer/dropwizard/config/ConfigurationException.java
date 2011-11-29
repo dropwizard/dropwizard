18a6eced0148c5ebb2eca4dde82687096531d5db
Coda Hale <coda.hale@gmail.com>
2011-11-28 22:52:45 -0800

    Added docs for ConfigurationException.

--- dropwizard/src/main/java/com/yammer/dropwizard/config/ConfigurationException.java
+++ dropwizard/src/main/java/com/yammer/dropwizard/config/ConfigurationException.java
@@ -2,9 +2,18 @@ package com.yammer.dropwizard.config;
 
 import java.io.File;
 
+/**
+ * An exception thrown where there is an error parsing a configuration object.
+ */
 public class ConfigurationException extends Exception {
     private static final long serialVersionUID = 5325162099634227047L;
 
+    /**
+     * Creates a new {@link ConfigurationException} for the given file with the given errors.
+     *
+     * @param file      the bad configuration file
+     * @param errors    the errors in the file
+     */
     public ConfigurationException(File file, Iterable<String> errors) {
         super(formatMessage(file, errors));
     }
