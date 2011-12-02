b1c3ba537c60d2d3e4671c52a3209086a19b02ff
Coda Hale <coda.hale@gmail.com>
2011-12-01 17:07:53 -0800

    Dumb down ConfigurationTest.

--- dropwizard/src/test/java/com/yammer/dropwizard/config/tests/ConfigurationTest.java
+++ dropwizard/src/test/java/com/yammer/dropwizard/config/tests/ConfigurationTest.java
@@ -1,11 +1,7 @@
 package com.yammer.dropwizard.config.tests;
 
 import com.yammer.dropwizard.config.Configuration;
-import com.yammer.dropwizard.config.ConfigurationFactory;
-import com.yammer.dropwizard.validation.Validator;
 import org.junit.Test;
-
-import java.io.File;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -23,18 +19,6 @@ public class ConfigurationTest {
     @Test
     public void hasALoggingConfiguration() throws Exception {
         assertThat(configuration.getLoggingConfiguration(),
-                   is(notNullValue()));
-    }
-
-    @Test
-    public void loadsFromTheExampleConfiguration() throws Exception {
-        final ConfigurationFactory<Configuration> factory = new ConfigurationFactory<Configuration>(
-                Configuration.class,
-                new Validator());
-
-        final Configuration config = factory.build(new File("dropwizard/src/test/resources/basic-configuration.yml"));
-
-        assertThat(config.getHttpConfiguration(),
                    is(notNullValue()));
     }
 }
