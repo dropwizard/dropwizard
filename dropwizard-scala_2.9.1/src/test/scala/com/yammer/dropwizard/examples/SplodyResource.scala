45c36b53076c500ba564e15d7b69c3eb5cfa4726
Coda Hale <coda.hale@gmail.com>
2011-12-29 00:15:37 -0800

    Make sure all our Scala resources are timed.

--- dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/SplodyResource.scala
+++ dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/SplodyResource.scala
@@ -3,6 +3,7 @@ package com.yammer.dropwizard.examples
 import javax.ws.rs.core.StreamingOutput
 import javax.ws.rs.{POST, GET, Path}
 import java.io.OutputStream
+import com.yammer.metrics.annotation.Timed
 
 @Path("/splode")
 class SplodyResource {
@@ -12,12 +13,14 @@ class SplodyResource {
    * An error which happens inside a Jersey resource.
    */
   @GET
+  @Timed
   def splode() = dumb.toString
 
   /**
    * An error which happens outside of a Jersey resource.
    */
   @POST
+  @Timed
   def sneakySplode(): StreamingOutput = new StreamingOutput {
     def write(output: OutputStream) {
       throw new RuntimeException("OH SWEET GOD WHAT")
--- dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/UploadResource.scala
+++ dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/UploadResource.scala
@@ -3,11 +3,13 @@ package com.yammer.dropwizard.examples
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.{POST, Consumes, Path}
 import com.codahale.logula.Logging
+import com.yammer.metrics.annotation.Timed
 
 @Path("/upload")
 @Consumes(Array(MediaType.WILDCARD))
 class UploadResource extends Logging {
   @POST
+  @Timed
   def upload(body: String) {
     log.info("New upload: %s", body)
   }
