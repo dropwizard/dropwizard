dropwizard/dropwizard

commit 7f96a9683db110dccabbc89f901371be681f5efd

Fix ServerCommand logging.

dropwizard-core/src/main/java/com/yammer/dropwizard/cli/ServerCommand.java
@@ -48,11 +48,11 @@ public class ServerCommand<T extends Configuration> extends ConfiguredCommand<T>
 
         final Server server = new ServerFactory(configuration.getHttpConfiguration()).buildServer(environment);
 
-        final Log logger = Log.forClass(ServerCommand.class);
-        logger.info("Starting {}", service.getName());
+        final Log log = Log.forClass(ServerCommand.class);
+        log.info("Starting {}", service.getName());
 
         try {
-            logger.info('\n' + Resources.toString(Resources.getResource("banner.txt"), Charsets.UTF_8));
+            log.info("{}\n", Resources.toString(Resources.getResource("banner.txt"), Charsets.UTF_8));
         } catch (IllegalArgumentException ignored) {
             // don't display the banner if there isn't one
         }
@@ -61,7 +61,7 @@ public class ServerCommand<T extends Configuration> extends ConfiguredCommand<T>
             server.start();
             server.join();
         } catch (Exception e) {
-            logger.error("Unable to start server, shutting down", e);
+            log.error(e, "Unable to start server, shutting down");
             server.stop();
         }
     }

commit 9956e5074c5fc1ded2cacaaee1b942a1af33b119

No really, fix ServerCommand logging.
Fixes a dumb regression in 7f96a9683db110dccabbc89f901371be681f5efd.

dropwizard-core/src/main/java/com/yammer/dropwizard/cli/ServerCommand.java
@@ -52,7 +52,7 @@ public class ServerCommand<T extends Configuration> extends ConfiguredCommand<T>
         log.info("Starting {}", service.getName());
 
         try {
-            log.info("{}\n", Resources.toString(Resources.getResource("banner.txt"), Charsets.UTF_8));
+            log.info("\n{}", Resources.toString(Resources.getResource("banner.txt"), Charsets.UTF_8));
         } catch (IllegalArgumentException ignored) {
             // don't display the banner if there isn't one
         }