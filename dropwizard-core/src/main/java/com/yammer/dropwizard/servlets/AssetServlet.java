0035291bd2a67674ac447e288f2a91e45efd55c0
Brian ONeill <bone@alumni.brown.edu>
2012-01-18 16:12:41 -0500

    Ability to serve up a default html file from the root of the uri/resource path. Presently it is hardcoded to serve up "index.htm", we should make that configurable. Also needed to set a default mimetype when it can't find one.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
@@ -1,18 +1,20 @@
 package com.yammer.dropwizard.servlets;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.io.Resources;
-import org.eclipse.jetty.http.MimeTypes;
-import org.eclipse.jetty.util.URIUtil;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.eclipse.jetty.http.MimeTypes;
+import org.eclipse.jetty.io.Buffer;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.io.Resources;
 
 public class AssetServlet extends HttpServlet {
     private static final long serialVersionUID = 6393345594784987908L;
@@ -20,7 +22,8 @@ public class AssetServlet extends HttpServlet {
     private static class AssetLoader extends CacheLoader<String, byte[]> {
         private final String resourcePath;
         private final String uriPath;
-
+        private final static String INDEX_FILENAME = "index.htm"; // TODO: Make this configurable.
+        
         private AssetLoader(String resourcePath, String uriPath) {
             this.resourcePath = resourcePath;
             this.uriPath = uriPath;
@@ -30,13 +33,16 @@ public class AssetServlet extends HttpServlet {
         public byte[] load(String key) throws Exception {
             final String resource = key.substring(uriPath.length());
             String fullResourcePath = this.resourcePath + resource;
-
-                return Resources.toByteArray(Resources.getResource(fullResourcePath.substring(1)));
+            if (key.equals(this.uriPath)) {
+                fullResourcePath = resourcePath + INDEX_FILENAME;
+            }
+            return Resources.toByteArray(Resources.getResource(fullResourcePath.substring(1)));
         }
     }
     
     private final transient LoadingCache<String, byte[]> cache;
     private final transient MimeTypes mimeTypes;
+    private final static String DEFAULT_MIME_TYPE = "text/html";
 
     public AssetServlet(String resourcePath, int maxCacheSize, String uriPath) {
         this.cache = buildCache(resourcePath, maxCacheSize, uriPath);
@@ -54,7 +60,12 @@ public class AssetServlet extends HttpServlet {
                          HttpServletResponse resp) throws ServletException, IOException {
         try {
             final byte[] resource = cache.getUnchecked(req.getRequestURI());
-            resp.setContentType(mimeTypes.getMimeByExtension(req.getRequestURI()).toString());
+            Buffer mimeType = mimeTypes.getMimeByExtension(req.getRequestURI());
+            if (mimeType == null) {
+                resp.setContentType(DEFAULT_MIME_TYPE);
+            } else {
+                resp.setContentType(mimeType.toString());
+            }
             final ServletOutputStream output = resp.getOutputStream();
             try {
                 output.write(resource);
