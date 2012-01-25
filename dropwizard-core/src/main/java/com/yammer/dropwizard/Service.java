58302dc1e46ffb334df1673f9728588aebc2446e
Coda Hale <coda.hale@gmail.com>
2012-01-25 09:13:55 +0200

    Throw an exception if a Scala class tries to extend Service.
    Scala projects need to extend ScalaService, which adds a different set of Jersey providers.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/Service.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/Service.java
@@ -1,7 +1,7 @@
 package com.yammer.dropwizard;
 
-import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.bundles.JavaBundle;
+import com.yammer.dropwizard.config.Configuration;
 
 /**
  * The default Java service class. Extend this to write your own service.
@@ -13,10 +13,24 @@ public abstract class Service<T extends Configuration> extends AbstractService<T
     protected Service(String name) {
         super(name);
         addBundle(new JavaBundle());
+        checkForScalaExtensions();
     }
 
     @Override
     protected final void subclassServiceInsteadOfThis() {
 
+    }
+
+    private void checkForScalaExtensions() {
+        try {
+            final Class<?> scalaObject = Class.forName("scala.ScalaObject");
+            final Class<?> klass = getClass();
+            if (scalaObject.isAssignableFrom(klass)) {
+                throw new IllegalStateException(klass.getCanonicalName() + " is a Scala class. " +
+                                                        "It should extend ScalaService, not Service.");
+            }
+        } catch (ClassNotFoundException ignored) {
+            // definitely not a Scala project
+        }
     }
 }
