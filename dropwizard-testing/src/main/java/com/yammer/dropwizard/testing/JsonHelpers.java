a6e3cf98a60a9dcf8de13232df29a6b059d46442
Coda Hale <coda.hale@gmail.com>
2012-02-03 20:15:44 -0800

    Fix JsonHelpers#asJson throws clause.

--- dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/JsonHelpers.java
+++ dropwizard-testing/src/main/java/com/yammer/dropwizard/testing/JsonHelpers.java
@@ -32,9 +32,9 @@ public class JsonHelpers {
      *
      * @param object    an object
      * @return {@code object} as a JSON string
-     * @throws IOException if there is an error writing {@code object} as JSON
+     * @throws IllegalArgumentException if there is an error encoding {@code object}
      */
-    public static String asJson(Object object) throws IOException {
+    public static String asJson(Object object) throws IllegalArgumentException {
         return JSON.writeValueAsString(object);
     }
 
