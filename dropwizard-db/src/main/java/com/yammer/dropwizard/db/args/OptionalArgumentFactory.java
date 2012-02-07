b298771cd99410f078c47e4082ac3233fa02ecd3
Coda Hale <coda.hale@gmail.com>
2012-02-07 15:40:47 -0800

    Upgrade to JDBI 2.31.1.

--- dropwizard-db/src/main/java/com/yammer/dropwizard/db/args/OptionalArgumentFactory.java
+++ dropwizard-db/src/main/java/com/yammer/dropwizard/db/args/OptionalArgumentFactory.java
@@ -7,16 +7,12 @@ import org.skife.jdbi.v2.tweak.ArgumentFactory;
 
 public class OptionalArgumentFactory implements ArgumentFactory<Optional<Object>> {
     @Override
-    public boolean accepts(Class<? super Optional<Object>> expectedType,
-                           Object value,
-                           StatementContext ctx) {
+    public boolean accepts(Class<?> expectedType, Object value, StatementContext ctx) {
         return value instanceof Optional;
     }
 
     @Override
-    public Argument build(Class<? super Optional<Object>> expectedType,
-                          Optional<Object> value,
-                          StatementContext ctx) {
+    public Argument build(Class<?> expectedType, Optional<Object> value, StatementContext ctx) {
         return new OptionalArgument(value);
     }
 }
