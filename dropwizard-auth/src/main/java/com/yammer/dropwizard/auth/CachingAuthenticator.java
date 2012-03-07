224b3e18ef89104dbedcbd23b6a3a567be0ddb53
Coda Hale <coda.hale@gmail.com>
2012-03-06 23:36:53 -0800

    Use static factory methods for CachingAuthenticator.

--- dropwizard-auth/src/main/java/com/yammer/dropwizard/auth/CachingAuthenticator.java
+++ dropwizard-auth/src/main/java/com/yammer/dropwizard/auth/CachingAuthenticator.java
@@ -8,10 +8,15 @@ import com.google.common.cache.LoadingCache;
 import java.util.concurrent.ExecutionException;
 
 public class CachingAuthenticator<C, P> implements Authenticator<C, P> {
+    public static <C, P> Authenticator<C, P> wrap(Authenticator<C, P> authenticator,
+                                                  CacheBuilder<Object, Object> builder) {
+        return new CachingAuthenticator<C, P>(authenticator, builder);
+    }
+    
     private final Authenticator<C, P> underlying;
     private final LoadingCache<C, Optional<P>> cache;
 
-    public CachingAuthenticator(Authenticator<C, P> authenticator,
+    private CachingAuthenticator(Authenticator<C, P> authenticator,
                                 CacheBuilder<Object, Object> builder) {
         this.underlying = authenticator;
         this.cache = builder.build(new CacheLoader<C, Optional<P>> () {
