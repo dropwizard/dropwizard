707e5e90063aba29965ddf3474e1f4d7873eaa36
Coda Hale <coda.hale@gmail.com>
2012-03-20 12:30:45 -0700

    Refactor AssetServlet a bit.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/servlets/AssetServlet.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.hash.Hashing;
 import com.google.common.io.Resources;
+import com.google.common.net.HttpHeaders;
 import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.io.Buffer;
 
@@ -15,20 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Calendar;
 
 public class AssetServlet extends HttpServlet {
     private static final long serialVersionUID = 6393345594784987908L;
 
     private static class AssetLoader extends CacheLoader<String, CachedAsset> {
-        private static final String INDEX_FILENAME = "index.htm"; // TODO: Make this configurable.
-
         private final String resourcePath;
         private final String uriPath;
+        private final String indexFilename;
 
-        private AssetLoader(String resourcePath, String uriPath) {
+        private AssetLoader(String resourcePath, String uriPath, String indexFilename) {
             this.resourcePath = resourcePath;
             this.uriPath = uriPath;
+            this.indexFilename = indexFilename;
         }
 
         @Override
@@ -36,49 +36,40 @@ public class AssetServlet extends HttpServlet {
             final String resource = key.substring(uriPath.length());
             String fullResourcePath = this.resourcePath + resource;
             if (key.equals(this.uriPath)) {
-                fullResourcePath = resourcePath + INDEX_FILENAME;
+                fullResourcePath = resourcePath + indexFilename;
             }
-
-            URL resourceURL = Resources.getResource(fullResourcePath.substring(1));
-
-            CachedAsset cachedAsset = new CachedAsset(Resources.toByteArray(resourceURL));
-
-            return cachedAsset;
+            final URL resourceURL = Resources.getResource(fullResourcePath.substring(1));
+            return new CachedAsset(Resources.toByteArray(resourceURL));
         }
     }
 
     private static class CachedAsset {
-
         private final byte[] resource;
-
-        private final String etag;
-
+        private final String eTag;
         private final long lastModifiedTime;
 
-        public CachedAsset(byte[] resource) {
-            this.resource = resource.clone();
+        private CachedAsset(byte[] resource) {
+            this.resource = resource;
+            this.eTag = Hashing.murmur3_128().hashBytes(resource).toString();
+            this.lastModifiedTime = roundedTimestamp();
+        }
 
-            this.etag = Hashing.murmur3_128().hashBytes(resource).toString();
-
-            // lazy and non-aggressive lastModified impl
+        private long roundedTimestamp() {
             // zero out the millis since the date we get back from If-Modified-Since will not have them
-            Calendar now = Calendar.getInstance();
-            now.set(Calendar.MILLISECOND, 0);
-            this.lastModifiedTime = now.getTime().getTime();
+            return (System.currentTimeMillis() / 1000) * 1000;
         }
 
         public byte[] getResource() {
             return resource.clone();
         }
 
-        public String getEtag() {
-            return etag;
+        public String getETag() {
+            return eTag;
         }
 
         public long getLastModifiedTime() {
             return lastModifiedTime;
         }
-
     }
 
     private static final String DEFAULT_MIME_TYPE = "text/html";
@@ -87,38 +78,27 @@ public class AssetServlet extends HttpServlet {
     private final transient MimeTypes mimeTypes;
 
     public AssetServlet(String resourcePath, int maxCacheSize, String uriPath) {
-        this.cache = buildCache(resourcePath, maxCacheSize, uriPath);
+        // TODO: 3/20/12 <coda> -- make the default filename here configurable
+        this.cache = CacheBuilder.newBuilder()
+                                 .maximumSize(maxCacheSize)
+                                 .build(new AssetLoader(resourcePath, uriPath, "index.htm"));
         this.mimeTypes = new MimeTypes();
     }
-
-    private static LoadingCache<String, CachedAsset> buildCache(String resourcePath, int maxCacheSize, String uriPath) {
-        return CacheBuilder.newBuilder()
-                           .maximumSize(maxCacheSize)
-                           .build(new AssetLoader(resourcePath, uriPath));
-    }
-
 
     @Override
     protected void doGet(HttpServletRequest req,
                          HttpServletResponse resp) throws ServletException, IOException {
         try {
 
-            CachedAsset cachedAsset = cache.getUnchecked(req.getRequestURI());
+            final CachedAsset cachedAsset = cache.getUnchecked(req.getRequestURI());
 
-            if ((req.getHeader("If-None-Match") != null) &&
-                    (req.getHeader("If-None-Match").equals(cachedAsset.getEtag()))) {
+            if (isCachedClientSide(req, cachedAsset)) {
                 resp.sendError(HttpServletResponse.SC_NOT_MODIFIED);
                 return;
             }
 
-            if ((req.getHeader("If-Modified-Since") != null) &&
-                    (cachedAsset.getLastModifiedTime() <= req.getDateHeader("If-Modified-Since"))) {
-                resp.sendError(HttpServletResponse.SC_NOT_MODIFIED);
-                return;
-            }
-
-            resp.setDateHeader("Last-Modified", cachedAsset.getLastModifiedTime());
-            resp.setHeader("ETag", cachedAsset.getEtag());
+            resp.setDateHeader(HttpHeaders.LAST_MODIFIED, cachedAsset.getLastModifiedTime());
+            resp.setHeader(HttpHeaders.ETAG, cachedAsset.getETag());
 
             final Buffer mimeType = mimeTypes.getMimeByExtension(req.getRequestURI());
             if (mimeType == null) {
@@ -126,6 +106,7 @@ public class AssetServlet extends HttpServlet {
             } else {
                 resp.setContentType(mimeType.toString());
             }
+
             final ServletOutputStream output = resp.getOutputStream();
             try {
                 output.write(cachedAsset.getResource());
@@ -135,5 +116,10 @@ public class AssetServlet extends HttpServlet {
         } catch (RuntimeException ignored) {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
+    }
+
+    private boolean isCachedClientSide(HttpServletRequest req, CachedAsset cachedAsset) {
+        return cachedAsset.getETag().equals(req.getHeader(HttpHeaders.IF_NONE_MATCH)) ||
+                (req.getDateHeader(HttpHeaders.IF_MODIFIED_SINCE) >= cachedAsset.getLastModifiedTime());
     }
 }
