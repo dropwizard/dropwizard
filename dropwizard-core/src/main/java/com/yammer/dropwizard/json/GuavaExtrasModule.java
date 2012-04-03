d0c9c8f0c12a62d7852f08fad1288a3fbb94d254
Coda Hale <coda.hale@gmail.com>
2012-04-02 17:10:37 -0700

    Add support for deserializing HostAndPort.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/json/GuavaExtrasModule.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/json/GuavaExtrasModule.java
@@ -1,6 +1,7 @@
 package com.yammer.dropwizard.json;
 
 import com.google.common.cache.CacheBuilderSpec;
+import com.google.common.net.HostAndPort;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.Version;
 import org.codehaus.jackson.map.*;
@@ -10,6 +11,15 @@ import org.codehaus.jackson.type.JavaType;
 import java.io.IOException;
 
 public class GuavaExtrasModule extends Module {
+    @JsonCachable
+    private static class HostAndPortDeserializer extends JsonDeserializer<HostAndPort> {
+        @Override
+        public HostAndPort deserialize(JsonParser jp,
+                                       DeserializationContext ctxt) throws IOException {
+            return HostAndPort.fromString(jp.getText());
+        }
+    }
+
     @JsonCachable
     private static class CacheBuilderSpecDeserializer extends JsonDeserializer<CacheBuilderSpec> {
         @Override
@@ -33,6 +43,11 @@ public class GuavaExtrasModule extends Module {
             if (CacheBuilderSpec.class.isAssignableFrom(type.getRawClass())) {
                 return new CacheBuilderSpecDeserializer();
             }
+
+            if (HostAndPort.class.isAssignableFrom(type.getRawClass())) {
+                return new HostAndPortDeserializer();
+            }
+
             return super.findBeanDeserializer(type, config, provider, beanDesc, property);
         }
     }
