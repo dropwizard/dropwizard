a53838ec1902c62d35f034d85261b06a3ae0734a
Adam Jordens <adam@jordens.org>
2012-07-08 20:48:51 -0700

    Addressed java.lang.Exception: No runnable methods in RequestLogConfigurationTest.
    - Test class had a @Before but no @Test

--- dropwizard-core/src/test/java/com/yammer/dropwizard/config/tests/RequestLogConfigurationTest.java
+++ dropwizard-core/src/test/java/com/yammer/dropwizard/config/tests/RequestLogConfigurationTest.java
@@ -5,17 +5,32 @@ import com.yammer.dropwizard.config.ConfigurationFactory;
 import com.yammer.dropwizard.config.RequestLogConfiguration;
 import com.yammer.dropwizard.validation.Validator;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.File;
+import java.util.TimeZone;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class RequestLogConfigurationTest {
     private RequestLogConfiguration requestLog;
 
     @Before
     public void setUp() throws Exception {
-        this.requestLog = ConfigurationFactory.forClass(RequestLogConfiguration.class,
-                                                        new Validator()).build(new File(Resources.getResource("yaml/requestLog.yml").getFile()));
+        this.requestLog = ConfigurationFactory
+                .forClass(RequestLogConfiguration.class, new Validator())
+                .build(new File(Resources.getResource("yaml/requestLog.yml").getFile()));
     }
 
+    @Test
+    public void defaultTimeZoneIsUTC() {
+        assertThat(requestLog.getTimeZone(), is(TimeZone.getTimeZone("UTC")));
+    }
+
+    @Test
+    public void fileConfigurationCanBeEnabled() throws Exception {
+        assertThat(requestLog.getFileConfiguration().isEnabled(), is(true));
+    }
 
 }
