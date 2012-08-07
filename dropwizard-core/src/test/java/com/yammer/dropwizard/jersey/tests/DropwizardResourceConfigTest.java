99737b28518236957016eb3cd7c26640738c7cfd
Coda Hale <coda.hale@gmail.com>
2012-08-06 17:03:40 -0700

    Fix DropwizardResourceConfigTest.

--- dropwizard-core/src/test/java/com/yammer/dropwizard/jersey/tests/DropwizardResourceConfigTest.java
+++ dropwizard-core/src/test/java/com/yammer/dropwizard/jersey/tests/DropwizardResourceConfigTest.java
@@ -1,17 +1,17 @@
 package com.yammer.dropwizard.jersey.tests;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Iterator;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
-import org.junit.Test;
-
+import com.google.common.collect.ImmutableSet;
 import com.sun.jersey.core.spi.scanning.PackageNamesScanner;
 import com.yammer.dropwizard.jersey.DropwizardResourceConfig;
 import com.yammer.dropwizard.jersey.tests.dummy.DummyResource;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class DropwizardResourceConfigTest {
 
@@ -19,27 +19,22 @@ public class DropwizardResourceConfigTest {
     public void findsResourceClassInPackage() {
         final DropwizardResourceConfig rc = new DropwizardResourceConfig(true);
         rc.init(new PackageNamesScanner(new String[] { DummyResource.class.getPackage().getName() }));
-        assertEquals("Resource classes found", 1, rc.getRootResourceClasses().size());
-        assertEquals(
-                "Unexpected resource class found",
-                DummyResource.class,
-                rc.getRootResourceClasses().iterator().next());
+
+        assertThat(rc.getRootResourceClasses(),
+                   is((Set<Class<?>>) ImmutableSet.<Class<?>>of(
+                           DummyResource.class
+                   )));
     }
 
     @Test
     public void findsResourceClassesInPackageAndSubpackage() {
         final DropwizardResourceConfig rc = new DropwizardResourceConfig(true);
         rc.init(new PackageNamesScanner(new String[] { getClass().getPackage().getName() }));
-        assertEquals("Resource classes found", 2, rc.getRootResourceClasses().size());
-        final Iterator<Class<?>> resources = rc.getRootResourceClasses().iterator();
-        assertEquals(
-                "Unexpected resource class found",
-                TestResource.class,
-                resources.next());
-        assertEquals(
-                "Unexpected resource class found",
-                DummyResource.class,
-                resources.next());
+
+        assertThat(rc.getRootResourceClasses(),
+                   is((Set<Class<?>>) ImmutableSet.<Class<?>>of(
+                           DummyResource.class, TestResource.class
+                   )));
     }
 
     @Path("/dummy")
