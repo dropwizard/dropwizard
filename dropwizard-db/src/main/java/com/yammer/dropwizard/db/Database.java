5314faa8cacd3051c7e2e25d495ddce94354f9ad
Coda Hale <coda.hale@gmail.com>
2012-08-07 18:16:36 -0700

    Use a less-bonkers naming strategy for JDBI.
    Otherwise you end up with a crapton of metrics that aren't useful because they're only called once.

--- dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
+++ dropwizard-db/src/main/java/com/yammer/dropwizard/db/Database.java
@@ -6,11 +6,16 @@ import com.yammer.dropwizard.db.args.OptionalArgumentFactory;
 import com.yammer.dropwizard.db.logging.LogbackLog;
 import com.yammer.dropwizard.lifecycle.Managed;
 import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.MetricName;
 import com.yammer.metrics.jdbi.InstrumentedTimingCollector;
+import com.yammer.metrics.jdbi.strategies.DelegatingStatementNameStrategy;
+import com.yammer.metrics.jdbi.strategies.NameStrategies;
+import com.yammer.metrics.jdbi.strategies.StatementNameStrategy;
 import org.apache.tomcat.dbcp.pool.ObjectPool;
 import org.skife.jdbi.v2.ColonPrefixNamedParamStatementRewriter;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.StatementContext;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
@@ -18,6 +23,21 @@ import java.sql.SQLException;
 
 public class Database extends DBI implements Managed {
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(Database.class);
+    private static final MetricName RAW_SQL = new MetricName(Database.class, "raw-sql");
+    private static class SanerNamingStrategy extends DelegatingStatementNameStrategy {
+        private SanerNamingStrategy() {
+            super(NameStrategies.CHECK_EMPTY,
+                  NameStrategies.CONTEXT_CLASS,
+                  NameStrategies.CONTEXT_NAME,
+                  NameStrategies.SQL_OBJECT,
+                  new StatementNameStrategy() {
+                      @Override
+                      public MetricName getStatementName(StatementContext statementContext) {
+                          return RAW_SQL;
+                      }
+                  });
+        }
+    }
 
     private final ObjectPool pool;
     private final String validationQuery;
@@ -27,7 +47,8 @@ public class Database extends DBI implements Managed {
         this.pool = pool;
         this.validationQuery = validationQuery;
         setSQLLog(new LogbackLog(LOGGER, Level.TRACE));
-        setTimingCollector(new InstrumentedTimingCollector(Metrics.defaultRegistry()));
+        setTimingCollector(new InstrumentedTimingCollector(Metrics.defaultRegistry(),
+                                                           new SanerNamingStrategy()));
         setStatementRewriter(new NamePrependingStatementRewriter(new ColonPrefixNamedParamStatementRewriter()));
         registerArgumentFactory(new OptionalArgumentFactory());
         registerContainerFactory(new ImmutableListContainerFactory());
