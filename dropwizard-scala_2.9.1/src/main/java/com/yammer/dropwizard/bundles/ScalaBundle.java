3e3def159dde291df19f915d956f79cadf389b7e
Nick Telford <nick.telford@gmail.com>
2012-10-09 11:42:25 +0100

    Cleanup Scala Service initialization
    Now that Bundles can hook in to the Bootstrap process, we can move all
    the Scala customizations from `ScalaService` to `ScalaBundle`.
    
    `ScalaService` remains solely to provide the default `main` method for
    Scala Services.

--- dropwizard-scala_2.9.1/src/main/java/com/yammer/dropwizard/bundles/ScalaBundle.java
+++ dropwizard-scala_2.9.1/src/main/java/com/yammer/dropwizard/bundles/ScalaBundle.java
@@ -1,10 +1,19 @@
 package com.yammer.dropwizard.bundles;
 
+import com.fasterxml.jackson.module.scala.DefaultScalaModule;
 import com.yammer.dropwizard.Bundle;
+import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.scala.inject.ScalaCollectionsQueryParamInjectableProvider;
 
 public class ScalaBundle extends Bundle {
+
+    @Override
+    public void initialize(final Bootstrap<?> bootstrap) {
+        bootstrap.addBundle(new ScalaBundle());
+        bootstrap.getObjectMapperFactory().registerModule(new DefaultScalaModule());
+    }
+
     @Override
     public void run(Environment environment) {
         environment.addProvider(new ScalaCollectionsQueryParamInjectableProvider());
