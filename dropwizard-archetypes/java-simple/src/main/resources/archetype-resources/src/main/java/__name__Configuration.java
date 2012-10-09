992d717d52b2f811272d79c8a6a5c35fa351f070
nicktelford <nick.telford@gmail.com>
2012-10-09 21:43:56 +0100

    Upadate Archetypes for Jackson 2.x
    The package name has changed from `org.codehaus.jackson` to
    `com.fasterxml.jackson`.

--- dropwizard-archetypes/java-simple/src/main/resources/archetype-resources/src/main/java/__name__Configuration.java
+++ dropwizard-archetypes/java-simple/src/main/resources/archetype-resources/src/main/java/__name__Configuration.java
@@ -1,12 +1,10 @@
 package ${package};
 
 import com.yammer.dropwizard.config.Configuration;
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
 public class ${name}Configuration extends Configuration {
-  
-  // TODO: implement service configuration
-
+    // TODO: implement service configuration
 }
--- dropwizard-archetypes/scala-simple/src/main/resources/archetype-resources/src/main/scala/__name__Configuration.scala
+++ dropwizard-archetypes/scala-simple/src/main/resources/archetype-resources/src/main/scala/__name__Configuration.scala
@@ -6,7 +6,5 @@ import org.hibernate.validator.constraints._
 import javax.validation.constraints._
 
 class ${name}Configuration extends Configuration {
-  
   // TODO: implement service configuration
-
 }
