b4434fd654482382baeb52312ddf193261c1eeb0
Coda Hale <coda.hale@gmail.com>
2012-10-10 14:18:06 -0700

    Clear up style for example resources.

--- dropwizard-example/src/main/java/com/example/helloworld/db/PersonDAO.java
+++ dropwizard-example/src/main/java/com/example/helloworld/db/PersonDAO.java
@@ -1,6 +1,7 @@
 package com.example.helloworld.db;
 
 import com.example.helloworld.core.Person;
+import com.google.common.base.Optional;
 import com.yammer.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 
@@ -11,12 +12,12 @@ public class PersonDAO extends AbstractDAO<Person> {
         super(factory);
     }
 
-    public Person findById(Long id) {
-        return get(id);
+    public Optional<Person> findById(Long id) {
+        return Optional.fromNullable(get(id));
     }
 
-    public long create(Person person) {
-        return persist(person).getId();
+    public Person create(Person person) {
+        return persist(person);
     }
 
     public List<Person> findAll() {
