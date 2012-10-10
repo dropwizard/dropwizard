a8b9b5d9b6b4b5c2da2108a52b22ebd287d775ad
Coda Hale <coda.hale@gmail.com>
2012-10-09 20:06:26 -0700

    Fiddle.

--- dropwizard-migrations/src/main/java/com/yammer/dropwizard/migrations/MigrationsBundle.java
+++ dropwizard-migrations/src/main/java/com/yammer/dropwizard/migrations/MigrationsBundle.java
@@ -6,12 +6,12 @@ import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.db.ConfigurationStrategy;
 import com.yammer.dropwizard.util.Generics;
 
-public abstract class MigrationsBundle<T extends Configuration> extends Bundle implements
-                                                                               ConfigurationStrategy<T> {
+public abstract class MigrationsBundle<T extends Configuration>
+        extends Bundle implements ConfigurationStrategy<T> {
     @Override
     @SuppressWarnings("unchecked")
     public void initialize(Bootstrap<?> bootstrap) {
-        final Class<T> configClass = (Class<T>) Generics.getTypeParameter(getClass(), Configuration.class);
-        bootstrap.addCommand(new DbCommand<T>(this, configClass));
+        final Class<T> klass = (Class<T>) Generics.getTypeParameter(getClass(), Configuration.class);
+        bootstrap.addCommand(new DbCommand<T>(this, klass));
     }
 }
