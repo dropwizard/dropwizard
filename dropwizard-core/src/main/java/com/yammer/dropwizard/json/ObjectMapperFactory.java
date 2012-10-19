007ea4a7f2ba64fad78f78c1b798ca45455719e4
Coda Hale <coda.hale@gmail.com>
2012-10-19 14:19:48 -0700

    Fix calls to deprecated Jackson method.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/json/ObjectMapperFactory.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/json/ObjectMapperFactory.java
@@ -506,15 +506,15 @@ public class ObjectMapperFactory {
         }
 
         for (Map.Entry<JsonGenerator.Feature, Boolean> entry : generatorFeatures.entrySet()) {
-            mapper.getJsonFactory().configure(entry.getKey(), entry.getValue());
+            mapper.getFactory().configure(entry.getKey(), entry.getValue());
         }
 
         for (Map.Entry<JsonParser.Feature, Boolean> entry : parserFeatures.entrySet()) {
-            mapper.getJsonFactory().configure(entry.getKey(), entry.getValue());
+            mapper.getFactory().configure(entry.getKey(), entry.getValue());
         }
 
         for (Map.Entry<JsonFactory.Feature, Boolean> entry : factoryFeatures.entrySet()) {
-            mapper.getJsonFactory().configure(entry.getKey(), entry.getValue());
+            mapper.getFactory().configure(entry.getKey(), entry.getValue());
         }
 
         if (annotationIntrospector != null) {
--- dropwizard-core/src/test/java/com/yammer/dropwizard/json/tests/ObjectMapperFactoryTest.java
+++ dropwizard-core/src/test/java/com/yammer/dropwizard/json/tests/ObjectMapperFactoryTest.java
@@ -31,7 +31,7 @@ public class ObjectMapperFactoryTest {
         final JsonFactory jsonFactory = new YAMLFactory();
         final ObjectMapper mapper = factory.build(jsonFactory);
 
-        assertThat(mapper.getJsonFactory())
+        assertThat(mapper.getFactory())
                 .isSameAs(jsonFactory);
     }
 
@@ -50,7 +50,7 @@ public class ObjectMapperFactoryTest {
     public void defaultsToAllowingComments() throws Exception {
         final ObjectMapper mapper = factory.build();
 
-        assertThat(mapper.getJsonFactory().isEnabled(JsonParser.Feature.ALLOW_COMMENTS))
+        assertThat(mapper.getFactory().isEnabled(JsonParser.Feature.ALLOW_COMMENTS))
                 .isTrue();
     }
 
