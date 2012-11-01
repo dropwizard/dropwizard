0d746dd1c3bf58aa674f742aae99024eeca76c73
Hal Hildebrand <hal.hildebrand@gmail.com>
2012-10-31 18:05:34 -0700

    Add tests for PortRange constraint

--- dropwizard-core/src/main/java/com/yammer/dropwizard/config/validation/PortRange.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/config/validation/PortRange.java
@@ -1,5 +1,5 @@
 /** 
- * (C) Copyright 2010 Hal Hildebrand, All Rights Reserved
+ * (C) Copyright 2012 Hal Hildebrand, All Rights Reserved
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
