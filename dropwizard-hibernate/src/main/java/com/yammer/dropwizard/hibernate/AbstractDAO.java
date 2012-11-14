6feb069a62c0c909802d8361592f59d167a79cbf
Coda Hale <coda.hale@gmail.com>
2012-11-14 15:05:54 -0800

    Nuke old author javadoc.

--- dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/AbstractDAO.java
+++ dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/AbstractDAO.java
@@ -12,7 +12,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * An abstract base class for Hibernate DAO classes.
  *
  * @param <E> the class which this DAO manages
- * @author coda
  */
 public class AbstractDAO<E> {
     private final SessionFactory provider;
