089c4c0da449d79e22a1d7d20c9b5ab292f795f1
Coda Hale <coda.hale@gmail.com>
2012-11-16 14:43:06 -0800

    Clean up dw-scala.

--- dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/ExampleConfiguration.scala
+++
@@ -1,9 +0,0 @@
-package com.yammer.dropwizard.examples
-
-import com.yammer.dropwizard.config.Configuration
-import com.fasterxml.jackson.annotation.JsonProperty
-
-class ExampleConfiguration extends Configuration {
-  @JsonProperty
-  var saying: String = "Hello, world!"
-}
--- dropwizard-scala_2.9.1/src/test/scala/com/yammer/dropwizard/examples/SayingFactory.scala
+++
@@ -1,5 +0,0 @@
-package com.yammer.dropwizard.examples
-
-object SayingFactory {
-  def buildSaying(implicit config: ExampleConfiguration) = config.saying
-}
