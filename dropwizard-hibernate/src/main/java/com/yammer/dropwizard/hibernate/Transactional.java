d246920f19683a36bb6fc9f335fb0b5d19f035d3
Coda Hale <coda.hale@gmail.com>
2012-11-16 11:23:05 -0800

    Added docs for Transactional.

--- dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/Transactional.java
+++ dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/Transactional.java
@@ -7,6 +7,14 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * When annotating a Jersey resource method, wraps the method in a Hibernate session and
+ * transaction. If the resource method returns without throwing an exception, the transaction is
+ * committed and the session is closed. If the resource method throws an exception, the transaction
+ * is rolled back and the session is closed.
+ *
+ * @see TransactionalRequestDispatcher
+ */
 @Target(METHOD)
 @Retention(RUNTIME)
 @Documented
