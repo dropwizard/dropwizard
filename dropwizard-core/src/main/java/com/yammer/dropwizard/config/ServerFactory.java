328fa1b9c8fb40d3cc496d01cba4851f9fa2a61b
Jon Radon <jon@radonfamily.com>
2013-02-01 14:53:00 -0500

    Ensure adminPort listens on bindHost
    adminPort was listening on all addresses even when bindHost was specified.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/config/ServerFactory.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/config/ServerFactory.java
@@ -430,6 +430,7 @@ public class ServerFactory {
 
     private Connector createInternalConnector() {
         final SocketConnector connector = new SocketConnector();
+        connector.setHost(config.getBindHost().orNull());
         connector.setPort(config.getAdminPort());
         connector.setName("internal");
         connector.setThreadPool(new QueuedThreadPool(8));
