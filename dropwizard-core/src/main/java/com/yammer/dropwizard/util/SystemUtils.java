a2b21e682c49591aa3127d85c183658835dceb86
Christopher Currie <ccurrie@impresys.com>
2013-02-11 09:23:14 -0800

    Line ending cleanup to help Windows

---
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/util/SystemUtils.java
@@ -0,0 +1,10 @@
+package com.yammer.dropwizard.util;
+
+/**
+ * Utility class for system behavior
+ */
+public final class SystemUtils {
+
+    public static final String LINE_SEPARATOR = java.lang.System.getProperty("line.separator", "\n");
+
+}
