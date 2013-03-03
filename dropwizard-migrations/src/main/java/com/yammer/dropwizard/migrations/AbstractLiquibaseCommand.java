a5ac7b74a5beb36b99a8c79bbd47e20ec91726f5
Coda Hale <coda.hale@gmail.com>
2013-03-02 20:02:15 -0800

    Make database commands exit with non-zero status on failure.
    Closes #262.

--- dropwizard-migrations/src/main/java/com/yammer/dropwizard/migrations/AbstractLiquibaseCommand.java
+++ dropwizard-migrations/src/main/java/com/yammer/dropwizard/migrations/AbstractLiquibaseCommand.java
@@ -40,6 +40,7 @@ public abstract class AbstractLiquibaseCommand<T extends Configuration> extends 
             run(namespace, managedLiquibase);
         } catch (ValidationFailedException e) {
             e.printDescriptiveError(System.err);
+            System.exit(-1);
         } finally {
             if (managedLiquibase != null) {
                 managedLiquibase.stop();
