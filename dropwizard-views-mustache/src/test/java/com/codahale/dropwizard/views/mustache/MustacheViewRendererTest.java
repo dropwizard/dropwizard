c6b2f2648f8fcdee334d5c753f727d5a35440fab
Coda Hale <coda.hale@gmail.com>
2013-05-05 22:27:04 -0700

    Fix MustacheViewRendererTest.

--- dropwizard-views-mustache/src/test/java/com/codahale/dropwizard/views/mustache/MustacheViewRendererTest.java
+++ dropwizard-views-mustache/src/test/java/com/codahale/dropwizard/views/mustache/MustacheViewRendererTest.java
@@ -56,14 +56,14 @@ public class MustacheViewRendererTest extends JerseyTest {
     public void rendersViewsWithAbsoluteTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/absolute").get(String.class);
         assertThat(response)
-                .isEqualToIgnoringCase("Woop woop. yay\n");
+                .isEqualTo("Woop woop. yay\n");
     }
 
     @Test
     public void rendersViewsWithRelativeTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/relative").get(String.class);
         assertThat(response)
-                .isEqualToIgnoringCase("Ok.\n");
+                .isEqualTo("Ok.\n");
     }
 
     @Test
