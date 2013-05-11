3a3cb47ad8c29869e0e95436c20579aef8e17b30
Coda Hale <coda.hale@gmail.com>
2013-05-11 09:39:55 -0700

    Added target to ConsoleAppenderFactory.

--- dropwizard-logging/src/main/java/com/codahale/dropwizard/logging/ConsoleAppenderFactory.java
+++ dropwizard-logging/src/main/java/com/codahale/dropwizard/logging/ConsoleAppenderFactory.java
@@ -8,20 +8,52 @@ import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.spi.FilterAttachable;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.common.base.Strings;
 
 import javax.validation.constraints.NotNull;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @JsonTypeName("console")
 public class ConsoleAppenderFactory implements AppenderFactory {
+    public enum OutputTarget {
+        STDERR("System.err"),
+        STDOUT("System.out");
+
+        private final String logbackName;
+
+        private OutputTarget(String logbackName) {
+            this.logbackName = logbackName;
+        }
+
+        @JsonCreator
+        public static OutputTarget parse(String s) {
+            return valueOf(s.toUpperCase(Locale.US));
+        }
+
+        @Override
+        @JsonValue
+        public String toString() {
+            return super.toString().toLowerCase(Locale.US);
+        }
+
+        public String getLogbackName() {
+            return logbackName;
+        }
+    }
+
     @NotNull
     private Level threshold = Level.ALL;
 
     @NotNull
     private TimeZone timeZone = TimeZone.getTimeZone("UTC");
+
+    @NotNull
+    private OutputTarget target = OutputTarget.STDOUT;
 
     private String logFormat;
 
@@ -59,6 +91,7 @@ public class ConsoleAppenderFactory implements AppenderFactory {
     public Appender<ILoggingEvent> build(LoggerContext context, String applicationName, Layout<ILoggingEvent> layout) {
         final ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
         appender.setContext(context);
+        appender.setTarget(target.getLogbackName());
         if (layout == null) {
             final LogFormatter formatter = new LogFormatter(context, timeZone);
             if (!Strings.isNullOrEmpty(logFormat)) {
