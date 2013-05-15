745ce79424cd5cd3c61994c775ad8f70931a0f0c
Coda Hale <coda.hale@gmail.com>
2013-05-15 16:15:05 -0700

    Added docs for DataSourceFactory.

--- dropwizard-db/src/main/java/com/codahale/dropwizard/db/DataSourceFactory.java
+++ dropwizard-db/src/main/java/com/codahale/dropwizard/db/DataSourceFactory.java
@@ -2,12 +2,11 @@ package com.codahale.dropwizard.db;
 
 import com.codahale.dropwizard.util.Duration;
 import com.codahale.dropwizard.validation.MinDuration;
+import com.codahale.dropwizard.validation.OneOf;
 import com.codahale.dropwizard.validation.ValidationMethod;
 import com.codahale.metrics.MetricRegistry;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
@@ -16,45 +15,236 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.sql.Connection;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
-// TODO: 5/15/13 <coda> -- add tests for DataSourceFactory
-// TODO: 5/15/13 <coda> -- add docs for DataSourceFactory
-
-@SuppressWarnings("UnusedDeclaration")
+/**
+ * A factory for pooled {@link ManagedDataSource}s.
+ * <p/>
+ * <b>Configuration Parameters:</b>
+ * <table>
+ *     <tr>
+ *         <td>Name</td>
+ *         <td>Default</td>
+ *         <td>Description</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code driverClass}</td>
+ *         <td><b>REQUIRED</b></td>
+ *         <td>The full name of the JDBC driver class.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code url}</td>
+ *         <td><b>REQUIRED</b></td>
+ *         <td>The URL of the server.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code user}</td>
+ *         <td><b>REQUIRED</b></td>
+ *         <td>The username used to connect to the server.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code password}</td>
+ *         <td>none</td>
+ *         <td>The password used to connect to the server.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code abandonWhenPercentageFull}</td>
+ *         <td>0</td>
+ *         <td>
+ *             Connections that have been abandoned (timed out) won't get closed and reported up
+ *             unless the number of connections in use are above the percentage defined by
+ *             {@code abandonWhenPercentageFull}. The value should be between 0-100.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code alternateUsernamesAllowed}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             Set to true if the call
+ *             {@link javax.sql.DataSource#getConnection(String, String) getConnection(username,password)}
+ *             is allowed. This is used for when the pool is used by an application accessing
+ *             multiple schemas. There is a performance impact turning this option on, even when not
+ *             used.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code commitOnReturn}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             Set to true if you want the connection pool to commit any pending transaction when a
+ *             connection is returned.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code autoCommitByDefault}</td>
+ *         <td>JDBC driver's default</td>
+ *         <td>The default auto-commit state of the connections.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code readOnlyByDefault}</td>
+ *         <td>JDBC driver's default</td>
+ *         <td>The default read-only state of the connections.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code properties}</td>
+ *         <td>none</td>
+ *         <td>Any additional JDBC driver parameters.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code defaultCatalog}</td>
+ *         <td>none</td>
+ *         <td>The default catalog to use for the connections.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code defaultTransactionIsolation}</td>
+ *         <td>JDBC driver default</td>
+ *         <td>
+ *             The default transaction isolation to use for the connections. Can be one of
+ *             {@code none}, {@code default}, {@code read-uncommitted}, {@code read-committed},
+ *             {@code repeatable-read}, or {@code serializable}.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code useFairQueue}</td>
+ *         <td>{@code true}</td>
+ *         <td>
+ *             If {@code true}, calls to {@code getConnection} are handled in a FIFO manner.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code initialSize}</td>
+ *         <td>10</td>
+ *         <td>
+ *             The initial size of the connection pool.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code minSize}</td>
+ *         <td>10</td>
+ *         <td>
+ *             The minimum size of the connection pool.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code maxSize}</td>
+ *         <td>10</td>
+ *         <td>
+ *             The maximum size of the connection pool.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code initializationQuery}</td>
+ *         <td>none</td>
+ *         <td>
+ *             A custom query to be run when a connection is first created.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code logAbandonedConnections}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             If {@code true}, logs stack traces of abandoned connections.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code logValidationErrors}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             If {@code true}, logs errors when connections fail validation.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code maxConnectionAge}</td>
+ *         <td>none</td>
+ *         <td>
+ *             If set, connections which have been open for longer than {@code maxConnectionAge} are
+ *             closed when returned.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code maxWaitForConnection}</td>
+ *         <td>30 seconds</td>
+ *         <td>
+ *             If a request for a connection is blocked for longer than this period, an exception
+ *             will be thrown.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code minIdleTime}</td>
+ *         <td>1 minute</td>
+ *         <td>
+ *             The minimum amount of time an connection must sit idle in the pool before it is
+ *             eligible for eviction.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code validationQuery}</td>
+ *         <td>{@code SELECT 1}</td>
+ *         <td>
+ *             The SQL query that will be used to validate connections from this pool before
+ *             returning them to the caller or pool. If specified, this query does not have to
+ *             return any data, it just can't throw a SQLException.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code checkConnectionWhileIdle}</td>
+ *         <td>{@code true}</td>
+ *         <td>
+ *             Set to true if query validation should take place while the connection is idle.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code checkConnectionOnBorrow}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             Whether or not connections will be validated before being borrowed from the pool. If
+ *             the connection fails to validate, it will be dropped from the pool, and another will
+ *             be borrowed.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code checkConnectionOnConnect}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             Whether or not connections will be validated before being added to the pool. If the
+ *             connection fails to validate, it won't be added to the pool.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code checkConnectionOnReturn}</td>
+ *         <td>{@code false}</td>
+ *         <td>
+ *             Whether or not connections will be validated after being returned to the pool. If
+ *             the connection fails to validate, it will be dropped from the pool.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code autoCommentsEnabled}</td>
+ *         <td>{@code true}</td>
+ *         <td>
+ *             Whether or not ORMs should automatically add comments.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code evictionInterval}</td>
+ *         <td>5 seconds</td>
+ *         <td>
+ *             The amount of time to sleep between runs of the idle connection validation, abandoned
+ *             cleaner and idle pool resizing.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code validationInterval}</td>
+ *         <td>30 seconds</td>
+ *         <td>
+ *             To avoid excess validation, only run validation once every interval.
+ *         </td>
+ *     </tr>
+ * </table>
+ */
 public class DataSourceFactory {
-    public enum TransactionIsolation {
-        NONE(Connection.TRANSACTION_NONE),
-        READ_UNCOMMITTED(Connection.TRANSACTION_READ_UNCOMMITTED),
-        READ_COMMITTED(Connection.TRANSACTION_READ_COMMITTED),
-        REPEATABLE_READ(Connection.TRANSACTION_REPEATABLE_READ),
-        SERIALIZABLE(Connection.TRANSACTION_SERIALIZABLE),
-        DEFAULT(org.apache.tomcat.jdbc.pool.DataSourceFactory.UNKNOWN_TRANSACTIONISOLATION);
-
-        private final int value;
-
-        TransactionIsolation(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        @Override
-        @JsonValue
-        public String toString() {
-            return super.toString().replace("_", "+").toLowerCase(Locale.ENGLISH);
-        }
-
-        @JsonCreator
-        public static TransactionIsolation parse(String type) {
-            return valueOf(type.toUpperCase(Locale.ENGLISH).replace("[^A-Za-z]", "_"));
-        }
-    }
-
     @NotNull
     private String driverClass = null;
 
@@ -84,7 +274,12 @@ public class DataSourceFactory {
     private String defaultCatalog;
 
     @NotNull
-    private TransactionIsolation defaultTransactionIsolation = TransactionIsolation.DEFAULT;
+    @OneOf(
+            value = {"none", "default", "read-uncommitted", "read-committed", "repeatable-read", "serializable"},
+            ignoreCase = true,
+            ignoreWhitespace = true
+    )
+    private String defaultTransactionIsolation = "default";
 
     private boolean useFairQueue = true;
 
@@ -99,19 +294,19 @@ public class DataSourceFactory {
 
     private String initializationQuery;
 
-    private boolean logAbandonedQueries = false;
+    private boolean logAbandonedConnections = false;
 
     private boolean logValidationErrors = false;
 
-    @MinDuration(1)
+    @MinDuration(value = 1, unit = TimeUnit.SECONDS)
     private Duration maxConnectionAge;
 
     @NotNull
-    @MinDuration(1)
+    @MinDuration(value = 1, unit = TimeUnit.SECONDS)
     private Duration maxWaitForConnection = Duration.seconds(30);
 
     @NotNull
-    @MinDuration(1)
+    @MinDuration(value = 1, unit = TimeUnit.SECONDS)
     private Duration minIdleTime = Duration.minutes(1);
 
     @NotNull
@@ -121,7 +316,7 @@ public class DataSourceFactory {
 
     private boolean checkConnectionOnBorrow = false;
 
-    private boolean checkConnectionOnConnect = false;
+    private boolean checkConnectionOnConnect = true;
 
     private boolean checkConnectionOnReturn = false;
 
@@ -336,13 +531,13 @@ public class DataSourceFactory {
     }
 
     @JsonProperty
-    public TransactionIsolation getDefaultTransactionIsolation() {
+    public String getDefaultTransactionIsolation() {
         return defaultTransactionIsolation;
     }
 
     @JsonProperty
-    public void setDefaultTransactionIsolation(TransactionIsolation defaultTransactionIsolation) {
-        this.defaultTransactionIsolation = defaultTransactionIsolation;
+    public void setDefaultTransactionIsolation(String isolation) {
+        this.defaultTransactionIsolation = isolation;
     }
 
     @JsonProperty
@@ -376,13 +571,13 @@ public class DataSourceFactory {
     }
 
     @JsonProperty
-    public boolean getLogAbandonedQueries() {
-        return logAbandonedQueries;
+    public boolean getLogAbandonedConnections() {
+        return logAbandonedConnections;
     }
 
     @JsonProperty
-    public void setLogAbandonedQueries(boolean log) {
-        this.logAbandonedQueries = log;
+    public void setLogAbandonedConnections(boolean log) {
+        this.logAbandonedConnections = log;
     }
 
     @JsonProperty
@@ -480,12 +675,12 @@ public class DataSourceFactory {
         poolConfig.setDefaultAutoCommit(autoCommitByDefault);
         poolConfig.setDefaultCatalog(defaultCatalog);
         poolConfig.setDefaultReadOnly(readOnlyByDefault);
-        poolConfig.setDefaultTransactionIsolation(defaultTransactionIsolation.getValue());
+        poolConfig.setDefaultTransactionIsolation(getIsolation());
         poolConfig.setDriverClassName(driverClass);
         poolConfig.setFairQueue(useFairQueue);
         poolConfig.setInitialSize(initialSize);
         poolConfig.setInitSQL(initializationQuery);
-        poolConfig.setLogAbandoned(logAbandonedQueries);
+        poolConfig.setLogAbandoned(logAbandonedConnections);
         poolConfig.setLogValidationErrors(logValidationErrors);
         poolConfig.setMaxActive(maxSize);
         poolConfig.setMaxIdle(maxSize);
@@ -507,8 +702,25 @@ public class DataSourceFactory {
         poolConfig.setTestOnConnect(checkConnectionOnConnect);
         poolConfig.setTestOnReturn(checkConnectionOnReturn);
         poolConfig.setTimeBetweenEvictionRunsMillis((int) evictionInterval.toMilliseconds());
-        poolConfig.setValidationInterval(1);
+        poolConfig.setValidationInterval(validationInterval.toMilliseconds());
 
         return new ManagedPooledDataSource(poolConfig, metricRegistry);
+    }
+
+    private int getIsolation() {
+        switch (defaultTransactionIsolation.toLowerCase()) {
+            case "none":
+                return Connection.TRANSACTION_NONE;
+            case "read-uncommitted":
+                return Connection.TRANSACTION_READ_UNCOMMITTED;
+            case "read-committed":
+                return Connection.TRANSACTION_READ_COMMITTED;
+            case "repeatable-read":
+                return Connection.TRANSACTION_REPEATABLE_READ;
+            case "serializable":
+                return Connection.TRANSACTION_SERIALIZABLE;
+            default:
+                return org.apache.tomcat.jdbc.pool.DataSourceFactory.UNKNOWN_TRANSACTIONISOLATION;
+        }
     }
 }
