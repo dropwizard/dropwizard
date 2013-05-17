7cf2911046344d1a71450b5dd0fc651728ebaf4a
Coda Hale <coda.hale@gmail.com>
2013-05-16 18:39:51 -0700

    Added MutableServletContextHandler.
    Because we don't know ahead of time if we'll need a security manager or a session manager.

---
+++ dropwizard-jetty/src/test/java/com/codahale/dropwizard/jetty/MutableServletContextHandlerTest.java
@@ -0,0 +1,67 @@
+package com.codahale.dropwizard.jetty;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class MutableServletContextHandlerTest {
+    private final MutableServletContextHandler handler = new MutableServletContextHandler();
+
+    @Test
+    public void defaultsToSessionsBeingDisabled() throws Exception {
+        assertThat(handler.isSessionsEnabled())
+                .isFalse();
+    }
+
+    @Test
+    public void defaultsToSecurityBeingDisabled() throws Exception {
+        assertThat(handler.isSecurityEnabled())
+                .isFalse();
+    }
+
+    @Test
+    public void canEnableSessionManagement() throws Exception {
+        handler.setSessionsEnabled(true);
+
+        assertThat(handler.isSessionsEnabled())
+                .isTrue();
+
+        assertThat(handler.isSecurityEnabled())
+                .isFalse();
+    }
+
+    @Test
+    public void canDisableSessionManagement() throws Exception {
+        handler.setSessionsEnabled(true);
+        handler.setSessionsEnabled(false);
+
+        assertThat(handler.isSessionsEnabled())
+                .isFalse();
+
+        assertThat(handler.isSecurityEnabled())
+                .isFalse();
+    }
+
+    @Test
+    public void canEnableSecurity() throws Exception {
+        handler.setSecurityEnabled(true);
+
+        assertThat(handler.isSessionsEnabled())
+                .isFalse();
+
+        assertThat(handler.isSecurityEnabled())
+                .isTrue();
+    }
+
+    @Test
+    public void canDisableSecurity() throws Exception {
+        handler.setSecurityEnabled(true);
+        handler.setSecurityEnabled(false);
+
+        assertThat(handler.isSessionsEnabled())
+                .isFalse();
+
+        assertThat(handler.isSecurityEnabled())
+                .isFalse();
+    }
+}
