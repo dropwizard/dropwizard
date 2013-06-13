4aef494f1c8afb1d9e3fea859a8dee0168e791d3
spacanowski <spacanowski@gmail.com>
2013-06-13 17:22:11 +0200

    Added ScanningHibernateBundle - extension of HibernateBundle with package scaner

---
+++ dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/fake/entities/pckg/FakeEntity2.java
@@ -0,0 +1,8 @@
+package com.codahale.dropwizard.hibernate.fake.entities.pckg;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FakeEntity2 {
+
+}
---
+++ dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/fake/entities/pckg/deep/deeper/FakeEntity1.java
@@ -0,0 +1,8 @@
+package com.codahale.dropwizard.hibernate.fake.entities.pckg.deep.deeper;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FakeEntity1 {
+
+}
