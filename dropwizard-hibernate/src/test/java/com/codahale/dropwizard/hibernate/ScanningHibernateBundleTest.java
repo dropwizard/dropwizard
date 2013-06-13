a3dd8b4b67265889552de84a0017c0407f53c349
spacanowski <spacanowski@gmail.com>
2013-06-13 17:22:11 +0200

    Added ScanningHibernateBundle - extension of HibernateBundle with package scaner

---
+++ dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/ScanningHibernateBundleTest.java
@@ -0,0 +1,24 @@
+package com.codahale.dropwizard.hibernate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+
+public class ScanningHibernateBundleTest {
+
+	@Test
+	public void testFindEntityClassesFromDirectory() {
+		//given
+		String packageWithEntities = "com.codahale.dropwizard.hibernate.fake.entities.pckg";
+		//when
+		ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(packageWithEntities);
+		
+		//then
+		assertFalse(findEntityClassesFromDirectory.isEmpty());
+		assertEquals(4, findEntityClassesFromDirectory.size());
+	}
+	
+}
---
+++ dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/fake/entities/pckg/deep/FakeEntity1.java
@@ -0,0 +1,8 @@
+package com.codahale.dropwizard.hibernate.fake.entities.pckg.deep;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FakeEntity1 {
+
+}
