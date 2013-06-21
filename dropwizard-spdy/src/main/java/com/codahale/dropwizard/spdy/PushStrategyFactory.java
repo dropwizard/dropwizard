d5427454a0ff2631cda55959b00e75f0b2caaf90
Coda Hale <coda.hale@gmail.com>
2013-06-20 21:49:37 -0700

    Added docs for dw-spdy.
    Plus came up with better names.

--- dropwizard-spdy/src/main/java/com/codahale/dropwizard/spdy/PushStrategyFactory.java
+++ dropwizard-spdy/src/main/java/com/codahale/dropwizard/spdy/PushStrategyFactory.java
@@ -4,6 +4,9 @@ import com.codahale.dropwizard.jackson.Discoverable;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.eclipse.jetty.spdy.server.http.PushStrategy;
 
+/**
+ * Builds {@link PushStrategy} instances for SPDY connectors.
+ */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 public interface PushStrategyFactory extends Discoverable {
     PushStrategy build();
