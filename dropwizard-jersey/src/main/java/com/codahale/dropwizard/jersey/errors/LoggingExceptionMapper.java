2e80e85d1b9a2d28289c6ed0ae036e9b88b531b7
Adam Jordens <adam@jordens.org>
2013-07-16 21:16:34 -0700

    A servlet filter that adds request identifiers to slf4j's mapped diagnostic context (MDC) , it allows logging output at multiple levels of your service to be easily grouped by the originating request.
    - Updated RequestIdLoggingFilter to add support for checking clientId's against a regex for validity.
    - Updated LoggingExceptionMapper to use the Service-Request-Id if it's available in the MDC.

--- dropwizard-jersey/src/main/java/com/codahale/dropwizard/jersey/errors/LoggingExceptionMapper.java
+++ dropwizard-jersey/src/main/java/com/codahale/dropwizard/jersey/errors/LoggingExceptionMapper.java
@@ -1,7 +1,10 @@
 package com.codahale.dropwizard.jersey.errors;
 
+import com.codahale.dropwizard.util.RequestId;
+import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -9,32 +12,44 @@ import javax.ws.rs.ext.ExceptionMapper;
 import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class LoggingExceptionMapper<E extends Throwable> implements ExceptionMapper<E> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingExceptionMapper.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(LoggingExceptionMapper.class);
 
-    @Override
-    public Response toResponse(E exception) {
-        if (exception instanceof WebApplicationException) {
-            return ((WebApplicationException) exception).getResponse();
-        }
-
-        final long id = ThreadLocalRandom.current().nextLong();
-        logException(id, exception);
-        return Response.serverError()
-                       .entity(new ErrorMessage(formatErrorMessage(id, exception)))
-                       .build();
+  @Override
+  public Response toResponse(E exception) {
+    if (exception instanceof WebApplicationException) {
+      return ((WebApplicationException) exception).getResponse();
     }
 
-    @SuppressWarnings("UnusedParameters")
-    protected String formatErrorMessage(long id, E exception) {
-        return String.format("There was an error processing your request. It has been logged (ID %016x).", id);
+    final long id = getServiceRequestId();
+    logException(id, exception);
+    return Response.serverError()
+        .entity(new ErrorMessage(formatErrorMessage(id, exception)))
+        .build();
+  }
+
+  @SuppressWarnings("UnusedParameters")
+  protected String formatErrorMessage(long id, E exception) {
+    return String.format("There was an error processing your request. It has been logged (ID %016x).", id);
+  }
+
+  protected void logException(long id, E exception) {
+    LOGGER.error(formatLogMessage(id, exception), exception);
+  }
+
+  @SuppressWarnings("UnusedParameters")
+  protected String formatLogMessage(long id, Throwable exception) {
+    return String.format("Error handling a request: %016x", id);
+  }
+
+  /**
+   * @return service request id from the underlying logger MDC, or a random number if not available.
+   */
+  long getServiceRequestId() {
+    String requestId = MDC.get(RequestId.SERVICE_REQUEST_ID);
+    if (requestId != null) {
+      return Long.valueOf(requestId);
     }
 
-    protected void logException(long id, E exception) {
-        LOGGER.error(formatLogMessage(id, exception), exception);
-    }
-
-    @SuppressWarnings("UnusedParameters")
-    protected String formatLogMessage(long id, Throwable exception) {
-        return String.format("Error handling a request: %016x", id);
-    }
+    return ThreadLocalRandom.current().nextLong();
+  }
 }
