7e2aeef16ca99db8c2d23b5cf4277901d1d558dc
Nick Telford <nick.telford@gmail.com>
2013-07-22 12:33:38 +0100

    Generalize DropwizardAppRule
    `DropwizardAppRule` uses reflection to invoke the nullary constructor of
    the target `Application`. However, applications that don't define a
    public nullary constructor cannot be used.
    
    This affects all Scala Applications, because the app is defined as an
    Object, (which has a private nullary constructor).
    
    This will also affect apps that are parameterized in some way, or that
    protect the nullary constructor (e.g. for Singletons).
    
    By introducing a `newApplication()` method and providing the current,
    reflection-based implementation as the default, users may override this
    to provide instances of their application themselves.

--- dropwizard-testing/src/main/java/com/codahale/dropwizard/testing/junit/DropwizardAppRule.java
+++ dropwizard-testing/src/main/java/com/codahale/dropwizard/testing/junit/DropwizardAppRule.java
@@ -13,10 +13,16 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
 import java.util.Enumeration;
 
 /**
  * A JUnit rule for starting and stopping your application at the start and end of a test class.
+ * <p/>
+ * By default, the {@link Application} will be constructed using reflection to invoke the nullary
+ * constructor. If your application does not provide a public nullary constructor, you will need to
+ * override the {@link #newApplication()} method to provide your application instance(s).
+ *
  * @param <C> the configuration type
  */
 public class DropwizardAppRule<C extends Configuration> implements TestRule {
@@ -70,7 +76,7 @@ public class DropwizardAppRule<C extends Configuration> implements TestRule {
         }
 
         try {
-            application = applicationClass.newInstance();
+            application = newApplication();
 
             final Bootstrap<C> bootstrap = new Bootstrap<C>(application) {
                 @Override
@@ -104,9 +110,17 @@ public class DropwizardAppRule<C extends Configuration> implements TestRule {
         return ((ServerConnector) jettyServer.getConnectors()[0]).getLocalPort();
     }
 
+    public Application<C> newApplication() {
+        try {
+            return applicationClass.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @SuppressWarnings("unchecked")
-    public <S extends Application<C>> S getApplication() {
-        return (S) application;
+    public <A extends Application<C>> A getApplication() {
+        return (A) application;
     }
 
     public Environment getEnvironment() {
