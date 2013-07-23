dd46def176da71279b31e1171ce2a96de2f4b94c
Adam Jordens <adam@jordens.org>
2013-07-23 11:23:25 -0700

    A servlet filter that adds request identifiers to slf4j's mapped diagnostic context (MDC), it allows logging output at multiple levels of your service to be easily grouped by the originating request.
    - Updated RequestIdLoggingFilter to add support for checking clientId's against a regex for validity.
    - Updated LoggingExceptionMapper to use the Service-Request-Id if it's available in the MDC.

--- dropwizard-jersey/src/test/java/com/codahale/dropwizard/jersey/errors/LoggingExceptionMapperTest.java
+++ dropwizard-jersey/src/test/java/com/codahale/dropwizard/jersey/errors/LoggingExceptionMapperTest.java
@@ -1,10 +1,12 @@
 package com.codahale.dropwizard.jersey.errors;
 
 import com.codahale.dropwizard.logging.LoggingFactory;
+import com.codahale.dropwizard.util.RequestId;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
+import org.apache.log4j.MDC;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
@@ -34,5 +36,22 @@ public class LoggingExceptionMapperTest extends JerseyTest {
             assertThat(e.getResponse().getEntity(String.class))
                     .startsWith("{\"message\":\"There was an error processing your request. It has been logged (ID ");
         }
+    }
+
+    @Test
+    public void requestIdInMDC() {
+      LoggingExceptionMapper mapper = new DefaultLoggingExceptionMapper();
+      try {
+          MDC.put(RequestId.SERVICE_REQUEST_ID, "1234");
+          assertThat(mapper.getServiceRequestId()).isEqualTo(1234);
+      } finally {
+          MDC.remove(RequestId.SERVICE_REQUEST_ID);
+      }
+    }
+
+    @Test
+    public void requestIdNotInMDC() {
+        LoggingExceptionMapper mapper = new DefaultLoggingExceptionMapper();
+        assertThat(mapper.getServiceRequestId()).isNotNull();
     }
 }
