90b3b0057baeba491fc13220f9b9e72d5e7c5f19
Andrew Look <look@adlast.com>
2013-09-16 16:40:53 -0700

    Add support for Joda DateTime objects to jDBI.

--- dropwizard-jdbi/src/test/java/com/codahale/dropwizard/jdbi/JDBITest.java
+++ dropwizard-jdbi/src/test/java/com/codahale/dropwizard/jdbi/JDBITest.java
@@ -11,6 +11,7 @@ import com.codahale.metrics.health.HealthCheckRegistry;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,7 @@ import org.skife.jdbi.v2.Handle;
 import org.skife.jdbi.v2.Query;
 import org.skife.jdbi.v2.util.StringMapper;
 
+import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.List;
 
@@ -63,22 +65,25 @@ public class JDBITest {
         try (Handle handle = dbi.open()) {
             handle.createCall("DROP TABLE people IF EXISTS").invoke();
             handle.createCall(
-                    "CREATE TABLE people (name varchar(100) primary key, email varchar(100), age int)")
+                    "CREATE TABLE people (name varchar(100) primary key, email varchar(100), age int, created_at timestamp)")
                   .invoke();
-            handle.createStatement("INSERT INTO people VALUES (?, ?, ?)")
+            handle.createStatement("INSERT INTO people VALUES (?, ?, ?, ?)")
                   .bind(0, "Coda Hale")
                   .bind(1, "chale@yammer-inc.com")
                   .bind(2, 30)
+                  .bind(3, new Timestamp(1365465078000L))
                   .execute();
-            handle.createStatement("INSERT INTO people VALUES (?, ?, ?)")
+            handle.createStatement("INSERT INTO people VALUES (?, ?, ?, ?)")
                   .bind(0, "Kris Gale")
                   .bind(1, "kgale@yammer-inc.com")
                   .bind(2, 32)
+                  .bind(3, new Timestamp(1365465078000L))
                   .execute();
-            handle.createStatement("INSERT INTO people VALUES (?, ?, ?)")
+            handle.createStatement("INSERT INTO people VALUES (?, ?, ?, ?)")
                   .bind(0, "Old Guy")
                   .bindNull(1, Types.VARCHAR)
                   .bind(2, 99)
+                  .bind(3, new Timestamp(1365465078000L))
                   .execute();
         }
     }
@@ -145,5 +150,15 @@ public class JDBITest {
         assertThat(missing).isNotNull();
         assertThat(missing.isPresent()).isFalse();
         assertThat(missing.orNull()).isNull();
+    }
+
+    @Test
+    public void sqlObjectsCanReturnJodaDateTime() throws Exception {
+        final PersonDAO dao = dbi.open(PersonDAO.class);
+
+        final DateTime found = dao.getLatestCreatedAt(new DateTime(1365465078000L));
+        assertThat(found).isNotNull();
+        assertThat(found.getMillis()).isEqualTo(1365467078000L);
+        assertThat(found).isEqualTo(new DateTime(1365467078000L));
     }
 }
--- dropwizard-jdbi/src/test/java/com/codahale/dropwizard/jdbi/PersonDAO.java
+++ dropwizard-jdbi/src/test/java/com/codahale/dropwizard/jdbi/PersonDAO.java
@@ -3,6 +3,7 @@ package com.codahale.dropwizard.jdbi;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import org.joda.time.DateTime;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
@@ -20,4 +21,7 @@ public interface PersonDAO {
     @SqlQuery("SELECT name FROM people WHERE email = :email ")
     @SingleValueResult(String.class)
     public Optional<String> findByEmail(@Bind("email")String email);
+
+    @SqlQuery("SELECT created_at FROM people WHERE created_at > :from ORDER BY created_at DESC LIMIT 1")
+    public DateTime getLatestCreatedAt(@Bind("from") DateTime from);
 }
