e179f3a60e118a92e0b03e7d1db8f2881ff8ef0a
Adam Thody <adam@adamthody.com>
2013-09-23 16:15:57 -0400

    Add JUnit rule to run migrations before tests and teardown after.

---
+++ dropwizard-testing-migrations/src/main/java/io/dropwizard/testing/junit/DatabaseMigrationRule.java
@@ -0,0 +1,50 @@
+package io.dropwizard.testing.junit;
+
+import com.google.common.base.Strings;
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LockException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.junit.rules.ExternalResource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseMigrationRule extends ExternalResource {
+
+    public static final String DEFAULT_CHANGELOG_FILENAME = "migrations.xml";
+    private final String url;
+    private final String username;
+    private final String password;
+    private final String changeLogFile;
+    private Liquibase liquibase;
+
+    public DatabaseMigrationRule(String url, String username, String password) {
+        this(url, username, password, null);
+    }
+
+    public DatabaseMigrationRule(String url, String username, String password, String changeLogFile) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.changeLogFile = Strings.isNullOrEmpty(changeLogFile) ? DEFAULT_CHANGELOG_FILENAME : changeLogFile;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        Connection connection = DriverManager.getConnection(url, username, password);
+        liquibase = new Liquibase(changeLogFile, new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
+        liquibase.update(null);
+    }
+
+    @Override
+    protected void after() {
+        try {
+            liquibase.dropAll();
+        } catch (DatabaseException | LockException e) {
+            // Wrap checked exceptions and rethrow to fail fast and not cause unexpected results in the next test.
+            throw new RuntimeException(e);
+        }
+    }
+}
