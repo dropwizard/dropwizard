af24145647928e005e4e7e3e216cf3c8e490bcb7
_ Kilemensi <kilemensi@gmail.com>
2013-09-30 01:18:12 +0300

    Fixes #398: EOL in tests strings must match those in template files

--- dropwizard-views-freemarker/src/test/java/io/dropwizard/views/freemarker/FreemarkerViewRendererTest.java
+++ dropwizard-views-freemarker/src/test/java/io/dropwizard/views/freemarker/FreemarkerViewRendererTest.java
@@ -56,14 +56,14 @@ public class FreemarkerViewRendererTest extends JerseyTest {
     public void rendersViewsWithAbsoluteTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/absolute").get(String.class);
         assertThat(response)
-                .isEqualToIgnoringCase(String.format("Woop woop. yay%n"));
+                .isEqualToIgnoringCase("Woop woop. yay\n");
     }
 
     @Test
     public void rendersViewsWithRelativeTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/relative").get(String.class);
         assertThat(response)
-                .isEqualToIgnoringCase(String.format("Ok.%n"));
+                .isEqualToIgnoringCase("Ok.\n");
     }
 
     @Test
