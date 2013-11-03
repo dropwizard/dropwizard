bd86eeb96fab2c0bb74c22441cab946d384f1a9e
Kristian Klette <klette@klette.us>
2013-11-03 15:48:54 +0100

    Add support for using Optional<DateTime> in dropwizard-jdbi
    Add support for Optional<DateTime> in dropwizard-jdbi by handling NULL-values
    in JodaDateTimeMapper. This also fixes a NPE-occuring when using the
    existing mapper to plain DateTime-object if extracted column is NULL.

--- dropwizard-jdbi/src/main/java/io/dropwizard/jdbi/args/JodaDateTimeMapper.java
+++ dropwizard-jdbi/src/main/java/io/dropwizard/jdbi/args/JodaDateTimeMapper.java
@@ -5,6 +5,7 @@ import org.skife.jdbi.v2.util.TypedMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 /**
  * A {@link TypedMapper} to map Joda {@link DateTime} objects.
@@ -13,11 +14,19 @@ public class JodaDateTimeMapper extends TypedMapper<DateTime> {
 
     @Override
     protected DateTime extractByName(final ResultSet r, final String name) throws SQLException {
-        return new DateTime(r.getTimestamp(name).getTime());
+        final Timestamp timestamp = r.getTimestamp(name);
+        if (timestamp == null) {
+            return null;
+        }
+        return new DateTime(timestamp.getTime());
     }
 
     @Override
     protected DateTime extractByIndex(final ResultSet r, final int index) throws SQLException {
-        return new DateTime(r.getTimestamp(index).getTime());
+        final Timestamp timestamp = r.getTimestamp(index);
+        if (timestamp == null) {
+            return null;
+        }
+        return new DateTime(timestamp.getTime());
     }
 }
