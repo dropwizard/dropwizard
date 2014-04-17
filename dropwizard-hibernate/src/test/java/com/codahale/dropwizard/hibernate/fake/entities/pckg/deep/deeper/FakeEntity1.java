31ef4ab7f613f958b028176aaf75f0321518d5b1
Szymon Pacanowski <spacanowski@gmail.com>
2014-04-17 17:27:46 +0200

    By hand test fix

--- dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/fake/entities/pckg/deep/deeper/FakeEntity1.java
+++
@@ -1,8 +0,0 @@
-package com.codahale.dropwizard.hibernate.fake.entities.pckg.deep.deeper;
-
-import javax.persistence.Entity;
-
-@Entity
-public class FakeEntity1 {
-
-}
---
+++ dropwizard-hibernate/src/test/java/com/codahale/dropwizard/hibernate/fake/entities/pckg/deep/deeper/dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/fake/entities/pckg/deep/ deeperFakeEntity1.java
@@ -0,0 +1,8 @@
+package io.dropwizard.hibernate.fake.entities.pckg.deep.deeper;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FakeEntity1 {
+
+}
