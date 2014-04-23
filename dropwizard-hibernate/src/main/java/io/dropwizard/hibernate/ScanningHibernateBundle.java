328f2a73111915ae03dc8f3b03a8148397059674
varunl <varunl@users.noreply.github.com>
2014-04-23 13:56:55 -0700

    Change tabs to spaces.

--- dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/ScanningHibernateBundle.java
+++ dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/ScanningHibernateBundle.java
@@ -14,40 +14,39 @@ import com.sun.jersey.spi.scanning.AnnotationScannerListener;
  */
 public abstract class ScanningHibernateBundle<T extends Configuration> extends HibernateBundle<T> {
 
-	/**
-	 * 
-	 * @param pckg string with package containing Hibernate entities (classes annotated with Hibernate @Entity annotation) 
-	 * 			e.g. com.codahale.fake.db.directory.entities
-	 */
-	protected ScanningHibernateBundle(String pckg) {
-		this(pckg, new SessionFactoryFactory());
-	}
-	
-	protected ScanningHibernateBundle(String pckg, SessionFactoryFactory sessionFactoryFactory) {
-		super(findEntityClassesFromDirectory(pckg), sessionFactoryFactory);
-	}
-	
-	/**
-	 * Method scanning given directory for classes containing Hibernate @Entity annotation
-	 * 
-	 * @param pckg string with package containing Hibernate entities (classes annotated with @Entity annotation) 
-	 * 			e.g. com.codahale.fake.db.directory.entities
-	 * @return ImmutableList with classes from given directory annotated with Hibernate @Entity annotation
-	 */
-	public static ImmutableList<Class<?>> findEntityClassesFromDirectory(String pckg) {
-		Builder<Class<?>> builder = ImmutableList.<Class<?>>builder();
+    /**
+     *
+     * @param pckg string with package containing Hibernate entities (classes annotated with Hibernate @Entity annotation)
+     *          e.g. com.codahale.fake.db.directory.entities
+     */
+    protected ScanningHibernateBundle(String pckg) {
+        this(pckg, new SessionFactoryFactory());
+    }
 
-		PackageNamesScanner scanner = new PackageNamesScanner(new String[] {pckg});
+    protected ScanningHibernateBundle(String pckg, SessionFactoryFactory sessionFactoryFactory) {
+        super(findEntityClassesFromDirectory(pckg), sessionFactoryFactory);
+    }
 
-		@SuppressWarnings("unchecked")
-		final AnnotationScannerListener asl = new AnnotationScannerListener(Entity.class);
+    /**
+     * Method scanning given directory for classes containing Hibernate @Entity annotation
+     *
+     * @param pckg string with package containing Hibernate entities (classes annotated with @Entity annotation)
+     *          e.g. com.codahale.fake.db.directory.entities
+     * @return ImmutableList with classes from given directory annotated with Hibernate @Entity annotation
+     */
+    public static ImmutableList<Class<?>> findEntityClassesFromDirectory(String pckg) {
+        Builder<Class<?>> builder = ImmutableList.<Class<?>>builder();
+
+        PackageNamesScanner scanner = new PackageNamesScanner(new String[] {pckg});
+
+        @SuppressWarnings("unchecked")
+        final AnnotationScannerListener asl = new AnnotationScannerListener(Entity.class);
         scanner.scan(asl);
 
         for (Class<?> clazz : asl.getAnnotatedClasses()) {
-        	builder.add(clazz);
-		}
+            builder.add(clazz);
+        }
 
-		return builder.build();
-	}
-	
+        return builder.build();
+    }
 }
