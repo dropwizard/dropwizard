5c54af2048336d3e6dc30e0cdb47464df3b8f0a8
Justin Rudd <justin.rudd@gmail.com>
2014-05-09 14:22:21 -0700

    Moves LoggingFactory.bootstrap into constructor.
    This will allow derivative classes to skip logging bootstrapping and depend on the
    normal logback configuration system.

--- dropwizard-core/src/main/java/io/dropwizard/Application.java
+++ dropwizard-core/src/main/java/io/dropwizard/Application.java
@@ -15,10 +15,6 @@ import io.dropwizard.util.JarLocation;
  * @param <T> the type of configuration class for this application
  */
 public abstract class Application<T extends Configuration> {
-    static {
-        // make sure spinning up Hibernate Validator doesn't yell at us
-        LoggingFactory.bootstrap();
-    }
 
     /**
      * Returns the {@link Class} of the configuration class type parameter.
@@ -72,6 +68,16 @@ public abstract class Application<T extends Configuration> {
         if (!cli.run(arguments)) {
             // only exit if there's an error running the command
             System.exit(1);
+        }
+    }
+
+    protected Application() {
+        this(true);
+    }
+
+    protected Application(final boolean bootstrapLogging) {
+        if (bootstrapLogging) {
+            LoggingFactory.bootstrap();
         }
     }
 }
