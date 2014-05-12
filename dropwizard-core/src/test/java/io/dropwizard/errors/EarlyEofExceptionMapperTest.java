92e206f5c0e31cc3e909ce7ee8e39b38d824703f
Jamie Furness <jfurness@yammer-inc.com>
2014-05-12 18:14:53 +0100

    Fixing poor rebase of early EOF exception mapper against master

---
+++ dropwizard-core/src/test/java/io/dropwizard/errors/EarlyEofExceptionMapperTest.java
@@ -0,0 +1,18 @@
+package io.dropwizard.errors;
+
+import org.eclipse.jetty.io.EofException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+public class EarlyEofExceptionMapperTest {
+
+    private final EarlyEofExceptionMapper mapper = new EarlyEofExceptionMapper();
+
+    @Test
+    public void testToReponse() {
+        final Response reponse = mapper.toResponse(new EofException());
+        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), reponse.getStatus());
+    }
+}
