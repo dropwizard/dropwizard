a11a01cd4542f2dba060db8d0b8c4d4e512d7c93
Jamie Furness <jfurness@yammer-inc.com>
2014-05-17 10:32:36 +0100

    Adding a test for setting the http client user agent

--- dropwizard-client/src/test/java/io/dropwizard/client/HttpClientBuilderTest.java
+++ dropwizard-client/src/test/java/io/dropwizard/client/HttpClientBuilderTest.java
@@ -2,9 +2,7 @@ package io.dropwizard.client;
 
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.ImmutableList;
-
 import io.dropwizard.util.Duration;
-
 import org.apache.http.Header;
 import org.apache.http.HeaderIterator;
 import org.apache.http.HttpHeaders;
@@ -61,6 +59,15 @@ public class HttpClientBuilderTest {
 
         assertThat(connectionManager.getDefaultMaxPerRoute())
                 .isEqualTo(413);
+    }
+
+    @Test
+    public void setsTheUserAgent() {
+        configuration.setUserAgent("qwerty");
+
+        final AbstractHttpClient client = (AbstractHttpClient) builder.using(configuration).build("test");
+        assertThat(client.getParams().getParameter(AllClientPNames.USER_AGENT))
+                .isEqualTo("qwerty");
     }
 
     @Test
