b32e48ede517797330be4e9f1057f8d324ba3ff0
Nick Babcock <nbabcock19@hotmail.com>
2014-05-24 22:40:54 -0400

    Appease FindBugs with mkdirs return value

--- dropwizard-metrics/src/main/java/io/dropwizard/metrics/CsvReporterFactory.java
+++ dropwizard-metrics/src/main/java/io/dropwizard/metrics/CsvReporterFactory.java
@@ -54,7 +54,11 @@ public class CsvReporterFactory extends BaseFormattedReporterFactory {
 
     @Override
     public ScheduledReporter build(MetricRegistry registry) {
-        file.mkdirs();
+        boolean creation = file.mkdirs();
+        if (!creation && !file.exists()) {
+            String msg = "Failed to create" + file.getAbsolutePath();
+            throw new RuntimeException(msg);
+        }
 
         return CsvReporter.forRegistry(registry)
                           .convertDurationsTo(getDurationUnit())
