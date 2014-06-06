684610b5b7ebd4055d56baf8eda110cf40e87ce8
Borge Nese <borge@nese.no>
2014-06-06 19:59:47 +0200

    Tests failed in Windows. Changed test to be platform independent.

--- dropwizard-views-mustache/src/test/java/io/dropwizard/views/mustache/MustacheViewRendererTest.java
+++ dropwizard-views-mustache/src/test/java/io/dropwizard/views/mustache/MustacheViewRendererTest.java
@@ -59,14 +59,14 @@ public class MustacheViewRendererTest extends JerseyTest {
     public void rendersViewsWithAbsoluteTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/absolute").get(String.class);
         assertThat(response)
-                .isEqualTo("Woop woop. yay\n");
+                .isEqualTo("Woop woop. yay" + System.lineSeparator());
     }
 
     @Test
     public void rendersViewsWithRelativeTemplatePaths() throws Exception {
         final String response = client().resource(getBaseURI() + "test/relative").get(String.class);
         assertThat(response)
-                .isEqualTo("Ok.\n");
+                .isEqualTo("Ok." + System.lineSeparator());
     }
 
     @Test
