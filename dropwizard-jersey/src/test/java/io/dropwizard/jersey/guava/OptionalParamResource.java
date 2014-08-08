0588768a43734800d8bbc1187bf75e4c08f5a34c
krismassey <opento1982>
2014-08-08 13:30:45 +0100

    Adding Optional @FormParam injectable. Copy of the functionality already provided by for @QueryParams

--- dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalParamResource.java
+++ dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalParamResource.java
@@ -2,17 +2,20 @@ package io.dropwizard.jersey.guava;
 
 import com.google.common.base.Optional;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/optional-param/")
 @Produces(MediaType.TEXT_PLAIN)
 public class OptionalParamResource {
     @GET
-    public String show(@QueryParam("id") Optional<Integer> id) {
+    public String showWithQueryParam(@QueryParam("id") Optional<Integer> id) {
+        return id.or(-1).toString();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public String showWithFormParam(@FormParam("id") Optional<Integer> id) {
         return id.or(-1).toString();
     }
 }
--- dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
+++ dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
@@ -2,17 +2,20 @@ package io.dropwizard.jersey.guava;
 
 import com.google.common.base.Optional;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/optional-return/")
 @Produces(MediaType.TEXT_PLAIN)
 public class OptionalReturnResource {
     @GET
-    public Optional<String> show(@QueryParam("id") String id) {
+    public Optional<String> showWithQueryParam(@QueryParam("id") String id) {
+        return Optional.fromNullable(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Optional<String> showWithFormParam(@QueryParam("id") String id) {
         return Optional.fromNullable(id);
     }
 }
