8fac59950217c8f312215dcd03615644eb4c6642
krismassey <opento1982>
2014-08-08 13:43:57 +0100

    organising import ordering

--- dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
+++ dropwizard-jersey/src/test/java/io/dropwizard/jersey/guava/OptionalReturnResource.java
@@ -1,13 +1,13 @@
 package io.dropwizard.jersey.guava;
 
+import com.google.common.base.Optional;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.FormParam;
-import com.google.common.base.Optional;
-
 import javax.ws.rs.core.MediaType;
 
 @Path("/optional-return/")
