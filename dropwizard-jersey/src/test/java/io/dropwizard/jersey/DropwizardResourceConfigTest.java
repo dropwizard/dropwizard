72669d9f4aba82c709e37c80c93eb5b9e5342d1d
Olve Sæther Hansen <olve@vimond.com>
2014-10-29 11:24:25 +0100

    Test and fix for issue #763

--- dropwizard-jersey/src/test/java/io/dropwizard/jersey/DropwizardResourceConfigTest.java
+++ dropwizard-jersey/src/test/java/io/dropwizard/jersey/DropwizardResourceConfigTest.java
@@ -7,9 +7,12 @@ import io.dropwizard.logging.LoggingFactory;
 import org.junit.Test;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.util.LinkedList;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
 
 @SuppressWarnings("unchecked")
 public class DropwizardResourceConfigTest {
@@ -36,11 +39,69 @@ public class DropwizardResourceConfigTest {
                         (DummyResource.class, TestResource.class);
     }
 
+    @Test
+    public void safeguardAgainstRecursiveEndpointLogging() throws Exception {
+        final DropwizardResourceConfig rc = DropwizardResourceConfig.forTesting(new MetricRegistry());
+        rc.init(new PackageNamesScanner(new String[] { getClass().getPackage().getName() }));
+
+        LinkedList<String> endpoints = new LinkedList<>();
+        try {
+            rc.populateEndpoints(endpoints, "/", TestResource.class, false);
+        } catch (StackOverflowError e) {
+            fail("Did not expect a StackOverflowError here", e);
+        }
+
+        assertThat(endpoints).contains(
+                "    GET     /dummy (io.dropwizard.jersey.DropwizardResourceConfigTest.TestResource)",
+                "    GET     /dummy/sub (io.dropwizard.jersey.DropwizardResourceConfigTest.SubResource)",
+                "    *       /dummy/sub/{subPath} (io.dropwizard.jersey.DropwizardResourceConfigTest.SubResource)",
+                "    *       /dummy/sub/subResource2 (io.dropwizard.jersey.DropwizardResourceConfigTest.SubResource)"
+        );
+
+    }
+
+
     @Path("/dummy")
     public static class TestResource {
         @GET
         public String foo() {
             return "bar";
         }
+
+        @Path("/sub")
+        public SubResource subFooLocator() {
+            return new SubResource();
+        }
     }
+
+    public static class SubResource {
+
+        @GET
+        public String subFoo() {
+            return "subFoo";
+        }
+
+        @Path("{subPath}")
+        public SubResource subSubFooLocator() {
+            return new SubResource();
+        }
+
+        @Path("subResource2")
+        public SubResource2 subSubFooLocator2() {
+            return new SubResource2();
+        }
+
+    }
+    public static class SubResource2 {
+
+        @POST
+        public String subFoo2() {
+            return "subFoo2";
+        }
+
+
+    }
+
+
+
 }
