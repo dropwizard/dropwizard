acefe05ff84c82e1310effa81e18e7698c59dec3
Jochen Schalanda <jochen@schalanda.name>
2014-12-28 19:33:21 +0100

    FixtureHelpers#fixture() should throw IllegalArgumentException
    The semantics of IllegalArgumentException are a better fit for those methods
    than IllegalStateException.
    
    > Thrown to indicate that a method has been passed an illegal or inappropriate argument.

--- dropwizard-testing/src/test/java/io/dropwizard/testing/FixtureHelpersTest.java
+++ dropwizard-testing/src/test/java/io/dropwizard/testing/FixtureHelpersTest.java
@@ -1,25 +1,18 @@
 package io.dropwizard.testing;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FixtureHelpersTest {
-
-    @Rule
-    public final ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void readsTheFileAsAString() {
         assertThat(fixture("fixtures/fixture.txt")).isEqualTo("YAY FOR ME");
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void throwsIllegalStateExceptionWhenFileDoesNotExist() {
-        thrown.expect(IllegalStateException.class);
         fixture("this-does-not-exist.foo");
     }
 }
