de86264f94e67513c2f638b29e5326678225613a
Jochen Schalanda <jochen@schalanda.name>
2015-01-17 17:37:30 +0100

    Add test for async Jersey resource methods, closes #767

---
+++ dropwizard-jersey/src/test/java/io/dropwizard/jersey/AsyncServletTest.java
@@ -0,0 +1,33 @@
+package io.dropwizard.jersey;
+
+import com.codahale.metrics.MetricRegistry;
+import io.dropwizard.jersey.dummy.DummyResource;
+import io.dropwizard.logging.LoggingFactory;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AsyncServletTest extends JerseyTest {
+    static {
+        LoggingFactory.bootstrap();
+    }
+
+    @Override
+    protected Application configure() {
+        return DropwizardResourceConfig.forTesting(new MetricRegistry())
+                .register(DummyResource.class);
+    }
+
+    @Test
+    public void testAsyncResponse() {
+        final Response response = target("/async").request(MediaType.TEXT_PLAIN_TYPE).get();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.readEntity(String.class)).isEqualTo("foobar");
+    }
+}
