8870ca6f18a2c2f24fe52cf334068fc36acd96eb
Pavan Gupta <pg8p@virginia.edu>
2015-01-28 13:29:15 -0500

    MultiPartBundle now accepts a config interface

--- dropwizard-forms/src/main/java/io/dropwizard/forms/MultiPartBundle.java
+++ dropwizard-forms/src/main/java/io/dropwizard/forms/MultiPartBundle.java
@@ -1,6 +1,7 @@
 package io.dropwizard.forms;
 
-import io.dropwizard.Bundle;
+import io.dropwizard.Configuration;
+import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
@@ -8,16 +9,18 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 /**
  * A {@link Bundle}, which enables the processing of multi-part form data by your application.
  *
+ * @param <T>    the required configuration interface
+ * 
  * @see org.glassfish.jersey.media.multipart.MultiPartFeature
  * @see <a href="https://jersey.java.net/documentation/latest/media.html#multipart">Jersey Multipart</a>
  */
-public class MultiPartBundle implements Bundle {
+public class MultiPartBundle<T extends Configuration> implements ConfiguredBundle<T> {
     @Override
     public void initialize(Bootstrap<?> bootstrap) {
     }
 
     @Override
-    public void run(Environment environment) {
-        environment.jersey().register(MultiPartFeature.class);
+    public final void run(T configuration, Environment environment) throws Exception {
+        environment.jersey().register(new MultiPartFeature());
     }
 }
