05172452f2ac16d565a7deb3f7b57c58cf803768
Glenn McAllister <gmcallister@proofpoint.com>
2015-02-23 22:57:57 -0500

    Rewrite the tests; previous set were incomplete
    So @joschi pointed out my tests were woefully incomplete, and rightly
    so. I’ve updated the set to incorporate his approach, while attempting
    to retain the concerns expresses by the original issue. With invalid or
    incorrect JSON we are not required to authenticate before being
    presented with a 400 error.
    
    Doing some digging, given that our auth approach does *not* use
    filters/interceptors, using the Priority annotation isn’t an easy
    solution. The abstract value factory approach we use kind of dumps us
    in the middle of the pipeline from what I can tell, but I’m no Jersey
    expert by any stretch; I’m happy to be corrected.
    
    We likely have to consider if there is a way where we can transform the
    current parameter approach transparently into a filter/interceptor
    approach.

--- dropwizard-auth/src/test/java/io/dropwizard/auth/basic/AuthBeforeValidateTest.java
+++ dropwizard-auth/src/test/java/io/dropwizard/auth/basic/AuthBeforeValidateTest.java
@@ -1,14 +1,12 @@
 package io.dropwizard.auth.basic;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -16,11 +14,12 @@ import javax.ws.rs.core.Response;
 
 import io.dropwizard.auth.Auth;
 import io.dropwizard.auth.AuthFactory;
-import io.dropwizard.auth.AuthResource;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.TransportObject;
 import io.dropwizard.jersey.DropwizardResourceConfig;
+import io.dropwizard.jersey.jackson.JsonProcessingExceptionMapper;
+import io.dropwizard.jersey.validation.ConstraintViolationExceptionMapper;
 import io.dropwizard.logging.LoggingFactory;
 
 import org.glassfish.jersey.servlet.ServletProperties;
@@ -54,68 +53,94 @@ public class AuthBeforeValidateTest extends JerseyTest {
     }
     
     @Test
-    public void doNotLeakAnyValidationInfoOnFailedAuthentication() {
-        try {
-            TransportObject to = new TransportObject();
-            Entity<TransportObject> entity = Entity.entity(to, MediaType.APPLICATION_JSON_TYPE);
-            target("/greet").request()
-                .header(HttpHeaders.AUTHORIZATION, "Basic Z29vZC1ndXk6c2VjcmV0")
-                .post(entity, String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            Response r = e.getResponse();
-            
-            assertThat(r.getStatus()).isEqualTo(401);
-            assertThat(r.getHeaders().get(HttpHeaders.WWW_AUTHENTICATE))
-                    .containsOnly("Basic realm=\"realm\""); 
-            assertThat(r.readEntity(String.class)).doesNotContain("may not be null");
-        }
+    public void authenticatedRequestSucceeds() throws Exception {
+        final TransportObject to = new TransportObject();
+        to.setGivenName("abba");
+        
+        final Response response = target("/test").request()
+            .header(HttpHeaders.AUTHORIZATION, "Basic Z29vZC1ndXk6c2VjcmV0")
+            .post(Entity.entity(to, MediaType.APPLICATION_JSON_TYPE));
+        
+        assertThat(response.readEntity(String.class)).isEqualTo("abba");
     }
     
     @Test
-    public void doNotLeakAnyValidationInfoOnMissingAuthentication() {
-        TransportObject to = new TransportObject();
-        try {
-            Entity<TransportObject> entity = Entity.entity(to, MediaType.APPLICATION_JSON_TYPE);
-            // Note that when the AUTHORIZATION header is missing on a protected resource, 
-            // we never even make it into the Authenticator instance, the client is presented
-            // with a 401 immediately.
-            target("/greet").request()
-                .post(entity, String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            Response r = e.getResponse();
-            
-            assertThat(r.getStatus()).isEqualTo(401);
-            assertThat(r.getHeaders().get(HttpHeaders.WWW_AUTHENTICATE))
-                    .containsOnly("Basic realm=\"realm\""); 
-            assertThat(r.readEntity(String.class)).doesNotContain("may not be null");
-        }
+    public void unauthenticatedRequestFailsWith401s() throws Exception {
+        final TransportObject to = new TransportObject();
+        to.setGivenName("abba");
+        
+        final Response response = target("/test").request()
+            .post(Entity.entity(to, MediaType.APPLICATION_JSON_TYPE));
+        
+        assertThat(response.getStatus()).isEqualTo(401);
+    }
+    
+    @Test
+    public void unauthenticatedRequestWithOptionalAuthSucceeds() throws Exception {
+        final TransportObject to = new TransportObject();
+        to.setGivenName("abba");
+        
+        final Response response = target("/test/optional").request()
+            .post(Entity.entity(to, MediaType.APPLICATION_JSON_TYPE));
+        
+        assertThat(response.readEntity(String.class)).isEqualTo("abba");
+    }
+    
+    @Test
+    public void authenticationMustHappenBeforeDeserialization() throws Exception {
+        final Response response = target("/test").request()
+            .post(Entity.entity("INVALID", MediaType.APPLICATION_JSON_TYPE));
+        
+        assertThat(response.getStatus()).isEqualTo(401);
+    }
+    
+    @Test
+    public void authenticationMustHappenBeforeValidation() throws Exception {
+        final Response response = target("/test").request()
+            .post(Entity.entity("{\"unknown\":123}", MediaType.APPLICATION_JSON_TYPE));
+        
+        assertThat(response.getStatus()).isEqualTo(401);
     }
     
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/greet")
+    @Path("/test")
     public static class ValidatedResource {
         
         @POST
-        public String greet(@Valid TransportObject validatedObject, @Auth String principal) {
-            return "hello";
+        public String show(@Valid TransportObject validatedObject, @Auth String principal) {
+            return validatedObject.getGivenName();
         }
+        
+        @POST
+        @Path("/optional")
+        public String optionalAuth(@Valid TransportObject validatedObject, @Auth(required = false) String principle) {
+            return validatedObject.getGivenName();
+        }
+    }
+    
+  
+    public static class BasicAuthenticator implements Authenticator<BasicCredentials, String> {
+
+        @Override
+        public Optional<String> authenticate(BasicCredentials credentials) throws AuthenticationException {
+            if ("good-guy".equals(credentials.getUsername()) &&
+                "secret".equals(credentials.getPassword())) {
+                return Optional.of("good-guy");
+            }
+            return Optional.absent();
+        }
+        
     }
     
     public static class AuthBeforeValidateTestResourceConfig extends DropwizardResourceConfig {
         public AuthBeforeValidateTestResourceConfig() {
             super(true, new MetricRegistry());
 
-            final Authenticator<BasicCredentials, String> denyAllAuthenticator = new Authenticator<BasicCredentials, String>() {
-                @Override
-                public Optional<String> authenticate(BasicCredentials credentials) throws AuthenticationException {
-                    // Deny everyone access
-                    return Optional.absent();
-                }
-            };
-            register(AuthFactory.binder(new BasicAuthFactory<>(denyAllAuthenticator, "realm", String.class)));
+            final Authenticator<BasicCredentials, String> basicAuthenticator = new BasicAuthenticator();
+            register(AuthFactory.binder(new BasicAuthFactory<>(basicAuthenticator, "realm", String.class)));
+            register(new ConstraintViolationExceptionMapper());
+            register(new JsonProcessingExceptionMapper(true));
             register(ValidatedResource.class);
         }
     }
