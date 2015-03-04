58c2ec5a2d0a46bb45101b18ca67b77bbfcac417
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-03-04 22:12:46 +0100

    Correct transcation commiting lifecycle
    Committing the transaction on RESP_FILTERS_START event
    instead of RESOURCE_METHOD_FINISHED.
    
    Fixes #850, closes #915

--- dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkApplicationListener.java
+++ dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkApplicationListener.java
@@ -78,7 +78,7 @@ public class UnitOfWorkApplicationListener implements ApplicationEventListener {
                     }
                 }
             }
-            else if (event.getType() == RequestEvent.Type.RESOURCE_METHOD_FINISHED) {
+            else if (event.getType() == RequestEvent.Type.RESP_FILTERS_START) {
                 if (this.session != null) {
                     try {
                         commitTransaction();
