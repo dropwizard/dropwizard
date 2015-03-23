b1153cdf355ab47370090897f56df5a350e74d9c
Artem Prigoda <arteamon@gmail.com>
2015-03-23 20:49:21 +0300

    Fix an example of IntegrationTest
    * The test can be executed from an IDE
    * Use a test, not a real configuration
    * Dynamically assign application and admin ports
    * Close an HTTP client in the test
    * Replace tabs to spaces

--- dropwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
+++ dropwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
@@ -2,32 +2,32 @@ package com.example.helloworld;
 
 import com.example.helloworld.core.Saying;
 import com.google.common.base.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class IntegrationTest {
-	@ClassRule
-	public static final DropwizardAppRule<HelloWorldConfiguration> RULE =
-		new DropwizardAppRule<HelloWorldConfiguration>(HelloWorldApplication.class, "example.yml");
-	
-	@Test
-	public void testHelloWorld() throws Exception {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:" + RULE.getLocalPort() + "/hello-world");
-		
-		final Optional<String> NAME = Optional.fromNullable("Dr. IntegrationTest");
-		
-		Saying saying = target.queryParam("name", NAME.get()).request().get(Saying.class);
-		
-		assertThat(saying.getContent()).isEqualTo(RULE.getConfiguration().buildTemplate().render(NAME));
-	}
+
+    @ClassRule
+    public static final DropwizardAppRule<HelloWorldConfiguration> RULE = new DropwizardAppRule<>(
+            HelloWorldApplication.class, ResourceHelpers.resourceFilePath("test-example.yml"));
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        final Optional<String> name = Optional.fromNullable("Dr. IntegrationTest");
+        final Client client = ClientBuilder.newClient();
+        final Saying saying = client.target("http://localhost:" + RULE.getLocalPort() + "/hello-world")
+                .queryParam("name", name.get())
+                .request()
+                .get(Saying.class);
+        assertThat(saying.getContent()).isEqualTo(RULE.getConfiguration().buildTemplate().render(name));
+        client.close();
+    }
 }
