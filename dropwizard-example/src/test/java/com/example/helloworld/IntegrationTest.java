c22648eaebea26dbd4665865ce65bc3521881ed9
Artem Prigoda <arteamon@gmail.com>
2015-03-23 21:38:42 +0300

    Add an integration test for work with a database
    * Create a temporary folder
    * Override the DW database config
    * Migrate the DB schema
    * Test that database-related resource is working

--- dropwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
+++ dropwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
@@ -1,33 +1,76 @@
 package com.example.helloworld;
 
+import com.example.helloworld.core.Person;
 import com.example.helloworld.core.Saying;
 import com.google.common.base.Optional;
+import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class IntegrationTest {
+
+    private static final String TMP_FILE = createTempFile();
+    private static final String CONFIG_PATH = ResourceHelpers.resourceFilePath("test-example.yml");
 
     @ClassRule
     public static final DropwizardAppRule<HelloWorldConfiguration> RULE = new DropwizardAppRule<>(
-            HelloWorldApplication.class, ResourceHelpers.resourceFilePath("test-example.yml"));
+            HelloWorldApplication.class, CONFIG_PATH,
+            ConfigOverride.config("database.url", "jdbc:h2:" + TMP_FILE));
+
+    private Client client;
+
+    @BeforeClass
+    public static void migrateDb() throws Exception {
+        RULE.getApplication().run("db", "migrate", CONFIG_PATH);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        client = ClientBuilder.newClient();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        client.close();
+    }
+
+    private static String createTempFile() {
+        try {
+            return File.createTempFile("test-example", null).getAbsolutePath();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @Test
     public void testHelloWorld() throws Exception {
         final Optional<String> name = Optional.fromNullable("Dr. IntegrationTest");
-        final Client client = ClientBuilder.newClient();
         final Saying saying = client.target("http://localhost:" + RULE.getLocalPort() + "/hello-world")
                 .queryParam("name", name.get())
                 .request()
                 .get(Saying.class);
         assertThat(saying.getContent()).isEqualTo(RULE.getConfiguration().buildTemplate().render(name));
-        client.close();
+    }
+
+    @Test
+    public void testPostPerson() throws Exception {
+        final Person person = new Person("Dr. IntegrationTest", "Chief Wizard");
+        final Person newPerson = client.target("http://localhost:" + RULE.getLocalPort() + "/people")
+                .request()
+                .post(Entity.entity(person, MediaType.APPLICATION_JSON_TYPE))
+                .readEntity(Person.class);
+        assertThat(newPerson.getId()).isNotNull();
+        assertThat(newPerson.getFullName()).isEqualTo(person.getFullName());
+        assertThat(newPerson.getJobTitle()).isEqualTo(person.getJobTitle());
     }
 }
