b8e31c6fe21a68d704eb174f29756861772fdc9c
Artem Prigoda <aprigoda@bcc.ru>
2015-04-12 01:16:26 +0300

    Add a benchmark for Size
    The benchmark measures an average time to parse a string
    to a Size representation.

---
+++ dropwizard-benchmarks/src/io/dropwizard/benchmarks/util/SizeBenchmark.java
@@ -0,0 +1,34 @@
+package io.dropwizard.benchmarks.util;
+
+import io.dropwizard.util.Size;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class SizeBenchmark {
+
+    /**
+     * Don't trust the IDE, it's advisedly non-final to avoid constant folding
+     */
+    private String size = "256KiB";
+
+    @Benchmark
+    public Size parseSize() {
+        return Size.parse(size);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Runner(new OptionsBuilder()
+                .include(SizeBenchmark.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .build())
+                .run();
+    }
+}
