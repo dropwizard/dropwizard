f1d576c03f64bf1c3493ab209ca642f9752598a3
Artem Prigoda <arteamon@gmail.com>
2015-04-21 22:58:22 +0300

    Defer DNS resolution in GraphiteReporter
    InetSockedAddress performs DNS resolution during
    construction time, which is not great if the Graphite server
    moves to a different IP-address.
    
    The solution is to initialize Graphite gateway with a hostname
    and port. In such configuration it will resolve an address just
    before connecting to the server.
    
    Resolve #1003

--- dropwizard-metrics-graphite/src/test/java/io/dropwizard/metrics/graphite/GraphiteReporterFactoryTest.java
+++ dropwizard-metrics-graphite/src/test/java/io/dropwizard/metrics/graphite/GraphiteReporterFactoryTest.java
@@ -1,14 +1,49 @@
 package io.dropwizard.metrics.graphite;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 import io.dropwizard.jackson.DiscoverableSubtypeResolver;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class GraphiteReporterFactoryTest {
+
     @Test
     public void isDiscoverable() throws Exception {
         assertThat(new DiscoverableSubtypeResolver().getDiscoveredSubtypes())
                 .contains(GraphiteReporterFactory.class);
+    }
+
+    @Test
+    public void testNoAddressResolutionForGraphite() throws Exception {
+        final GraphiteReporter.Builder builderSpy = mock(GraphiteReporter.Builder.class);
+        new GraphiteReporterFactory() {
+            @Override
+            protected GraphiteReporter.Builder builder(MetricRegistry registry) {
+                return builderSpy;
+            }
+        }.build(new MetricRegistry());
+
+        final ArgumentCaptor<Graphite> argument = ArgumentCaptor.forClass(Graphite.class);
+        verify(builderSpy).build(argument.capture());
+
+        final Graphite graphite = argument.getValue();
+        assertThat(getField(graphite, "hostname")).isEqualTo("localhost");
+        assertThat(getField(graphite, "port")).isEqualTo(8080);
+        assertThat(getField(graphite, "address")).isNull();
+    }
+
+    private static Object getField(Graphite graphite, String name) {
+        try {
+            return FieldUtils.getDeclaredField(Graphite.class, name, true).get(graphite);
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
