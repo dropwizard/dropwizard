8b2cb570c286ea840e449bf4a0e0470ef3599aa5
Nick Babcock <nbabcock19@hotmail.com>
2015-05-01 12:05:10 -0400

    Extract constraint status for public use
    Useful for those who want to derive their own `ConstraintExceptionMapper`

--- dropwizard-jersey/src/main/java/io/dropwizard/jersey/validation/ConstraintViolationExceptionMapper.java
+++ dropwizard-jersey/src/main/java/io/dropwizard/jersey/validation/ConstraintViolationExceptionMapper.java
@@ -1,46 +1,18 @@
 package io.dropwizard.jersey.validation;
 
-import javax.validation.ConstraintViolation;
+import io.dropwizard.validation.ConstraintViolations;
+
 import javax.validation.ConstraintViolationException;
-import javax.validation.ElementKind;
-import javax.validation.Path.Node;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.Set;
 
 @Provider
 public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
     @Override
     public Response toResponse(ConstraintViolationException exception) {
-        int status = 422;
-
-        // Detect where the constraint validation occurred so we can return an appropriate status
-        // code. If the constraint failed with a *Param annotation, return a bad request. If it
-        // failed validating the return value, return internal error. Else return unprocessable
-        // entity.
-        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
-        if (violations.size() > 0) {
-            ConstraintViolation<?> violation = violations.iterator().next();
-            boolean isReturnValue = false;
-            boolean isArgument = false;
-
-            // A return value can only occur at the last path, but a parameter
-            // can occur anywhere, such as a @BeanParam that has validations.
-            for (Node node : violation.getPropertyPath()) {
-                isArgument |= node.getKind().equals(ElementKind.PARAMETER);
-                isReturnValue = node.getKind().equals(ElementKind.RETURN_VALUE);
-            }
-
-            if (isReturnValue) {
-                status = 500;
-            } else if (isArgument) {
-                status = 400;
-            }
-        }
-
         final ValidationErrorMessage message = new ValidationErrorMessage(exception.getConstraintViolations());
-        return Response.status(status)
+        return Response.status(ConstraintViolations.determineStatus(exception))
                        .entity(message)
                        .build();
     }
--- dropwizard-validation/src/main/java/io/dropwizard/validation/ConstraintViolations.java
+++ dropwizard-validation/src/main/java/io/dropwizard/validation/ConstraintViolations.java
@@ -7,6 +7,8 @@ import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ElementKind;
 import javax.validation.Path;
 import java.util.Set;
 
@@ -48,5 +50,33 @@ public class ConstraintViolations {
             builder.add(violation);
         }
         return builder.build();
+    }
+
+    public static int determineStatus(ConstraintViolationException exception) {
+        // Detect where the constraint validation occurred so we can return an appropriate status
+        // code. If the constraint failed with a *Param annotation, return a bad request. If it
+        // failed validating the return value, return internal error. Else return unprocessable
+        // entity.
+        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
+        if (violations.size() > 0) {
+            ConstraintViolation<?> violation = violations.iterator().next();
+            boolean isReturnValue = false;
+            boolean isArgument = false;
+
+            // A return value can only occur at the last path, but a parameter
+            // can occur anywhere, such as a @BeanParam that has validations.
+            for (Path.Node node : violation.getPropertyPath()) {
+                isArgument |= node.getKind().equals(ElementKind.PARAMETER);
+                isReturnValue = node.getKind().equals(ElementKind.RETURN_VALUE);
+            }
+
+            if (isReturnValue) {
+                return 500;
+            } else if (isArgument) {
+                return 400;
+            }
+        }
+
+        return 422;
     }
 }
