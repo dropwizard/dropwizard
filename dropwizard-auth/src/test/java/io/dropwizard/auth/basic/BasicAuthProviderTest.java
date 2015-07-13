31467ddf63b67af39e31231bf59f694fa4c35271
Carlo <carlo@renttherunway.com>
2015-07-13 16:37:17 -0400

    make the auth tests DRY

--- dropwizard-auth/src/test/java/io/dropwizard/auth/basic/BasicAuthProviderTest.java
+++ dropwizard-auth/src/test/java/io/dropwizard/auth/basic/BasicAuthProviderTest.java
@@ -1,187 +1,49 @@
 package io.dropwizard.auth.basic;
 
-import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.ImmutableList;
-import io.dropwizard.auth.AuthDynamicFeature;
-import io.dropwizard.auth.AuthResource;
-import io.dropwizard.auth.AuthValueFactoryProvider;
-import io.dropwizard.auth.PrincipalImpl;
+import io.dropwizard.auth.*;
 import io.dropwizard.auth.util.AuthUtil;
 import io.dropwizard.jersey.DropwizardResourceConfig;
-import io.dropwizard.logging.BootstrapLogging;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-import org.glassfish.jersey.servlet.ServletProperties;
-import org.glassfish.jersey.test.DeploymentContext;
-import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.ServletDeploymentContext;
-import org.glassfish.jersey.test.TestProperties;
-import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
-import org.glassfish.jersey.test.spi.TestContainerException;
-import org.glassfish.jersey.test.spi.TestContainerFactory;
-import org.junit.Test;
-
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.HttpHeaders;
 import java.security.Principal;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
-
-public class BasicAuthProviderTest extends JerseyTest {
-    private final static String ADMIN_ROLE = "ADMIN";
-
-    static {
-        BootstrapLogging.bootstrap();
-    }
-
-    @Override
-    protected TestContainerFactory getTestContainerFactory()
-            throws TestContainerException {
-        return new GrizzlyWebTestContainerFactory();
-    }
-
-    @Override
-    protected DeploymentContext configureDeployment() {
-        forceSet(TestProperties.CONTAINER_PORT, "0");
-        return ServletDeploymentContext.builder(new BasicAuthTestResourceConfig())
-                .initParam(ServletProperties.JAXRS_APPLICATION_CLASS, BasicAuthTestResourceConfig.class.getName())
-                .build();
-    }
-
-    @Test
-    public void respondsToMissingCredentialsWith401() throws Exception {
-        try {
-            target("/test/admin").request().get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(401);
-            assertThat(e.getResponse().getHeaders().get(HttpHeaders.WWW_AUTHENTICATE))
-                    .containsOnly("Basic realm=\"realm\"");
-        }
-    }
-
-    @Test
-    public void resourceWithoutAuth200() {
-        assertThat(target("/test/noauth").request()
-                .get(String.class))
-                .isEqualTo("hello");
-    }
-
-    @Test
-    public void resourceWithAuthenticationWithoutAuthorizationWithCorrectCredentials200() {
-        assertThat(target("/test/profile").request()
-                .header(HttpHeaders.AUTHORIZATION, "Basic b3JkaW5hcnktZ3V5OnNlY3JldA==")
-                .get(String.class))
-                .isEqualTo("'ordinary-guy' has user privileges");
-    }
-
-    @Test
-    public void resourceWithAuthenticationWithoutAuthorizationNoCredentials401() {
-        try {
-            target("/test/profile").request().get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(401);
-            assertThat(e.getResponse().getHeaders().get(HttpHeaders.WWW_AUTHENTICATE))
-                    .containsOnly("Basic realm=\"realm\"");
-        }
-    }
-
-    @Test
-    public void resourceWithAuthorizationPrincipalIsNotAuthorized403() {
-        try {
-            target("/test/admin").request()
-                    .header(HttpHeaders.AUTHORIZATION, "Basic b3JkaW5hcnktZ3V5OnNlY3JldA==")
-                    .get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(403);
-        }
-    }
-
-    @Test
-    public void resourceWithDenyAllAndNoAuth401() {
-        try {
-            target("/test/denied").request().get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(401);
-        }
-    }
-
-    @Test
-    public void resourceWithDenyAllAndAuth403() {
-        try {
-            target("/test/denied").request()
-                    .header(HttpHeaders.AUTHORIZATION, "Basic Z29vZC1ndXk6c2VjcmV0")
-                    .get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(403);
-        }
-    }
-
-    @Test
-    public void transformsCredentialsToPrincipals() throws Exception {
-        assertThat(target("/test/admin").request()
-                .header(HttpHeaders.AUTHORIZATION, "Basic Z29vZC1ndXk6c2VjcmV0")
-                .get(String.class))
-                .isEqualTo("'good-guy' has admin privileges");
-    }
-
-    @Test
-    public void transformsCredentialsToPrincipalsWhenAuthAnnotationExistsWithoutMethodAnnotation() throws Exception {
-        assertThat(target("/test/implicit-permitall").request()
-            .header(HttpHeaders.AUTHORIZATION, "Basic Z29vZC1ndXk6c2VjcmV0")
-            .get(String.class))
-            .isEqualTo("'good-guy' has user privileges");
-    }
-
-    @Test
-    public void respondsToNonBasicCredentialsWith401() throws Exception {
-        try {
-            target("/test/admin").request()
-                    .header(HttpHeaders.AUTHORIZATION, "Derp Z29vZC1ndXk6c2VjcmV0")
-                    .get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(401);
-            assertThat(e.getResponse().getHeaders().get(HttpHeaders.WWW_AUTHENTICATE))
-                    .containsOnly("Basic realm=\"realm\"");
-        }
-    }
-
-    @Test
-    public void respondsToExceptionsWith500() throws Exception {
-        try {
-            target("/test/admin").request()
-                    .header(HttpHeaders.AUTHORIZATION, "Basic YmFkLWd1eTpzZWNyZXQ=")
-                    .get(String.class);
-            failBecauseExceptionWasNotThrown(WebApplicationException.class);
-        } catch (WebApplicationException e) {
-            assertThat(e.getResponse().getStatus()).isEqualTo(500);
-        }
-    }
-
-    public static class BasicAuthTestResourceConfig extends DropwizardResourceConfig {
-        public BasicAuthTestResourceConfig() {
-            super(true, new MetricRegistry());
-
-            register(new AuthDynamicFeature(getAuthFilter()));
-            register(new AuthValueFactoryProvider.Binder(Principal.class));
-            register(RolesAllowedDynamicFeature.class);
-            register(AuthResource.class);
-        }
-
-        private ContainerRequestFilter getAuthFilter() {
-            final String adminUser = "good-guy";
-            final String ordinaryUser = "ordinary-guy";
-
+public class BasicAuthProviderTest extends AuthBaseTest<BasicAuthProviderTest.BasicAuthTestResourceConfig> {
+    public static class BasicAuthTestResourceConfig extends AuthBaseResourceConfig{
+        protected ContainerRequestFilter getAuthFilter() {
             BasicCredentialAuthFilter.Builder<Principal> builder = new BasicCredentialAuthFilter.Builder<>();
-            builder.setAuthorizer(AuthUtil.getTestAuthorizer(adminUser, ADMIN_ROLE));
-            builder.setAuthenticator(AuthUtil.getBasicAuthenticator(ImmutableList.of(adminUser, ordinaryUser)));
+            builder.setAuthorizer(AuthUtil.getTestAuthorizer(ADMIN_USER, ADMIN_ROLE));
+            builder.setAuthenticator(AuthUtil.getBasicAuthenticator(ImmutableList.of(ADMIN_USER, ORDINARY_USER)));
             return builder.buildAuthFilter();
         }
+    }
+
+    @Override
+    protected DropwizardResourceConfig getDropwizardResourceConfig() {
+        return new BasicAuthTestResourceConfig();
+    }
+
+    @Override
+    protected Class<BasicAuthTestResourceConfig> getDropwizardResourceConfigClass() {
+        return BasicAuthTestResourceConfig.class;
+    }
+
+    @Override
+    protected String getPrefix() {
+        return BASIC_PREFIX;
+    }
+
+    @Override
+    protected String getOrdinaryGuyValidToken() {
+        return ORDINARY_USER_ENCODED_TOKEN;
+    }
+
+    @Override
+    protected String getGoodGuyValidToken() {
+        return GOOD_USER_ENCODED_TOKEN;
+    }
+
+    @Override
+    protected String getBadGuyToken() {
+        return BAD_USER_ENCODED_TOKEN;
     }
 }
