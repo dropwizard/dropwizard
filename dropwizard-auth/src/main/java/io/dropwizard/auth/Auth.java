cc2b4326be244c8f5aa4b863da253f26d001b22c
Carlo <carlo@renttherunway.com>
2015-08-31 10:55:18 -0400

    remove method element type

--- dropwizard-auth/src/main/java/io/dropwizard/auth/Auth.java
+++ dropwizard-auth/src/main/java/io/dropwizard/auth/Auth.java
@@ -7,6 +7,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
 public @interface Auth {
 }
