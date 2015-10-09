04e4c98070ab2da88d82eab7aa7600940278bcce
maffe <github.com-6h11@xnfz.eu>
2015-10-09 15:34:27 +0200

    compile error fix

--- dropwizard-jersey/src/main/java/io/dropwizard/jersey/errors/LoggingExceptionMapper.java
+++ dropwizard-jersey/src/main/java/io/dropwizard/jersey/errors/LoggingExceptionMapper.java
@@ -27,7 +27,7 @@ public abstract class LoggingExceptionMapper<E extends Throwable> implements Exc
             status = response.getStatus();
             errorMessage = new ErrorMessage(status, exception.getLocalizedMessage());
         } else {
-            final long id = logException(id, exception);
+            final long id = logException(exception);
             status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
             errorMessage = new ErrorMessage(formatErrorMessage(id, exception));
         }
