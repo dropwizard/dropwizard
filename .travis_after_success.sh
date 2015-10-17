8f1dfb43af24f7ccc2f41a71badd0940dd7c5bdd
Jochen Schalanda <jochen@schalanda.name>
2015-10-17 15:20:15 +0200

    Skip unnecessary tasks for artifact deployment in Travis CI

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -9,7 +9,7 @@ mvn -B cobertura:cobertura coveralls:report
 
 # Do not deploy SNAPSHOT artifacts for PRs
 if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-    mvn -B deploy --settings maven_deploy_settings.xml
+    mvn -B deploy --settings maven_deploy_settings.xml -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dsource.skip=true -Dfindbugs.skip=true
 else
     echo 'Pull request, skipping deploy.'
 fi
