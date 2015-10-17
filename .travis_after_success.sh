bf49c80e0abaf4c6a9a3e758a9e38f58c428dc30
Jochen Schalanda <jochen@schalanda.name>
2015-10-17 14:28:27 +0200

    Fix .travis_after_success.sh script

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Only update coveralls after the JDK8 build.
-if [[ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ]]; then
-    echo "Running Coveralls for JDK version: ${TRAVIS_JDK_VERSION}"
-    mvn cobertura:cobertura coveralls:report
-else
-    echo "Skipping coveralls for JDK version: ${TRAVIS_JDK_VERSION}"
+
+if [[ "${TRAVIS_JDK_VERSION}" != "oraclejdk8" ]]; then
+    echo "Skipping after_success actions for JDK version ${TRAVIS_JDK_VERSION}"
+    exit
 fi
 
-# Only deploy SNAPSHOT artifacts after the JDK8 build
-if [[ "$TRAVIS_PULL_REQUEST" = "false" -a "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ]]; then
-    mvn deploy --settings maven_deploy_settings.xml
+mvn -B cobertura:cobertura coveralls:report
+
+# Do not deploy SNAPSHOT artifacts for PRs
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    mvn -B deploy --settings maven_deploy_settings.xml
 else
     echo 'Pull request, skipping deploy.'
 fi
