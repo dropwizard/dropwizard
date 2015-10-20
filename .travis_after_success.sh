c68100b53c1591ee92a9ed32c5df8f8127548e69
Jochen Schalanda <jochen@schalanda.name>
2015-10-20 12:14:21 +0200

    Skip deployment of tags and non-master branches in Travis CI

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 if [[ "${TRAVIS_JDK_VERSION}" != "oraclejdk8" ]]; then
-    echo "Skipping after_success actions for JDK version ${TRAVIS_JDK_VERSION}"
+    echo "Skipping after_success actions for JDK version \"${TRAVIS_JDK_VERSION}\""
     exit
 fi
 
 mvn -B cobertura:cobertura coveralls:report
 
-# Do not deploy SNAPSHOT artifacts for PRs
-if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-    mvn -B deploy --settings maven_deploy_settings.xml -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dsource.skip=true -Dfindbugs.skip=true
-else
-    echo 'Pull request, skipping deploy.'
+if [[ -n ${TRAVIS_TAG} ]]; then
+    echo "Skipping deployment for tag \"${TRAVIS_TAG}\""
+    exit
 fi
+
+if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
+    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
+    exit
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+    echo "Skipping deployment for pull request"
+    exit
+fi
+
+mvn -B deploy --settings maven_deploy_settings.xml -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dsource.skip=true -Dfindbugs.skip=true
