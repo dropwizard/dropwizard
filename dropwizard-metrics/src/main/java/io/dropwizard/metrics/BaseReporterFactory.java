bf687c63cfacd9ee8e7e2f09dcadc0c35d81e0bd
Jochen Schalanda <jochen@schalanda.name>
2015-10-31 13:22:06 +0100

    Check validity of BaseReporterFactory.frequency

--- dropwizard-metrics/src/main/java/io/dropwizard/metrics/BaseReporterFactory.java
+++ dropwizard-metrics/src/main/java/io/dropwizard/metrics/BaseReporterFactory.java
@@ -3,6 +3,7 @@ package io.dropwizard.metrics;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.ScheduledReporter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Optional;
 import com.google.common.cache.CacheBuilder;
@@ -10,6 +11,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableSet;
 import io.dropwizard.util.Duration;
+import io.dropwizard.validation.MinDuration;
 import org.hibernate.validator.valuehandling.UnwrapValidatedValue;
 
 import javax.validation.Valid;
@@ -86,9 +88,9 @@ public abstract class BaseReporterFactory implements ReporterFactory {
     @NotNull
     private ImmutableSet<String> includes = ImmutableSet.of();
 
-    @NotNull
     @Valid
-    @UnwrapValidatedValue(false)
+    @MinDuration(0)
+    @UnwrapValidatedValue
     private Optional<Duration> frequency = Optional.absent();
 
     private boolean useRegexFilters = false;
@@ -173,6 +175,7 @@ public abstract class BaseReporterFactory implements ReporterFactory {
      * @see #getIncludes()
      * @see #getExcludes()
      */
+    @JsonIgnore
     public MetricFilter getFilter() {
         final StringMatchingStrategy stringMatchingStrategy = getUseRegexFilters() ?
                 REGEX_STRING_MATCHING_STRATEGY : DEFAULT_STRING_MATCHING_STRATEGY;
