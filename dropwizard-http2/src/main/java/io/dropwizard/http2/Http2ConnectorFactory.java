665fcdb734b066e875c9dd8ee7314c7e83b091bb
Artem Prigoda <arteamon@gmail.com>
2015-11-19 15:22:32 +0300

    Support for HTTP/2 connector additional parameters
    Add the ability to configure *maxConcurrentStreams* and
    *initialStreamSendWindow*. Unfortunately, there is no way we can
    reuse the code and docs  between connectors, so we just duplicate
    them.

--- dropwizard-http2/src/main/java/io/dropwizard/http2/Http2ConnectorFactory.java
+++ dropwizard-http2/src/main/java/io/dropwizard/http2/Http2ConnectorFactory.java
@@ -1,6 +1,7 @@
 package io.dropwizard.http2;
 
 import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.dropwizard.jetty.HttpsConnectorFactory;
 import io.dropwizard.jetty.Jetty93InstrumentedConnectionFactory;
@@ -13,11 +14,61 @@ import org.eclipse.jetty.util.thread.ThreadPool;
 
 /**
  * Builds HTTP/2 over TLS connectors.
+ * <p/>
+ * <b>Configuration Parameters:</b>
+ * <table>
+ *     <tr>
+ *         <td>Name</td>
+ *         <td>Default</td>
+ *         <td>Description</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code maxConcurrentStreams}</td>
+ *         <td><1024</td>
+ *         <td>
+ *             The maximum number of concurrently open streams allowed on a single HTTP/2 connection.
+ *             Larger values increase parallelism, but cost a memory commitment.
+ *         </td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@code initialStreamSendWindow}</td>
+ *         <td>65535</td>
+ *         <td>
+ *             The initial flow control window size for a new stream. Larger values may allow greater throughput,
+ *             but also risk head of line blocking if TCP/IP flow control is triggered.
+ *         </td>
+ *     </tr>
+ * </table>
+ * <p/>
+ * For more configuration parameters, see {@link HttpsConnectorFactory}.
  *
  * @see HttpsConnectorFactory
  */
 @JsonTypeName("http2")
 public class Http2ConnectorFactory extends HttpsConnectorFactory {
+
+    private int maxConcurrentStreams = 1024;
+    private int initialStreamSendWindow = 65535;
+
+    @JsonProperty
+    public int getMaxConcurrentStreams() {
+        return maxConcurrentStreams;
+    }
+
+    @JsonProperty
+    public void setMaxConcurrentStreams(int maxConcurrentStreams) {
+        this.maxConcurrentStreams = maxConcurrentStreams;
+    }
+
+    @JsonProperty
+    public int getInitialStreamSendWindow() {
+        return initialStreamSendWindow;
+    }
+
+    @JsonProperty
+    public void setInitialStreamSendWindow(int initialStreamSendWindow) {
+        this.initialStreamSendWindow = initialStreamSendWindow;
+    }
 
     /**
      * Supported protocols
@@ -34,7 +85,10 @@ public class Http2ConnectorFactory extends HttpsConnectorFactory {
         final HttpConfiguration httpConfig = buildHttpConfiguration();
         final HttpConnectionFactory http1 = buildHttpConnectionFactory(httpConfig);
         final HTTP2ServerConnectionFactory http2 = new HTTP2ServerConnectionFactory(httpConfig);
-        final NegotiatingServerConnectionFactory alpn =  new ALPNServerConnectionFactory(H2, H2_17);
+        http2.setMaxConcurrentStreams(maxConcurrentStreams);
+        http2.setInitialStreamSendWindow(initialStreamSendWindow);
+
+        final NegotiatingServerConnectionFactory alpn = new ALPNServerConnectionFactory(H2, H2_17);
         alpn.setDefaultProtocol(HTTP_1_1); // Speak HTTP 1.1 over TLS if negotiation fails
 
         final SslContextFactory sslContextFactory = buildSslContextFactory();
