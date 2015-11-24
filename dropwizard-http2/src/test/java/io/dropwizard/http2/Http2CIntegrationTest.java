518e74c79e7f4e698f9eec5bf65b4e8765d1b115
Artem Prigoda <arteamon@gmail.com>
2015-11-24 11:09:58 +0300

    Use a high-level Jetty client for testing HTTP/2 API
    Jetty 9 provides an HTTP client with a nice synchronous/asynchronous
    API to which we could plug-in the HTTP/2 transport.
    
    Using it makes tests more readable and convenient for maintaining.

--- dropwizard-http2/src/test/java/io/dropwizard/http2/Http2CIntegrationTest.java
+++ dropwizard-http2/src/test/java/io/dropwizard/http2/Http2CIntegrationTest.java
@@ -1,20 +1,19 @@
 package io.dropwizard.http2;
 
+import com.google.common.base.Charsets;
 import com.google.common.net.HttpHeaders;
 import io.dropwizard.Configuration;
 import io.dropwizard.logging.BootstrapLogging;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.eclipse.jetty.http.HttpFields;
-import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Result;
+import org.eclipse.jetty.client.util.BufferingResponseListener;
 import org.eclipse.jetty.http.HttpVersion;
-import org.eclipse.jetty.http.MetaData;
-import org.eclipse.jetty.http2.api.Session;
-import org.eclipse.jetty.http2.api.server.ServerSessionListener;
 import org.eclipse.jetty.http2.client.HTTP2Client;
-import org.eclipse.jetty.http2.frames.HeadersFrame;
-import org.eclipse.jetty.util.FuturePromise;
-import org.eclipse.jetty.util.Promise;
+import org.eclipse.jetty.http2.client.HTTP2ClientConnectionFactory;
+import org.eclipse.jetty.http2.client.http.HttpClientTransportOverHTTP2;
 import org.glassfish.jersey.client.JerseyClient;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.After;
@@ -24,7 +23,6 @@ import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.InetSocketAddress;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -40,10 +38,13 @@ public class Http2CIntegrationTest {
     public DropwizardAppRule<Configuration> appRule = new DropwizardAppRule<>(
             FakeApplication.class, ResourceHelpers.resourceFilePath("test-http2c.yml"));
 
-    private final HTTP2Client client = new HTTP2Client();
+    private HttpClient client;
 
     @Before
     public void setUp() throws Exception {
+        final HTTP2Client http2Client = new HTTP2Client();
+        http2Client.setClientConnectionFactory(new HTTP2ClientConnectionFactory()); // No need for ALPN
+        client = new HttpClient(new HttpClientTransportOverHTTP2(http2Client), null);
         client.start();
     }
 
@@ -67,42 +68,38 @@ public class Http2CIntegrationTest {
 
     @Test
     public void testHttp2c() throws Exception {
-        final String hostname = "127.0.0.1";
+        final String hostname = "localhost";
         final int port = appRule.getLocalPort();
 
-        final FuturePromise<Session> sessionPromise = new FuturePromise<>();
-        client.connect(new InetSocketAddress(hostname, port), new ServerSessionListener.Adapter(), sessionPromise);
-        final Session session = sessionPromise.get(5, TimeUnit.SECONDS);
-
-        final MetaData.Request request = new MetaData.Request("GET",
-                new HttpURI("http://" + hostname + ":" + port + "/api/test"),
-                HttpVersion.HTTP_2, new HttpFields());
-        CountDownLatch latch = new CountDownLatch(1);
-        session.newStream(new HeadersFrame(request, null, true), new Promise.Adapter<>(), new ResponseListener(latch));
-
-        latch.await(5, TimeUnit.SECONDS);
-        assertThat(latch.getCount()).isEqualTo(0);
+        final ContentResponse response = client.GET("http://" + hostname + ":" + port + "/api/test");
+        assertThat(response.getVersion()).isEqualTo(HttpVersion.HTTP_2);
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).isEqualTo(FakeApplication.HELLO_WORLD);
     }
 
     @Test
     public void testHttp2cManyRequests() throws Exception {
-        final String hostname = "127.0.0.1";
+        final String hostname = "localhost";
         final int port = appRule.getLocalPort();
 
-        final FuturePromise<Session> sessionPromise = new FuturePromise<>();
-        client.connect(new InetSocketAddress(hostname, port), new ServerSessionListener.Adapter(), sessionPromise);
-        final Session session = sessionPromise.get(5, TimeUnit.SECONDS);
+        // For some reason the library requires to perform the first request synchronously with HTTP/2
+        testHttp2c();
 
-        final MetaData.Request request = new MetaData.Request("GET",
-                new HttpURI("http://" + hostname + ":" + port + "/api/test"),
-                HttpVersion.HTTP_2, new HttpFields());
-        int amount = 100;
+        final int amount = 100;
         final CountDownLatch latch = new CountDownLatch(amount);
         for (int i = 0; i < amount; i++) {
-            session.newStream(new HeadersFrame(request, null, true), new Promise.Adapter<>(),
-                    new ResponseListener(latch));
+            client.newRequest("http://" + hostname + ":" + port + "/api/test")
+                    .send(new BufferingResponseListener() {
+                        @Override
+                        public void onComplete(Result result) {
+                            assertThat(result.getResponse().getVersion()).isEqualTo(HttpVersion.HTTP_2);
+                            assertThat(result.getResponse().getStatus()).isEqualTo(200);
+                            assertThat(getContentAsString(Charsets.UTF_8)).isEqualTo(FakeApplication.HELLO_WORLD);
+                            latch.countDown();
+                        }
+                    });
         }
-        latch.await(10, TimeUnit.SECONDS);
-        assertThat(latch.getCount()).isEqualTo(0);
+
+        assertThat(latch.await(5, TimeUnit.SECONDS)).isTrue();
     }
 }
--- dropwizard-http2/src/test/java/io/dropwizard/http2/Http2IntegrationTest.java
+++ dropwizard-http2/src/test/java/io/dropwizard/http2/Http2IntegrationTest.java
@@ -1,5 +1,6 @@
 package io.dropwizard.http2;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
 import com.google.common.net.HttpHeaders;
 import io.dropwizard.Configuration;
@@ -7,16 +8,13 @@ import io.dropwizard.logging.BootstrapLogging;
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.eclipse.jetty.http.HttpFields;
-import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Result;
+import org.eclipse.jetty.client.util.BufferingResponseListener;
 import org.eclipse.jetty.http.HttpVersion;
-import org.eclipse.jetty.http.MetaData;
-import org.eclipse.jetty.http2.api.Session;
-import org.eclipse.jetty.http2.api.server.ServerSessionListener;
 import org.eclipse.jetty.http2.client.HTTP2Client;
-import org.eclipse.jetty.http2.frames.HeadersFrame;
-import org.eclipse.jetty.util.FuturePromise;
-import org.eclipse.jetty.util.Promise;
+import org.eclipse.jetty.http2.client.http.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.glassfish.jersey.client.JerseyClient;
 import org.glassfish.jersey.client.JerseyClientBuilder;
@@ -27,7 +25,6 @@ import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.InetSocketAddress;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -46,9 +43,8 @@ public class Http2IntegrationTest {
             ConfigOverride.config("tls_http2", "server.connector.keyStorePath",
                     ResourceHelpers.resourceFilePath("stores/http2_server.jks"))
     );
-
-    private final HTTP2Client client = new HTTP2Client();
     private final SslContextFactory sslContextFactory = new SslContextFactory();
+    private HttpClient client;
 
     @Before
     public void setUp() throws Exception {
@@ -57,7 +53,7 @@ public class Http2IntegrationTest {
         sslContextFactory.setIncludeCipherSuites("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
         sslContextFactory.start();
 
-        client.addBean(sslContextFactory);
+        client = new HttpClient(new HttpClientTransportOverHTTP2(new HTTP2Client()), sslContextFactory);
         client.start();
     }
 
@@ -86,19 +82,10 @@ public class Http2IntegrationTest {
         final String hostname = "localhost";
         final int port = appRule.getLocalPort();
 
-        final FuturePromise<Session> sessionPromise = new FuturePromise<>();
-        client.connect(sslContextFactory, new InetSocketAddress(hostname, port),
-                new ServerSessionListener.Adapter(), sessionPromise);
-        final Session session = sessionPromise.get(5, TimeUnit.SECONDS);
-
-        final MetaData.Request request = new MetaData.Request("GET",
-                new HttpURI("https://" + hostname + ":" + port + "/api/test"),
-                HttpVersion.HTTP_2, new HttpFields());
-
-        final CountDownLatch latch = new CountDownLatch(1);
-        session.newStream(new HeadersFrame(request, null, true), new Promise.Adapter<>(), new ResponseListener(latch));
-        latch.await(5, TimeUnit.SECONDS);
-        assertThat(latch.getCount()).isEqualTo(0);
+        final ContentResponse response = client.GET("https://" + hostname + ":" + port + "/api/test");
+        assertThat(response.getVersion()).isEqualTo(HttpVersion.HTTP_2);
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).isEqualTo(FakeApplication.HELLO_WORLD);
     }
 
     @Test
@@ -106,22 +93,24 @@ public class Http2IntegrationTest {
         final String hostname = "localhost";
         final int port = appRule.getLocalPort();
 
-        final FuturePromise<Session> sessionPromise = new FuturePromise<>();
-        client.connect(sslContextFactory, new InetSocketAddress(hostname, port),
-                new ServerSessionListener.Adapter(), sessionPromise);
-        final Session session = sessionPromise.get(5, TimeUnit.SECONDS);
-
-        final MetaData.Request request = new MetaData.Request("GET",
-                new HttpURI("https://" + hostname + ":" + port + "/api/test"),
-                HttpVersion.HTTP_2, new HttpFields());
+        // For some reason the library requires to perform the first request synchronously with HTTP/2
+        testHttp2();
 
         final int amount = 100;
         final CountDownLatch latch = new CountDownLatch(amount);
         for (int i = 0; i < amount; i++) {
-            session.newStream(new HeadersFrame(request, null, true), new Promise.Adapter<>(),
-                    new ResponseListener(latch));
+            client.newRequest("https://" + hostname + ":" + port + "/api/test")
+                    .send(new BufferingResponseListener() {
+                        @Override
+                        public void onComplete(Result result) {
+                            assertThat(result.getResponse().getVersion()).isEqualTo(HttpVersion.HTTP_2);
+                            assertThat(result.getResponse().getStatus()).isEqualTo(200);
+                            assertThat(getContentAsString(Charsets.UTF_8)).isEqualTo(FakeApplication.HELLO_WORLD);
+                            latch.countDown();
+                        }
+                    });
         }
-        latch.await(10, TimeUnit.SECONDS);
-        assertThat(latch.getCount()).isEqualTo(0);
+
+        assertThat(latch.await(5, TimeUnit.SECONDS)).isTrue();
     }
 }
