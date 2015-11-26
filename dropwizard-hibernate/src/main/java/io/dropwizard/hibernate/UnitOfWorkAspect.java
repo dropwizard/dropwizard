1249da47f99d730816695acd5c7e463352153c52
Artem Prigoda <arteamon@gmail.com>
2015-11-26 19:17:37 +0300

    Generalize operations over `@UnitOfWork` methods
    Extract operations that should be performed on a `@UnitOfWork` method
    in an aspect. The aspect provides a public API for 3 operations:
    
    * Open session/open transaction before a method invocation;
    * Close session/commit transaction after the method invocation;
    * Close session/rollback transaction in case of an error.
    
    It will allow to create proxies/AOP solutions for handling the
    `@UnitOfWork` annotation outside of Jersey resources.

---
+++ dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkAspect.java
@@ -0,0 +1,121 @@
+package io.dropwizard.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.context.internal.ManagedSessionContext;
+
+import java.util.Map;
+
+/**
+ * An aspect providing operations around a method with the {@link UnitOfWork} annotation.
+ * It opens a Hibernate session and optionally a transaction.
+ * <p>It should be created for every invocation of the method.</p>
+ */
+class UnitOfWorkAspect {
+
+    private final Map<String, SessionFactory> sessionFactories;
+
+    public UnitOfWorkAspect(Map<String, SessionFactory> sessionFactories) {
+        this.sessionFactories = sessionFactories;
+    }
+
+    // Context variables
+    private UnitOfWork unitOfWork;
+    private Session session;
+    private SessionFactory sessionFactory;
+
+    public void beforeStart(UnitOfWork unitOfWork) {
+        if (unitOfWork == null) {
+            return;
+        }
+        this.unitOfWork = unitOfWork;
+
+        sessionFactory = sessionFactories.get(unitOfWork.value());
+        if (sessionFactory == null) {
+            // If the user didn't specify the name of a session factory,
+            // and we have only one registered, we can assume that it's the right one.
+            if (unitOfWork.value().equals(HibernateBundle.DEFAULT_NAME) && sessionFactories.size() == 1) {
+                sessionFactory = sessionFactories.values().iterator().next();
+            } else {
+                throw new IllegalArgumentException("Unregistered Hibernate bundle: '" + unitOfWork.value() + "'");
+            }
+        }
+        session = sessionFactory.openSession();
+        try {
+            configureSession();
+            ManagedSessionContext.bind(session);
+            beginTransaction();
+        } catch (Throwable th) {
+            session.close();
+            session = null;
+            ManagedSessionContext.unbind(sessionFactory);
+            throw th;
+        }
+    }
+
+    public void afterEnd() {
+        if (session == null) {
+            return;
+        }
+
+        try {
+            commitTransaction();
+        } catch (Exception e) {
+            rollbackTransaction();
+            throw e;
+        } finally {
+            session.close();
+            session = null;
+            ManagedSessionContext.unbind(sessionFactory);
+        }
+
+    }
+
+    public void onError() {
+        if (session == null) {
+            return;
+        }
+
+        try {
+            rollbackTransaction();
+        } finally {
+            session.close();
+            session = null;
+            ManagedSessionContext.unbind(sessionFactory);
+        }
+    }
+
+    private void configureSession() {
+        session.setDefaultReadOnly(unitOfWork.readOnly());
+        session.setCacheMode(unitOfWork.cacheMode());
+        session.setFlushMode(unitOfWork.flushMode());
+    }
+
+    private void beginTransaction() {
+        if (!unitOfWork.transactional()) {
+            return;
+        }
+        session.beginTransaction();
+    }
+
+    private void rollbackTransaction() {
+        if (!unitOfWork.transactional()) {
+            return;
+        }
+        final Transaction txn = session.getTransaction();
+        if (txn != null && txn.isActive()) {
+            txn.rollback();
+        }
+    }
+
+    private void commitTransaction() {
+        if (!unitOfWork.transactional()) {
+            return;
+        }
+        final Transaction txn = session.getTransaction();
+        if (txn != null && txn.isActive()) {
+            txn.commit();
+        }
+    }
+}
