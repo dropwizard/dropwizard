9693be5c0868662de37ccd07c36945ee5ca79f6e
Matt Whipple <matt@mattwhipple.com>
2016-01-13 21:08:53 -0500

    Allow RequestLogFactory implementation discovery

--- dropwizard-jetty/src/main/java/io/dropwizard/jetty/RequestLogFactory.java
+++ dropwizard-jetty/src/main/java/io/dropwizard/jetty/RequestLogFactory.java
@@ -3,13 +3,15 @@ package io.dropwizard.jetty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.eclipse.jetty.server.RequestLog;
 
+import io.dropwizard.jackson.Discoverable;
+
 /**
  * A service provider interface for creating a Jetty {@link RequestLog}
  *
  * @param <T> type of a {@link RequestLog} implementation
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = Slf4jRequestLogFactory.class)
-public interface RequestLogFactory<T extends RequestLog> {
+public interface RequestLogFactory<T extends RequestLog> extends Discoverable {
 
     boolean isEnabled();
 
