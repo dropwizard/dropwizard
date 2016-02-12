f51104513e7f1d8d8c9bf2b3703af3761a8a1c59
Sourav Mitra <souravmitr@gmail.com>
2016-02-12 12:01:41 +0530

    Added support for multiple packages in Scanning Hibernate Bundle

--- dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/ScanningHibernateBundleTest.java
+++ dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/ScanningHibernateBundleTest.java
@@ -14,10 +14,23 @@ public class ScanningHibernateBundleTest {
         //given
         String packageWithEntities = "io.dropwizard.hibernate.fake.entities.pckg";
         //when
-        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(packageWithEntities);
+        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(new String[] { packageWithEntities });
 
         //then
         assertFalse(findEntityClassesFromDirectory.isEmpty());
         assertEquals(4, findEntityClassesFromDirectory.size());
+    }
+
+    @Test
+    public void testFindEntityClassesFromMultipleDirectories() {
+        //given
+        String packageWithEntities = "io.dropwizard.hibernate.fake.entities.pckg";
+        String packageWithEntities2 = "io.dropwizard.hibernate.fake2.entities.pckg";
+        //when
+        ImmutableList<Class<?>> findEntityClassesFromDirectory = ScanningHibernateBundle.findEntityClassesFromDirectory(new String[] { packageWithEntities, packageWithEntities2 });
+
+        //then
+        assertFalse(findEntityClassesFromDirectory.isEmpty());
+        assertEquals(8, findEntityClassesFromDirectory.size());
     }
 }
---
+++ dropwizard-hibernate/src/test/java/io/dropwizard/hibernate/fake2/entities/pckg/deep/FakeEntity1.java
@@ -0,0 +1,7 @@
+package io.dropwizard.hibernate.fake2.entities.pckg.deep;
+import javax.persistence.Entity;
+
+@Entity
+public class FakeEntity1 {
+
+}
