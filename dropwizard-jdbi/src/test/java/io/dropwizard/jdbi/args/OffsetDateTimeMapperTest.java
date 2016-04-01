c02b4bd6d044a9dab0886247b57356002e385170
Patryk Najda <Patryk.Najda@wp.pl>
2016-04-01 22:30:14 +0200

    Fix tests affected by DST change

--- dropwizard-jdbi/src/test/java/io/dropwizard/jdbi/args/OffsetDateTimeMapperTest.java
+++ dropwizard-jdbi/src/test/java/io/dropwizard/jdbi/args/OffsetDateTimeMapperTest.java
@@ -5,8 +5,9 @@ import org.mockito.Mockito;
 
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -17,12 +18,13 @@ public class OffsetDateTimeMapperTest {
 
     @Test
     public void mapColumnByName() throws Exception {
-        when(resultSet.getTimestamp("name")).thenReturn(Timestamp.valueOf("2007-12-03 10:15:30.375"));
+        final Instant now = OffsetDateTime.now().toInstant();
+
+        when(resultSet.getTimestamp("name")).thenReturn(Timestamp.from(now));
 
         OffsetDateTime actual = new OffsetDateTimeMapper().mapColumn(resultSet, "name", null);
 
-        final ZoneOffset localOffset = ZoneOffset.from(OffsetDateTime.now());
-        assertThat(actual).isEqualTo(OffsetDateTime.of(2007, 12, 3, 10, 15, 30, 375_000_000, localOffset));
+        assertThat(actual).isEqualTo(OffsetDateTime.ofInstant(now, ZoneId.systemDefault()));
     }
 
     @Test
@@ -36,12 +38,13 @@ public class OffsetDateTimeMapperTest {
 
     @Test
     public void mapColumnByIndex() throws Exception {
-        when(resultSet.getTimestamp(1)).thenReturn(Timestamp.valueOf("2007-12-03 10:15:30.375"));
+        final Instant now = OffsetDateTime.now().toInstant();
+
+        when(resultSet.getTimestamp(1)).thenReturn(Timestamp.from(now));
 
         OffsetDateTime actual = new OffsetDateTimeMapper().mapColumn(resultSet, 1, null);
 
-        final ZoneOffset localOffset = ZoneOffset.from(OffsetDateTime.now());
-        assertThat(actual).isEqualTo(OffsetDateTime.of(2007, 12, 3, 10, 15, 30, 375_000_000, localOffset));
+        assertThat(actual).isEqualTo(OffsetDateTime.ofInstant(now, ZoneId.systemDefault()));
     }
 
     @Test
