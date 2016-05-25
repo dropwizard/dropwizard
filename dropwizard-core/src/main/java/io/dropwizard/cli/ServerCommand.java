34011e21582e6ba69494f8eea802a98f7242b186
BusComp <BusComp@users.noreply.github.com>
2016-05-25 15:17:23 +0200

    Catch exceptions of server.stop() and cleanup() in ServerCommand (#1560)
    Fixes #1557

--- dropwizard-core/src/main/java/io/dropwizard/cli/ServerCommand.java
+++ dropwizard-core/src/main/java/io/dropwizard/cli/ServerCommand.java
@@ -53,8 +53,16 @@ public class ServerCommand<T extends Configuration> extends EnvironmentCommand<T
             server.start();
         } catch (Exception e) {
             LOGGER.error("Unable to start server, shutting down", e);
-            server.stop();
-            cleanup();
+            try {
+                server.stop();
+            } catch (Exception e1) {
+                LOGGER.warn("Failure during stop server", e1);
+            }
+            try {
+                cleanup();
+            } catch (Exception e2) {
+                LOGGER.warn("Failure during cleanup", e2);
+            }
             throw e;
         }
     }
