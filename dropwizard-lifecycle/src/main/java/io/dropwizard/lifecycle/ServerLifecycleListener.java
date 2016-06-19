5b8ab10f350fcdbedfc35282493eb408fcd9e52f
Justin Plock <justin@plock.net>
2016-06-19 09:29:43 -0400

    Add helper functions for getting the local and admin ports from a server instance

--- dropwizard-lifecycle/src/main/java/io/dropwizard/lifecycle/ServerLifecycleListener.java
+++ dropwizard-lifecycle/src/main/java/io/dropwizard/lifecycle/ServerLifecycleListener.java
@@ -1,9 +1,19 @@
 package io.dropwizard.lifecycle;
 
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-
+import org.eclipse.jetty.server.ServerConnector;
 import java.util.EventListener;
 
 public interface ServerLifecycleListener extends EventListener {
     void serverStarted(Server server);
+
+    default int getLocalPort(Server server) {
+        return ((ServerConnector) server.getConnectors()[0]).getLocalPort();
+    }
+
+    default int getAdminPort(Server server) {
+        final Connector[] connectors = server.getConnectors();
+        return ((ServerConnector) connectors[connectors.length -1]).getLocalPort();
+    }
 }
