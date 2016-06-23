3036c7fad962bbb9c52d8548b8a15c58e96b93e3
Artem Prigoda <arteamon@gmail.com>
2016-06-23 10:00:09 +0200

    Setup the host header for HTTP requests

--- dropwizard-jetty/src/test/java/io/dropwizard/jetty/BiDiGzipHandlerTest.java
+++ dropwizard-jetty/src/test/java/io/dropwizard/jetty/BiDiGzipHandlerTest.java
@@ -38,6 +38,8 @@ public class BiDiGzipHandlerTest {
 
     @Before
     public void setUp() throws Exception {
+        request.setHeader(HttpHeaders.HOST, "localhost");
+
         gzipHandler.setExcludedAgentPatterns();
         gzipHandler.addIncludedMethods("POST");
 
