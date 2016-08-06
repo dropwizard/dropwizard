1e40fef4f4e8f048885b7ce6067bd9cd87a749b2
Carter Kozak <c4kofony@gmail.com>
2016-08-06 19:26:51 -0400

    Set FAIL_ON_UNKNOWN_PROPERTIES in DefaultConfigurationFactoryFactory (#1677)
    ObjectMapper-customization logic is moved from YamlConfigurationFactory
    to DefaultConfigurationFactoryFactory, making it easier to modify with a
    custom ConfigurationFactoryFactory implementation.
    
    There are some (uncommon) use cases where it's beneficial to update an
    application without modifying the configuration, which may fail if the
    application configuration has removed a configuration property.
    
    A protected configureObjectMapper method is added to
    DefaultConfigurationFactoryFactory to allow the configuration
    factory to be extended to apply (or remove) deserialization
    features.

--- dropwizard-configuration/src/test/java/io/dropwizard/configuration/ConfigurationFactoryFactoryTest.java
+++ dropwizard-configuration/src/test/java/io/dropwizard/configuration/ConfigurationFactoryFactoryTest.java
@@ -1,11 +1,15 @@
 package io.dropwizard.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 import io.dropwizard.configuration.ConfigurationFactoryTest.Example;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.validation.BaseValidator;
-import org.junit.Before;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import javax.validation.Validator;
 import java.io.File;
@@ -15,21 +19,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfigurationFactoryFactoryTest {
 
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
     private final ConfigurationFactoryFactory<Example> factoryFactory = new DefaultConfigurationFactoryFactory<>();
     private final Validator validator = BaseValidator.newValidator();
-    private File validFile;
-
-    @Before
-    public void setUp() throws Exception {
-        this.validFile = new File(Resources.getResource("factory-test-valid.yml").toURI());
-    }
 
     @Test
     public void createDefaultFactory() throws Exception {
+        File validFile = new File(Resources.getResource("factory-test-valid.yml").toURI());
         ConfigurationFactory<Example> factory =
             factoryFactory.create(Example.class, validator, Jackson.newObjectMapper(), "dw");
         final Example example = factory.build(validFile);
         assertThat(example.getName())
             .isEqualTo("Coda Hale");
+    }
+
+    @Test
+    public void createDefaultFactoryFailsUnknownProperty() throws Exception {
+        File validFileWithUnknownProp = new File(
+            Resources.getResource("factory-test-unknown-property.yml").toURI());
+        ConfigurationFactory<Example> factory =
+            factoryFactory.create(Example.class, validator, Jackson.newObjectMapper(), "dw");
+        expectedException.expect(ConfigurationException.class);
+        expectedException.expectMessage("Unrecognized field at: trait");
+        factory.build(validFileWithUnknownProp);
+    }
+
+    @Test
+    public void createFactoryAllowingUnknownProperties() throws Exception {
+        ConfigurationFactoryFactory<Example> customFactory = new PassThroughConfigurationFactoryFactory();
+        File validFileWithUnknownProp = new File(
+            Resources.getResource("factory-test-unknown-property.yml").toURI());
+        ConfigurationFactory<Example> factory =
+            customFactory.create(
+                Example.class,
+                validator,
+                Jackson.newObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES),
+                "dw");
+        Example example = factory.build(validFileWithUnknownProp);
+        assertThat(example.getName())
+            .isEqualTo("Mighty Wizard");
+    }
+
+    private static final class PassThroughConfigurationFactoryFactory
+            extends DefaultConfigurationFactoryFactory<Example> {
+        @Override
+        protected ObjectMapper configureObjectMapper(ObjectMapper objectMapper) {
+            return objectMapper;
+        }
     }
 }
