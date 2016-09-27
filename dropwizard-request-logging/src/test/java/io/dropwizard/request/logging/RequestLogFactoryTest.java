9e01f7ec99e47f24f26698dbde7379e51b4e3aad
Jonathan Monette <jon.monette@gmail.com>
2016-09-27 00:18:51 -0500

    Rename META-INF.services -> META-INF/services for dropwizard-request-logging (#1743)
    * Rename META-INF.services -> META-INF/services for dropwizard-request-logging
    * Added Discoverable subtype test for LogbackAccessRequestLogFactory

--- dropwizard-request-logging/src/test/java/io/dropwizard/request/logging/RequestLogFactoryTest.java
+++ dropwizard-request-logging/src/test/java/io/dropwizard/request/logging/RequestLogFactoryTest.java
@@ -1,8 +1,11 @@
 package io.dropwizard.request.logging;
 
+import java.io.File;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 import io.dropwizard.configuration.YamlConfigurationFactory;
+import io.dropwizard.jackson.DiscoverableSubtypeResolver;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.logging.ConsoleAppenderFactory;
 import io.dropwizard.logging.FileAppenderFactory;
@@ -10,8 +13,6 @@ import io.dropwizard.logging.SyslogAppenderFactory;
 import io.dropwizard.validation.BaseValidator;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,5 +38,11 @@ public class RequestLogFactoryTest {
         assertThat(logbackAccessRequestLogFactory.getAppenders().size()).isEqualTo(1);
         assertThat(logbackAccessRequestLogFactory.getAppenders().get(0))
             .isInstanceOf(FileAppenderFactory.class);
+    }
+
+    @Test
+    public void isDiscoverable() throws Exception {
+        assertThat(new DiscoverableSubtypeResolver().getDiscoveredSubtypes())
+            .contains(LogbackAccessRequestLogFactory.class);
     }
 }
