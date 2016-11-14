4c578443f2f1bc6e359fd97cc386a126d6a34137
Artem Prigoda <arteamon@gmail.com>
2016-11-14 18:48:08 +0100

    Fix the META-INF/services structure in `dropwizard-request-logging`
    Otherwise Dropwizard logging is not `discoverable`

--- dropwizard-request-logging/src/test/java/io/dropwizard/request/logging/old/LogbackClassicRequestLogFactoryTest.java
+++ dropwizard-request-logging/src/test/java/io/dropwizard/request/logging/old/LogbackClassicRequestLogFactoryTest.java
@@ -3,11 +3,13 @@ package io.dropwizard.request.logging.old;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 import io.dropwizard.configuration.YamlConfigurationFactory;
+import io.dropwizard.jackson.DiscoverableSubtypeResolver;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.logging.BootstrapLogging;
 import io.dropwizard.logging.ConsoleAppenderFactory;
 import io.dropwizard.logging.FileAppenderFactory;
 import io.dropwizard.logging.SyslogAppenderFactory;
+import io.dropwizard.request.logging.RequestLogFactory;
 import io.dropwizard.validation.BaseValidator;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,23 +25,30 @@ public class LogbackClassicRequestLogFactoryTest {
         BootstrapLogging.bootstrap();
     }
 
-    private LogbackClassicRequestLogFactory requestLog;
+    private RequestLogFactory requestLog;
 
     @Before
     public void setUp() throws Exception {
         final ObjectMapper objectMapper = Jackson.newObjectMapper();
         objectMapper.getSubtypeResolver().registerSubtypes(ConsoleAppenderFactory.class, FileAppenderFactory.class,
             SyslogAppenderFactory.class);
-        this.requestLog = new YamlConfigurationFactory<>(LogbackClassicRequestLogFactory.class,
+        this.requestLog = new YamlConfigurationFactory<>(RequestLogFactory.class,
             BaseValidator.newValidator(), objectMapper, "dw")
             .build(new File(Resources.getResource("yaml/logbackClassicRequestLog.yml").toURI()));
     }
 
     @Test
     public void testDeserialized() {
-        assertThat(requestLog.getTimeZone()).isEqualTo(TimeZone.getTimeZone("Europe/Amsterdam"));
-        assertThat(requestLog.getAppenders()).hasSize(3).extractingResultOf("getClass").contains(
+        LogbackClassicRequestLogFactory classicRequestLogFactory = (LogbackClassicRequestLogFactory) requestLog;
+        assertThat(classicRequestLogFactory.getTimeZone()).isEqualTo(TimeZone.getTimeZone("Europe/Amsterdam"));
+        assertThat(classicRequestLogFactory.getAppenders()).hasSize(3).extractingResultOf("getClass").contains(
             ConsoleAppenderFactory.class, FileAppenderFactory.class, SyslogAppenderFactory.class
         );
+    }
+
+    @Test
+    public void isDiscoverable() throws Exception {
+        assertThat(new DiscoverableSubtypeResolver().getDiscoveredSubtypes())
+            .contains(LogbackClassicRequestLogFactory.class);
     }
 }
