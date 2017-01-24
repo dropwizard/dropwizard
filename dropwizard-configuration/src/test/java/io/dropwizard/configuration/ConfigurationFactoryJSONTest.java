0c1ef29565339bfa764112874f76c877f17cc540
Manuel Hegner <manuel.hegner@bakdata.com>
2017-01-24 13:18:37 +0100

    reimplemented full JSON support as a new JsonConfigurationFactory

---
+++ dropwizard-configuration/src/test/java/io/dropwizard/configuration/ConfigurationFactoryJSONTest.java
@@ -0,0 +1,46 @@
+package io.dropwizard.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.dropwizard.jackson.Jackson;
+
+public class ConfigurationFactoryJSONTest extends ConfigurationFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    	factory = new JsonConfigurationFactory<>(Example.class, validator, Jackson.newObjectMapper(), "dw");
+        this.malformedFile = resourceFileName("factory-test-malformed.json");
+        this.emptyFile = resourceFileName("factory-test-empty.json");
+        this.invalidFile = resourceFileName("factory-test-invalid.json");
+        this.validFile = resourceFileName("factory-test-valid.json");
+        this.typoFile = resourceFileName("factory-test-typo.json");
+        this.wrongTypeFile = resourceFileName("factory-test-wrong-type.json");
+        this.malformedAdvancedFile = resourceFileName("factory-test-malformed-advanced.json");
+    }
+    
+    @Override
+    public void throwsAnExceptionOnMalformedFiles() throws Exception {
+        try {
+            factory.build(malformedFile);
+            failBecauseExceptionWasNotThrown(ConfigurationParsingException.class);
+        } catch (ConfigurationParsingException e) {
+            assertThat(e.getMessage())
+                    .containsOnlyOnce("* Malformed JSON at line:");
+        }
+    }
+
+    @Test
+    public void printsDetailedInformationOnMalformedYaml() throws Exception {
+        assertThatThrownBy(() -> factory.build(malformedAdvancedFile))
+            .hasMessageContaining(String.format(
+                malformedAdvancedFile.getName()+" has an error:%n" +
+                "  * Malformed JSON at line: 7, column: 3; Unexpected close marker '}': expected ']'"));
+    }
+}
