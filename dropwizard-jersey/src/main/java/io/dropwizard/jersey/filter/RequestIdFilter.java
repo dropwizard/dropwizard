274b375b9179b652d048ae1da00c23dae76a7b3e
Justin Plock <justin@plock.net>
2017-03-05 16:52:39 -0500

    Add some commonly used and useful jersey request/response filters

---
+++ dropwizard-jersey/src/main/java/io/dropwizard/jersey/filter/RequestIdFilter.java
@@ -0,0 +1,36 @@
+package io.dropwizard.jersey.filter;
+
+import java.io.IOException;
+import java.util.UUID;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class adds a "X-Request-Id" HTTP response header and logs the following
+ * information: request method, request path, request ID, response status,
+ * response bytes (or -1 if not known)
+ */
+@Provider
+@Priority(Priorities.USER)
+public class RequestIdFilter implements ContainerResponseFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestIdFilter.class);
+    private static final String REQUEST_ID = "X-Request-Id";
+
+    @Override
+    public void filter(final ContainerRequestContext request,
+            final ContainerResponseContext response) throws IOException {
+
+        final UUID id = UUID.randomUUID();
+        LOGGER.info("method={} path={} request_id={} status={} bytes={}",
+                request.getMethod(), request.getUriInfo().getPath(), id,
+                response.getStatus(), response.getLength());
+        response.getHeaders().add(REQUEST_ID, id);
+    }
+}
