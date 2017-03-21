fd75f91d4e5527dd906aa446c01e45260df9a97e
Jochen Schalanda <jochen@schalanda.name>
2017-03-21 10:17:22 +0100

    Update copyright notice
    [ci skip]
    
    (cherry picked from commit 2dbd11ac4677b71cd6d4228242a8be2ebc13729c)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Dropwizard'
-copyright = u'2010-2013, Coda Hale, Yammer Inc., 2014-2016 Dropwizard Team'
+copyright = u'2010-2013, Coda Hale, Yammer Inc., 2014-2017 Dropwizard Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
