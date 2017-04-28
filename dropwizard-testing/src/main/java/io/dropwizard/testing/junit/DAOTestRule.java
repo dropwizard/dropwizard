f7c0b0aa66a3b1e9fcaf131137d9313f1ac63845
Nick Babcock <nbabcock19@hotmail.com>
2017-04-28 11:13:58 -0400

    Simplify line using Map::forEach

--- dropwizard-testing/src/main/java/io/dropwizard/testing/junit/DAOTestRule.java
+++ dropwizard-testing/src/main/java/io/dropwizard/testing/junit/DAOTestRule.java
@@ -124,7 +124,7 @@ public class DAOTestRule extends ExternalResource {
             config.setProperty("jadira.usertype.autoRegisterUserTypes", "true");
 
             entityClasses.forEach(config::addAnnotatedClass);
-            properties.entrySet().forEach(e -> config.setProperty(e.getKey(), e.getValue()));
+            properties.forEach(config::setProperty);
 
             return new DAOTestRule(config.buildSessionFactory());
         }
