a665a1ce25978bb5652bd40ae94e2348fc099648
Jelmer ter Wal <jelmer.terwal@nedap.com>
2017-04-29 17:52:40 +0200

    fix test

--- dropwizard-cdi-weld/src/test/java/io/dropwizard/cdi/WeldApplicationTest.java
+++ dropwizard-cdi-weld/src/test/java/io/dropwizard/cdi/WeldApplicationTest.java
@@ -13,11 +13,11 @@ import static org.junit.Assert.assertThat;
 public class WeldApplicationTest {
 
     @Rule
-    public static final DropwizardAppRule<Configuration> APP_RULE = new DropwizardAppRule<>(WeldTestApplication.class);
+    public final DropwizardAppRule<Configuration> appRule = new DropwizardAppRule<>(WeldTestApplication.class);
 
     @Test
     public void dummyResourceRequest() throws Exception {
-        String response = ClientBuilder.newClient().target("http://localhost:" + APP_RULE.getLocalPort() + "/dummy").request().get(String.class);
+        String response = ClientBuilder.newClient().target("http://localhost:" + appRule.getLocalPort() + "/dummy").request().get(String.class);
         assertThat(response, is(DummyProducer.DUMMY));
     }
 }
