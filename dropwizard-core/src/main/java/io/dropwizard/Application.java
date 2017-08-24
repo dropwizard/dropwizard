2660b276ed424eb5da7a29b97506b8b197a97686
Lior Bar-On <baronlior@gmail.com>
2017-08-25 01:36:27 +0300

    fix a small type in a comment on Application.java

--- dropwizard-core/src/main/java/io/dropwizard/Application.java
+++ dropwizard-core/src/main/java/io/dropwizard/Application.java
@@ -86,7 +86,7 @@ public abstract class Application<T extends Configuration> {
         final Bootstrap<T> bootstrap = new Bootstrap<>(this);
         addDefaultCommands(bootstrap);
         initialize(bootstrap);
-        // Should by called after initialize to give an opportunity to set a custom metric registry
+        // Should be called after initialize to give an opportunity to set a custom metric registry
         bootstrap.registerMetrics();
 
         final Cli cli = new Cli(new JarLocation(getClass()), bootstrap, System.out, System.err);
