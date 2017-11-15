2635ce6a95107911ac20e8d84d1e5228d4e6be51
maffe <maffe@users.noreply.github.com>
2017-11-15 12:53:03 +0100

    Fixed typo in Javadoc markup

--- dropwizard-http2/src/main/java/io/dropwizard/http2/Http2CConnectorFactory.java
+++ dropwizard-http2/src/main/java/io/dropwizard/http2/Http2CConnectorFactory.java
@@ -31,7 +31,7 @@ import javax.validation.constraints.Min;
  *     </tr>
  *     <tr>
  *         <td>{@code maxConcurrentStreams}</td>
- *         <td><1024</td>
+ *         <td>1024</td>
  *         <td>
  *             The maximum number of concurrently open streams allowed on a single HTTP/2 connection.
  *             Larger values increase parallelism, but cost a memory commitment.
