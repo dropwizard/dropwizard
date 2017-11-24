5355d5643c66d688e34b166eff5bb6b8be80c17c
Artem Prigoda <arteamon@gmail.com>
2017-11-24 19:27:41 +0100

    Don't shut down asynchronous executor in Jersey (#2221)
    If a user customized a Jersey client, Jersey creates a new copy
    of the instance. Jersey has a mechanism for detecting leaks, and Jetty
    will invoke the `dispose` method on the old client. Unfortunately,
    during a copy the executor is shared. Therefore, the user will not be
    able to perform async requests anymore.
    
    The executor is managed by the Dropwizard environment, so we can leave
    with the fact that it's closed by a kill signal.
    
    Fixes #2218

--- dropwizard-client/src/main/java/io/dropwizard/client/DropwizardExecutorProvider.java
+++ dropwizard-client/src/main/java/io/dropwizard/client/DropwizardExecutorProvider.java
@@ -1,52 +1,21 @@
 package io.dropwizard.client;
 
-import com.google.common.util.concurrent.ForwardingExecutorService;
-import io.dropwizard.util.Duration;
 import org.glassfish.jersey.client.ClientAsyncExecutor;
 import org.glassfish.jersey.spi.ExecutorServiceProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutorService;
 
 /**
  * An {@link ExecutorServiceProvider} implementation for use within
  * Dropwizard.
- *
- * With {DropwizardExecutorProvider.DisposableExecutorService}, one
- * can signal that an {ExecutorService} is to be gracefully shut down
- * upon its disposal by the Jersey runtime. It is used as a means of
- * signaling to {@link DropwizardExecutorProvider} that the executor
- * is not shared.
  */
 @ClientAsyncExecutor
 class DropwizardExecutorProvider implements ExecutorServiceProvider {
-    /**
-     * An {@link ExecutorService} decorator used as a marker by
-     * {@link DropwizardExecutorProvider#dispose} to induce service
-     * shutdown.
-     */
-    static class DisposableExecutorService extends ForwardingExecutorService {
-        private final ExecutorService delegate;
-
-        public DisposableExecutorService(ExecutorService delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        protected ExecutorService delegate() {
-            return delegate;
-        }
-    }
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DropwizardExecutorProvider.class);
 
     private final ExecutorService executor;
-    private final Duration shutdownGracePeriod;
 
-    DropwizardExecutorProvider(ExecutorService executor, Duration shutdownGracePeriod) {
+    DropwizardExecutorProvider(ExecutorService executor) {
         this.executor = executor;
-        this.shutdownGracePeriod = shutdownGracePeriod;
     }
 
     @Override
@@ -56,15 +25,5 @@ class DropwizardExecutorProvider implements ExecutorServiceProvider {
 
     @Override
     public void dispose(ExecutorService executorService) {
-        if (executorService instanceof DisposableExecutorService) {
-            executorService.shutdown();
-
-            try {
-                executorService.awaitTermination(
-                    shutdownGracePeriod.getQuantity(), shutdownGracePeriod.getUnit());
-            } catch (InterruptedException err) {
-                LOGGER.warn("Interrupted while waiting for ExecutorService shutdown", err);
-            }
-        }
     }
 }
