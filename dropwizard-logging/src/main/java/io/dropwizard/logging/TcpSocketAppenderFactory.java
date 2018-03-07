1333f696ddb99c895e647c36be39369f1c32fa62
Artem Prigoda <arteamon@gmail.com>
2018-03-07 12:14:07 +0100

    Reuse common code across the appenders
    Add a new class `AsbtractOutputStreamAppenderFactory` which allows
    us to build appenders based on `ÒutputStreamAppender`, e.g. console,
    file, tcp, udp. It adds a new abstract method for building an appender
    which is implemented by actual factories and takes care of enchancing
    this appender with the layout encoder, filter factories and an async
    appender.

--- dropwizard-logging/src/main/java/io/dropwizard/logging/TcpSocketAppenderFactory.java
+++ dropwizard-logging/src/main/java/io/dropwizard/logging/TcpSocketAppenderFactory.java
@@ -1,14 +1,10 @@
 package io.dropwizard.logging;
 
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.Appender;
-import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import ch.qos.logback.core.OutputStreamAppender;
 import ch.qos.logback.core.spi.DeferredProcessingAware;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import io.dropwizard.logging.async.AsyncAppenderFactory;
-import io.dropwizard.logging.filter.LevelFilterFactory;
-import io.dropwizard.logging.layout.LayoutFactory;
 import io.dropwizard.logging.socket.DropwizardSocketAppender;
 import io.dropwizard.util.Duration;
 import io.dropwizard.util.Size;
@@ -17,7 +13,6 @@ import io.dropwizard.validation.PortRange;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.net.SocketFactory;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -59,7 +54,7 @@ import javax.validation.constraints.NotNull;
  * </table>
  */
 @JsonTypeName("tcp")
-public class TcpSocketAppenderFactory<E extends DeferredProcessingAware> extends AbstractAppenderFactory<E> {
+public class TcpSocketAppenderFactory<E extends DeferredProcessingAware> extends AbstractOutputStreamAppenderFactory<E> {
 
     @NotEmpty
     private String host = "localhost";
@@ -126,22 +121,13 @@ public class TcpSocketAppenderFactory<E extends DeferredProcessingAware> extends
     }
 
     @Override
-    public Appender<E> build(LoggerContext context, String applicationName, LayoutFactory<E> layoutFactory,
-                             LevelFilterFactory<E> levelFilterFactory, AsyncAppenderFactory<E> asyncAppenderFactory) {
-        final DropwizardSocketAppender<E> appender = new DropwizardSocketAppender<>(host, port,
+    protected OutputStreamAppender<E> appender(LoggerContext context) {
+        final OutputStreamAppender<E> appender = new DropwizardSocketAppender<>(host, port,
             (int) connectionTimeout.toMilliseconds(), (int) sendBufferSize.toBytes(), socketFactory());
         appender.setContext(context);
         appender.setName("tcp-socket-appender");
         appender.setImmediateFlush(immediateFlush);
-
-        final LayoutWrappingEncoder<E> layoutEncoder = new LayoutWrappingEncoder<>();
-        layoutEncoder.setLayout(buildLayout(context, layoutFactory));
-        appender.setEncoder(layoutEncoder);
-
-        appender.addFilter(levelFilterFactory.build(threshold));
-        getFilterFactories().forEach(f -> appender.addFilter(f.build()));
-        appender.start();
-        return wrapAsync(appender, asyncAppenderFactory);
+        return appender;
     }
 
     protected SocketFactory socketFactory() {
