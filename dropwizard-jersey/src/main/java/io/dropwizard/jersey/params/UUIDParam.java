cc51afafc53f13277397dc672b87c9b87c62faac
Nick Babcock <nbabcock19@hotmail.com>
2018-06-10 08:39:50 -0500

    UUID param to length check input

--- dropwizard-jersey/src/main/java/io/dropwizard/jersey/params/UUIDParam.java
+++ dropwizard-jersey/src/main/java/io/dropwizard/jersey/params/UUIDParam.java
@@ -24,6 +24,17 @@ public class UUIDParam extends AbstractParam<UUID> {
 
     @Override
     protected UUID parse(@Nullable String input) throws Exception {
+        // From UUID RFC 4122 spec, a UUID contains 32 hex digits with 4 dashes. fromString will
+        // ensure that only hex exists and that there are 4 dashes, but does no length checking, so
+        // the input could have additional hex digits appended and no error would be raised. Since
+        // the spec clearly defines the length to be 36, we'll ensure the input conforms. Some UUID
+        // implementations are lenient and allow absent dashes (thus making the total length 32),
+        // but since fromString requires dashes we don't need to worry about supporting a range of
+        // lengths.
+        if (input != null && input.length() != 36) {
+            throw new IllegalArgumentException("Expecting a UUID of 36 in length");
+        }
+
         return UUID.fromString(input);
     }
 
