78ed403ecc0cf602b48742aa2443b29835020060
chris <christopher.burke@airnz.co.nz>
2019-01-17 07:51:43 +1300

    Fallback to default Jackson enum handling when DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL or DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE is set.

--- dropwizard-jackson/src/main/java/io/dropwizard/jackson/FuzzyEnumModule.java
+++ dropwizard-jackson/src/main/java/io/dropwizard/jackson/FuzzyEnumModule.java
@@ -74,8 +74,10 @@ public class FuzzyEnumModule extends Module {
                                                         DeserializationConfig config,
                                                         BeanDescription desc) throws JsonMappingException {
             // If the user configured to use `toString` method to deserialize enums
-            if (config.hasDeserializationFeatures(
-                    DeserializationFeature.READ_ENUMS_USING_TO_STRING.getMask())) {
+            if (config.hasDeserializationFeatures(DeserializationFeature.READ_ENUMS_USING_TO_STRING.getMask()) ||
+                config.hasDeserializationFeatures(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL.getMask()) ||
+                // The presence of @JsonEnumDefaultValue will cause a fallback to the default, however lets short circuit here
+                config.hasDeserializationFeatures(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE.getMask())) {
                 return null;
             }
 
--- dropwizard-jackson/src/test/java/io/dropwizard/jackson/FuzzyEnumModuleTest.java
+++ dropwizard-jackson/src/test/java/io/dropwizard/jackson/FuzzyEnumModuleTest.java
@@ -1,6 +1,7 @@
 package io.dropwizard.jackson;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -66,6 +67,9 @@ public class FuzzyEnumModuleTest {
 
         @JsonProperty("forgot password")
         FORGOT_PASSWORD,
+
+        @JsonEnumDefaultValue
+        DEFAULT
     }
 
     @Before
@@ -144,6 +148,20 @@ public class FuzzyEnumModuleTest {
         final ObjectMapper toStringEnumsMapper = mapper.copy()
                 .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
         assertThat(toStringEnumsMapper.readValue("\"Pound sterling\"", CurrencyCode.class)).isEqualTo(CurrencyCode.GBP);
+    }
+
+    @Test
+    public void readsUnknownEnumValuesAsNull() throws Exception {
+        final ObjectMapper toStringEnumsMapper = mapper.copy()
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        assertThat(toStringEnumsMapper.readValue("\"Pound sterling\"", CurrencyCode.class)).isNull();
+    }
+
+    @Test
+    public void readsUnknownEnumValuesUsingDefaultValue() throws Exception {
+        final ObjectMapper toStringEnumsMapper = mapper.copy()
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+        assertThat(toStringEnumsMapper.readValue("\"Pound sterling\"", EnumWithPropertyAnno.class)).isEqualTo(EnumWithPropertyAnno.DEFAULT);
     }
 
     @Test
