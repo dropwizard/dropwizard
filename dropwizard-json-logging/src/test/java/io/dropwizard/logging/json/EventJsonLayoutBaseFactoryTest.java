3c630a5ccc7cba523488ddfbcd5008bb7d432bc3
Jochen Schalanda <jochen@schalanda.name>
2019-03-03 11:25:06 +0100

    Clean up EventJsonLayoutBaseFactoryTest

--- dropwizard-json-logging/src/test/java/io/dropwizard/logging/json/EventJsonLayoutBaseFactoryTest.java
+++ dropwizard-json-logging/src/test/java/io/dropwizard/logging/json/EventJsonLayoutBaseFactoryTest.java
@@ -1,40 +1,40 @@
 package io.dropwizard.logging.json;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.pattern.ThrowableHandlingConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.ThrowableProxy;
-import com.google.common.base.Throwables;
 import io.dropwizard.logging.json.layout.ExceptionFormat;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.regex.Pattern;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-public class EventJsonLayoutBaseFactoryTest {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collections;
 
-    private static final String LINE_SEPERATOR = System.getProperty("line.separator");
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class EventJsonLayoutBaseFactoryTest {
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
     // Use unique spelling to guard against false positive matches
     private ThrowableProxy proxy = new ThrowableProxy(new RuntimeException("wrapp3d", new IOException("r00t")));
-    private String packageFilter = "org.junit";
     private ILoggingEvent event = Mockito.mock(ILoggingEvent.class);
 
     @BeforeEach
-    public void before() {
+    void before() {
         when(event.getThrowableProxy()).thenReturn(proxy);
     }
 
     @Test
-    public void testCreateThrowableProxyConverter_Configured() {
+    void testCreateThrowableProxyConverter_Configured() {
         ExceptionFormat format = new ExceptionFormat();
         format.setDepth("8");
         format.setRootFirst(false);
+        String packageFilter = "org.junit";
         format.setEvaluators(Collections.singletonList(packageFilter));
 
         EventJsonLayoutBaseFactory factory = new EventJsonLayoutBaseFactory();
@@ -44,24 +44,24 @@ public class EventJsonLayoutBaseFactoryTest {
         converter.start();
 
         // Verify the original stack includes the excluded packages
-        assertThat(Throwables.getStackTraceAsString(proxy.getThrowable())).contains(packageFilter);
+        assertThat(proxy.getThrowable()).hasStackTraceContaining(packageFilter);
 
         String conversion = converter.convert(event);
 
         // Verify the conversion depth
         // 2 messages and 8 lines of stack per throwable
-        assertThat(conversion.split(LINE_SEPERATOR)).hasSize(18);
+        assertThat(conversion).hasLineCount(18);
 
         // Verify that the root is not first
-        assertThat(conversion).containsPattern(Pattern.compile("wrapp3d.*r00t", Pattern.DOTALL));
+        assertThat(conversion).containsSubsequence("wrapp3d", "r00t");
 
         // Verify the conversion does not includes the excluded packages and contains skipped lines
         assertThat(conversion).doesNotContain(packageFilter);
-        assertThat(conversion).containsPattern(Pattern.compile("\\[\\d+ skipped\\]"));
+        assertThat(conversion).containsPattern("\\[\\d+ skipped\\]");
     }
 
     @Test
-    public void testCreateThrowableProxyConverter_Default() {
+    void testCreateThrowableProxyConverter_Default() throws Exception {
         EventJsonLayoutBaseFactory factory = new EventJsonLayoutBaseFactory();
 
         ThrowableHandlingConverter converter = factory.createThrowableProxyConverter(new LoggerContext());
@@ -69,12 +69,20 @@ public class EventJsonLayoutBaseFactoryTest {
 
         String conversion = converter.convert(event);
 
-        int originalSize = Throwables.getStackTraceAsString(proxy.getThrowable()).split(LINE_SEPERATOR).length;
+        int originalSize = getStackTraceAsString(proxy.getThrowable()).split(LINE_SEPARATOR).length;
 
         // Verify that the full stack is included
-        assertThat(conversion.split(LINE_SEPERATOR)).hasSize(originalSize);
+        assertThat(conversion).hasLineCount(originalSize);
 
         // Verify that the root is first
-        assertThat(conversion).containsPattern(Pattern.compile("r00t.*wrapp3d", Pattern.DOTALL));
+        assertThat(conversion).containsSubsequence("r00t", "wrapp3d");
+    }
+
+    private static String getStackTraceAsString(Throwable throwable) throws IOException {
+        try (StringWriter stringWriter = new StringWriter();
+             PrintWriter printWriter = new PrintWriter(stringWriter)) {
+            throwable.printStackTrace(printWriter);
+            return stringWriter.toString();
+        }
     }
 }
