6a0afd6ef8aa04dd5a0c506e36bb4cc1c492b536
Jochen Schalanda <jochen@schalanda.name>
2019-03-17 13:58:18 +0100

    Always run JaCoCo, also on Java 11
    Refs jacoco/jacoco#663

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -3,6 +3,6 @@ set -e
 set -uxo pipefail
 
 if [[ "${TRAVIS_JDK_VERSION}" == "openjdk8" ]]; then
-    ./mvnw test jacoco:report coveralls:report -P code-coverage -B -q
+    ./mvnw coveralls:report -B -q
     exit $?
 fi
