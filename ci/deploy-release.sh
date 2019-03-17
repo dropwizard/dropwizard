f97e73a2369904a6e1f908be4c5c3671034c96ff
Jochen Schalanda <jochen@schalanda.name>
2019-03-17 19:00:29 +0100

    Use pinentry-mode=loopback for GnuPG
    https://issues.apache.org/jira/browse/MGPG-59

--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -4,11 +4,7 @@ set -uxo pipefail
 
 # Decrypt and import signing key
 openssl aes-256-cbc -K $encrypted_ec79e61fc360_key -iv $encrypted_ec79e61fc360_iv -in ci/dropwizard.asc.enc -out ci/dropwizard.asc -d
-gpg --fast-import ci/dropwizard.asc
-
-# Avoid error message: "gpg: signing failed: Inappropriate ioctl for device"
-# https://tutorials.technology/solved_errors/21-gpg-signing-failed-Inappropriate-ioctl-for-device.html
-export GPG_TTY=$(tty)
+gpg2 --import ci/dropwizard.asc
 
 ./mvnw -B deploy --settings 'ci/settings.xml' -DperformRelease=true -Dmaven.test.skip=true
 
