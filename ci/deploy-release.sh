eebd6707281c324ea4dbf40d0b627892cca5a395
Jochen Schalanda <jochen@schalanda.name>
2019-03-25 00:10:44 +0100

    Don't deploy GitHub Pages
    Travis CI doesn't support retaining the contents of the gh-pages branch without a custom script

--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -7,10 +7,3 @@ openssl aes-256-cbc -K $encrypted_ec79e61fc360_key -iv $encrypted_ec79e61fc360_i
 gpg --armor --import ci/dropwizard.asc
 
 ./mvnw -B deploy --settings 'ci/settings.xml' -DperformRelease=true -Dmaven.test.skip=true
-
-# Documentation
-./mvnw -B site site:stage --settings 'ci/settings.xml' -Dmaven.test.skip=true
-
-DOCS_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
-mkdir gh-pages
-mv target/staging gh-pages/"${DOCS_VERSION}"
