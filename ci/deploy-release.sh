fa4d1b8a49a7ea5cb1ee43c21680f3110bcfb6d7
Jochen Schalanda <jochen@schalanda.name>
2019-03-24 22:08:07 +0100

    Use GnuPG 1.x in deploy-release.sh

--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -4,7 +4,7 @@ set -uxo pipefail
 
 # Decrypt and import signing key
 openssl aes-256-cbc -K $encrypted_ec79e61fc360_key -iv $encrypted_ec79e61fc360_iv -in ci/dropwizard.asc.enc -out ci/dropwizard.asc -d
-gpg2 --armor --import ci/dropwizard.asc
+gpg --armor --import ci/dropwizard.asc
 
 ./mvnw -B deploy --settings 'ci/settings.xml' -DperformRelease=true -Dmaven.test.skip=true
 
