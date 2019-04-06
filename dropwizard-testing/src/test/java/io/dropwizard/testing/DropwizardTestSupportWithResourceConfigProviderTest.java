6c17257182e973dc59b5a9c318a5de5a36594737
Michael Zamani <michael.zamani1@gmail.com>
2019-04-06 11:27:49 -0700

    Only override ConfigurationSourceProvider if explicitly provided (#2720)
    Fixes #2719

--- dropwizard-testing/src/test/java/io/dropwizard/testing/DropwizardTestSupportWithResourceConfigProviderTest.java
+++ dropwizard-testing/src/test/java/io/dropwizard/testing/DropwizardTestSupportWithResourceConfigProviderTest.java
@@ -1,16 +1,21 @@
 package io.dropwizard.testing;
 
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import io.dropwizard.testing.app.TestApplication;
 import io.dropwizard.testing.app.TestConfiguration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DropwizardTestSupportWithResourceConfigProviderTest {
+    private static final TestResourceConfigurationSourceProvider TEST_CONFIG_SOURCE_PROVIDER =
+            new TestResourceConfigurationSourceProvider();
     private static final DropwizardTestSupport<TestConfiguration> TEST_SUPPORT = new DropwizardTestSupport<>(
             TestApplication.class, "test-config.yaml", new ResourceConfigurationSourceProvider());
 
@@ -40,5 +45,34 @@ class DropwizardTestSupportWithResourceConfigProviderTest {
     void returnsEnvironment() {
         final Environment environment = TEST_SUPPORT.getEnvironment();
         assertThat(environment.getName()).isEqualTo("TestApplication");
+    }
+
+    @Test
+    void doesNotOverwriteConfigSourceProviderIfNotProvidedInConstructor() throws Exception {
+        DropwizardTestSupport<TestConfiguration> support = new DropwizardTestSupport<>(
+                TestApplication.class, "test-config.yaml");
+        try {
+            support.before();
+            assertThat(TEST_CONFIG_SOURCE_PROVIDER.openCalled).isTrue();
+        } finally {
+            support.after();
+        }
+    }
+
+    public static class TestResourceConfigurationSourceProvider extends ResourceConfigurationSourceProvider {
+        volatile boolean openCalled = false;
+
+        @Override
+        public InputStream open(String path) throws IOException {
+            openCalled = true;
+            return super.open(path);
+        }
+    }
+
+    public static class TestApplication extends io.dropwizard.testing.app.TestApplication {
+        @Override
+        public void initialize(Bootstrap<TestConfiguration> bootstrap) {
+            bootstrap.setConfigurationSourceProvider(TEST_CONFIG_SOURCE_PROVIDER);
+        }
     }
 }
