b1c39570ef51e0c337f6cacea159fbd80fb598ce
Jochen Schalanda <jochen@schalanda.name>
2019-10-06 18:21:55 +0200

    Enable Jackson Afterburner only on Java 8 (#2966)
    The Jackson Afterburner module is currently not compatible with the JPMS and will lead to illegal reflective access operation warnings:
    ```
    WARNING: An illegal reflective access operation has occurred
    WARNING: Illegal reflective access by com.fasterxml.jackson.module.afterburner.util.MyClassLoader (file:.m2/repository/com/fasterxml/jackson/module/jackson-module-afterburner/2.9.9/jackson-module-afterburner-2.9.9.jar) to method java.lang.ClassLoader.findLoadedClass(java.lang.String)
    WARNING: Please consider reporting this to the maintainers of com.fasterxml.jackson.module.afterburner.util.MyClassLoader
    WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
    WARNING: All illegal access operations will be denied in a future release
    ```
    
    Fixes #2909
    Refs FasterXML/jackson-modules-base#37

--- dropwizard-jackson/src/main/java/io/dropwizard/jackson/Jackson.java
+++ dropwizard-jackson/src/main/java/io/dropwizard/jackson/Jackson.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import io.dropwizard.util.JavaVersion;
 
 import javax.annotation.Nullable;
 
@@ -60,7 +61,9 @@ public class Jackson {
         mapper.registerModule(new GuavaExtrasModule());
         mapper.registerModule(new CaffeineModule());
         mapper.registerModule(new JodaModule());
-        mapper.registerModule(new AfterburnerModule());
+        if (JavaVersion.isJava8()) {
+            mapper.registerModule(new AfterburnerModule());
+        }
         mapper.registerModule(new FuzzyEnumModule());
         mapper.registerModule(new ParameterNamesModule());
         mapper.registerModule(new Jdk8Module());
