0b3b513f1fd3add9f88a6640e168f617a052c211
Jochen Schalanda <jochen@schalanda.name>
2019-10-13 11:59:59 +0200

    Try speeding up Maven builds by tuning JVM parameters

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -3,6 +3,6 @@ set -e
 set -uxo pipefail
 
 if [[ "${TRAVIS_JDK_VERSION}" == "openjdk8" ]]; then
-    ./mvnw coveralls:report -B -q
+    ./mvnw -V -B -ff -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -q coveralls:report
     exit $?
 fi
