63afddab4501394ce181632274ef7c8f3fb3d29e
Jochen Schalanda <jochen@schalanda.name>
2019-11-24 08:19:39 +0100

    Fix JavaVersionTest
    Test methods have to be public with JUnit 4.x.

--- dropwizard-util/src/test/java/io/dropwizard/util/JavaVersionTest.java
+++ dropwizard-util/src/test/java/io/dropwizard/util/JavaVersionTest.java
@@ -6,19 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaVersionTest {
     @Test
-    void isJava8_returns_false_if_specVersion_cannot_be_read() {
+    public void isJava8_returns_false_if_specVersion_cannot_be_read() {
         System.clearProperty("java.specification.version");
         assertThat(JavaVersion.isJava8()).isFalse();
     }
 
     @Test
-    void isJava8_returns_true_if_specVersion_is_Java_8() {
+    public void isJava8_returns_true_if_specVersion_is_Java_8() {
         System.setProperty("java.specification.version", "1.8.0_222");
         assertThat(JavaVersion.isJava8()).isTrue();
     }
 
     @Test
-    void isJava8_returns_true_if_specVersion_is_Java_11() {
+    public void isJava8_returns_true_if_specVersion_is_Java_11() {
         System.setProperty("java.specification.version", "11");
         assertThat(JavaVersion.isJava8()).isFalse();
     }
