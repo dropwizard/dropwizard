d2613326eec5e1e098c61cd4752cc6b659e07d4f
Philip K. Warren <pkwarren@gmail.com>
2019-12-17 11:08:51 -0600

    Prefer jakarta apis over older javax apis.
    Update to use the latest jakarta apis in all dropwizard modules instead
    of a mix of jakarta and javax.* ones (which ship the same classes). This
    should make the APIs on the classpath consistent when creating a shaded
    application jar and also avoid warnings about duplicate classes during
    shading.

--- dropwizard-auth/src/main/java/io/dropwizard/auth/CachingAuthorizer.java
+++ dropwizard-auth/src/main/java/io/dropwizard/auth/CachingAuthorizer.java
@@ -21,45 +21,6 @@ import java.util.stream.Collectors;
 
 import static com.codahale.metrics.MetricRegistry.name;
 
-class AuthorizationContext<P extends Principal> {
-    private final P principal;
-    private final String role;
-    private final @Nullable ContainerRequestContext requestContext;
-
-    AuthorizationContext(P principal, String role, @Nullable ContainerRequestContext requestContext) {
-        this.principal = principal;
-        this.role = role;
-        this.requestContext = requestContext;
-    }
-
-    P getPrincipal() {
-        return principal;
-    }
-
-    String getRole() {
-        return role;
-    }
-
-    @Nullable ContainerRequestContext getRequestContext() {
-        return requestContext;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AuthorizationContext<?> that = (AuthorizationContext<?>) o;
-        return Objects.equals(principal, that.principal) &&
-            Objects.equals(role, that.role) &&
-            Objects.equals(requestContext, that.requestContext);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(principal, role, requestContext);
-    }
-}
-
 /**
  * An {@link Authorizer} decorator which uses a {@link Caffeine} cache to
  * temporarily cache principals' role associations.
