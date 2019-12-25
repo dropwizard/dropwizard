483ee34006546002f1b524d7f9f000f841a3bd48
Harry <harryehowarth@gmail.com>
2019-12-25 00:40:52 +0000

    Expand imports

--- dropwizard-example/src/main/java/com/example/helloworld/core/Person.java
+++ dropwizard-example/src/main/java/com/example/helloworld/core/Person.java
@@ -1,6 +1,13 @@
 package com.example.helloworld.core;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import java.util.Objects;
 
--- dropwizard-example/src/main/java/com/example/helloworld/resources/HelloWorldResource.java
+++ dropwizard-example/src/main/java/com/example/helloworld/resources/HelloWorldResource.java
@@ -9,7 +9,11 @@ import io.dropwizard.jersey.params.DateTimeParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
