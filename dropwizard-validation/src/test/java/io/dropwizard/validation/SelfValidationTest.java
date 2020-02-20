aa9ae9689d7d215f6115efb5850160e0a8cd35e5
Jochen Schalanda <jochen@schalanda.name>
2020-02-20 02:36:40 +0100

    Escape EL expressions in ViolationCollector
    Fixes #3153
    Refs #3157
    (cherry picked from commit d87d1e4f8e20f6494c0232bf8560c961b46db634)

--- dropwizard-validation/src/test/java/io/dropwizard/validation/SelfValidationTest.java
+++ dropwizard-validation/src/test/java/io/dropwizard/validation/SelfValidationTest.java
@@ -10,11 +10,11 @@ import javax.validation.Validator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelfValidationTest {
-
     private static final String FAILED = "failed";
 
     @SelfValidating
     public static class FailingExample {
+        @SuppressWarnings("unused")
         @SelfValidation
         public void validateFail(ViolationCollector col) {
             col.addViolation(FAILED);
@@ -23,6 +23,7 @@ public class SelfValidationTest {
 
     @SelfValidating
     public static class DirectContextExample {
+        @SuppressWarnings("unused")
         @SelfValidation
         public void validateFail(ViolationCollector col) {
             col.getContext().buildConstraintViolationWithTemplate(FAILED).addConstraintViolation();
@@ -51,34 +52,44 @@ public class SelfValidationTest {
             col.addViolation(FAILED);
         }
 
+        @SuppressWarnings("unused")
         @SelfValidation
         public boolean validateFailReturn(ViolationCollector col) {
             col.addViolation(FAILED);
             return true;
         }
 
+        @SuppressWarnings("unused")
         @SelfValidation
         private void validateFailPrivate(ViolationCollector col) {
             col.addViolation(FAILED);
         }
     }
 
-
     @SelfValidating
     public static class ComplexExample {
+        @SuppressWarnings("unused")
         @SelfValidation
         public void validateFail1(ViolationCollector col) {
             col.addViolation(FAILED + "1");
         }
 
+        @SuppressWarnings("unused")
         @SelfValidation
         public void validateFail2(ViolationCollector col) {
-            col.addViolation(FAILED + "2");
+            col.addViolation("p2", FAILED);
         }
 
+        @SuppressWarnings("unused")
         @SelfValidation
         public void validateFail3(ViolationCollector col) {
-            col.addViolation(FAILED + "3");
+            col.addViolation("p", 3, FAILED);
+        }
+
+        @SuppressWarnings("unused")
+        @SelfValidation
+        public void validateFail4(ViolationCollector col) {
+            col.addViolation("p", "four", FAILED);
         }
 
         @SuppressWarnings("unused")
@@ -91,42 +102,54 @@ public class SelfValidationTest {
     public static class NoValidations {
     }
 
+    @SelfValidating
+    public static class InjectionExample {
+        @SuppressWarnings("unused")
+        @SelfValidation
+        public void validateFail(ViolationCollector col) {
+            col.addViolation("${'value'}");
+            col.addViolation("${'property'}", "${'value'}");
+            col.addViolation("${'property'}", 1, "${'value'}");
+            col.addViolation("${'property'}", "${'key'}", "${'value'}");
+        }
+    }
+
     private final Validator validator = BaseValidator.newValidator();
 
     @Test
-    public void failingExample() throws Exception {
+    public void failingExample() {
         assertThat(ConstraintViolations.format(validator.validate(new FailingExample())))
             .containsOnly(" " + FAILED);
     }
 
     @Test
-    public void correctExample() throws Exception {
+    public void correctExample() {
         assertThat(ConstraintViolations.format(validator.validate(new CorrectExample())))
             .isEmpty();
     }
 
     @Test
-    public void multipleTestingOfSameClass() throws Exception {
+    public void multipleTestingOfSameClass() {
         assertThat(ConstraintViolations.format(validator.validate(new CorrectExample())))
-            .isEmpty();
+                .isEmpty();
         assertThat(ConstraintViolations.format(validator.validate(new CorrectExample())))
             .isEmpty();
     }
 
     @Test
-    public void testDirectContextUsage() throws Exception {
+    public void testDirectContextUsage() {
         assertThat(ConstraintViolations.format(validator.validate(new DirectContextExample())))
             .containsOnly(" " + FAILED);
     }
 
     @Test
-    public void complexExample() throws Exception {
+    public void complexExample() {
         assertThat(ConstraintViolations.format(validator.validate(new ComplexExample())))
-            .containsOnly(
-                " " + FAILED + "1",
-                " " + FAILED + "2",
-                " " + FAILED + "3"
-            );
+                .containsExactly(
+                        " failed1",
+                        "p2 failed",
+                        "p[3] failed",
+                        "p[four] failed");
     }
 
     @Test
@@ -136,8 +159,18 @@ public class SelfValidationTest {
     }
 
     @Test
-    public void giveWarningIfNoValidationMethods() throws Exception {
+    public void giveWarningIfNoValidationMethods() {
         assertThat(ConstraintViolations.format(validator.validate(new NoValidations())))
             .isEmpty();
+    }
+
+    @Test
+    public void violationMessagesAreEscaped() {
+        assertThat(ConstraintViolations.format(validator.validate(new InjectionExample()))).containsExactly(
+                " ${'value'}",
+                "${'property'} ${'value'}",
+                "${'property'}[${'key'}] ${'value'}",
+                "${'property'}[1] ${'value'}"
+        );
     }
 }
