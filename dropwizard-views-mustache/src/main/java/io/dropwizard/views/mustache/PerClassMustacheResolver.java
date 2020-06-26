1f94d50f6d567baec3a4ae7633d2d62b87608a28
Oleg Zenchenko <oleg.zenchenko.at@gmail.com>
2020-06-26 00:16:15 -0400

    3311. Replace jsr305 with spotbugs annotations in dropwizard-views-mustache

--- dropwizard-views-mustache/src/main/java/io/dropwizard/views/mustache/PerClassMustacheResolver.java
+++ dropwizard-views-mustache/src/main/java/io/dropwizard/views/mustache/PerClassMustacheResolver.java
@@ -3,7 +3,7 @@ package io.dropwizard.views.mustache;
 import com.github.mustachejava.MustacheResolver;
 import io.dropwizard.views.View;
 
-import javax.annotation.Nullable;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
