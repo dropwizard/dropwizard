fd7db7063ce9fdb0db80c3c0af330b052870a1a0
Jochen Schalanda <jochen@schalanda.name>
2020-07-29 12:56:17 +0200

    Add Jersey bean validation end-to-end test (#3391)

---
+++ dropwizard-e2e/src/main/java/com/example/validation/ValidatedBean.java
@@ -0,0 +1,44 @@
+package com.example.validation;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+public class ValidatedBean {
+    @JsonProperty("string")
+    @NotBlank
+    private final String string;
+
+    @JsonProperty("number")
+    @PositiveOrZero
+    private final Long number;
+
+    @JsonProperty("list")
+    @Size(min = 1, max = 3)
+    private final Collection<@NotBlank String> list;
+
+    @JsonCreator
+    public ValidatedBean(@JsonProperty("string") String string,
+                         @JsonProperty("number") Long number,
+                         @JsonProperty("list") Collection<String> list) {
+        this.string = string;
+        this.number = number;
+        this.list = list;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public Collection<String> getList() {
+        return list;
+    }
+}
--- dropwizard-e2e/src/main/java/com/example/validation/ValidatedResource.java
+++ dropwizard-e2e/src/main/java/com/example/validation/ValidatedResource.java
@@ -2,11 +2,15 @@ package com.example.validation;
 
 import io.dropwizard.validation.OneOf;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -22,5 +26,16 @@ public class ValidatedResource {
     @Path("/default")
     public void defaultValidation(@QueryParam("value") @OneOf("right") String value) {
         //Do nothing
+    }
+
+    @POST
+    @Path("/bean-validation")
+    public void beanValidation(@Valid @NotNull ValidatedBean bean) {
+        if (bean == null
+            || bean.getString() == null || bean.getString().trim().isEmpty()
+            || bean.getNumber() < 0
+            || bean.getList().size() < 1) {
+            throw new IllegalArgumentException();
+        }
     }
 }
