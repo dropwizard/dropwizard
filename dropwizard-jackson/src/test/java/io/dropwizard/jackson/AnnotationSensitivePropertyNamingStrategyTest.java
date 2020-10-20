af3f257f21640365d5526f88f3e6930864b27556
Jochen Schalanda <jochen@schalanda.name>
2020-10-20 08:24:23 +0200

    Add null-checks to AnnotationSensitivePropertyNamingStrategy
    Fixes #3514

--- dropwizard-jackson/src/test/java/io/dropwizard/jackson/AnnotationSensitivePropertyNamingStrategyTest.java
+++ dropwizard-jackson/src/test/java/io/dropwizard/jackson/AnnotationSensitivePropertyNamingStrategyTest.java
@@ -3,12 +3,15 @@ package io.dropwizard.jackson;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.datatype.guava.deser.util.RangeHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Nullable;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class AnnotationSensitivePropertyNamingStrategyTest {
     public static class RegularExample {
@@ -68,5 +71,43 @@ public class AnnotationSensitivePropertyNamingStrategyTest {
     public void deserializesSnakeCaseProperties() throws Exception {
         assertThat(mapper.readValue("{\"first_name\":\"woo\"}", SnakeCaseExample.class).firstName)
                 .isEqualTo("woo");
+    }
+
+    @Test
+    public void nameForConstructorParameterWorksWithNullField() throws Exception {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForConstructorParameter(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    public void nameForFieldWorksWithNullField() throws Exception {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForField(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    public void nameForGetterMethodWorksWithNullField() throws Exception {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForGetterMethod(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    public void nameForSetterMethodWorksWithNullField() throws Exception {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForSetterMethod(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    // https://github.com/dropwizard/dropwizard/issues/3514
+    public void usingRangeHelperDoesNotThrowNullPointerException() throws Exception {
+        final RangeHelper.RangeProperties standardNames = RangeHelper.standardNames();
+        assertThat(standardNames.lowerBoundType).isNotBlank();
+        assertThat(standardNames.lowerEndpoint).isNotBlank();
+        assertThat(standardNames.upperBoundType).isNotBlank();
+        assertThat(standardNames.upperEndpoint).isNotBlank();
     }
 }
