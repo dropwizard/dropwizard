5aaf55a57fca50bfa053c1ad1607938762fff8e0
Jochen Schalanda <jochen@schalanda.name>
2020-10-20 10:45:31 +0200

    Add null-checks to AnnotationSensitivePropertyNamingStrategy (#3515)
    Refs FasterXML/jackson-datatypes-collections#79
    Fixes #3514
    
    (cherry picked from commit 402062a685da0bd96d6f3d03eca3e74284f1379c)

--- dropwizard-jackson/src/test/java/io/dropwizard/jackson/AnnotationSensitivePropertyNamingStrategyTest.java
+++ dropwizard-jackson/src/test/java/io/dropwizard/jackson/AnnotationSensitivePropertyNamingStrategyTest.java
@@ -3,14 +3,17 @@ package io.dropwizard.jackson;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.datatype.guava.deser.util.RangeHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Nullable;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
-public class AnnotationSensitivePropertyNamingStrategyTest {
+class AnnotationSensitivePropertyNamingStrategyTest {
     public static class RegularExample {
         @JsonProperty
         @Nullable
@@ -42,31 +45,69 @@ public class AnnotationSensitivePropertyNamingStrategyTest {
     private final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeEach
-    public void setUp() throws Exception {
+    void setUp() {
         mapper.setPropertyNamingStrategy(strategy);
     }
 
     @Test
-    public void serializesRegularProperties() throws Exception {
+    void serializesRegularProperties() throws Exception {
         assertThat(mapper.writeValueAsString(new RegularExample("woo")))
                 .isEqualTo("{\"firstName\":\"woo\"}");
     }
 
     @Test
-    public void serializesSnakeCaseProperties() throws Exception {
+    void serializesSnakeCaseProperties() throws Exception {
         assertThat(mapper.writeValueAsString(new SnakeCaseExample("woo")))
                 .isEqualTo("{\"first_name\":\"woo\"}");
     }
 
     @Test
-    public void deserializesRegularProperties() throws Exception {
+    void deserializesRegularProperties() throws Exception {
         assertThat(mapper.readValue("{\"firstName\":\"woo\"}", RegularExample.class).firstName)
                 .isEqualTo("woo");
     }
 
     @Test
-    public void deserializesSnakeCaseProperties() throws Exception {
+    void deserializesSnakeCaseProperties() throws Exception {
         assertThat(mapper.readValue("{\"first_name\":\"woo\"}", SnakeCaseExample.class).firstName)
                 .isEqualTo("woo");
+    }
+
+    @Test
+    void nameForConstructorParameterWorksWithNullField() {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForConstructorParameter(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    void nameForFieldWorksWithNullField() {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForField(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    void nameForGetterMethodWorksWithNullField() {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForGetterMethod(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    void nameForSetterMethodWorksWithNullField() {
+        final MapperConfig<?> mapperConfig = mock(MapperConfig.class);
+        final String name = strategy.nameForSetterMethod(mapperConfig, null, "defaultName");
+        assertThat(name).isEqualTo("defaultName");
+    }
+
+    @Test
+    // https://github.com/dropwizard/dropwizard/issues/3514
+    void usingRangeHelperDoesNotThrowNullPointerException() {
+        final RangeHelper.RangeProperties standardNames = RangeHelper.standardNames();
+        assertThat(standardNames.lowerBoundType).isNotBlank();
+        assertThat(standardNames.lowerEndpoint).isNotBlank();
+        assertThat(standardNames.upperBoundType).isNotBlank();
+        assertThat(standardNames.upperEndpoint).isNotBlank();
     }
 }
