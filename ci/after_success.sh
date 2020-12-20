f128d4243a2fdc247637e38921c2a755f9c54afc
Jochen Schalanda <jochen@schalanda.name>
2020-12-20 16:28:47 +0100

    Remove Travis CI configuration
    (cherry picked from commit 80589ad65a225e45cb5f1108021aa5d55fb02846)

--- ci/after_success.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -uxo pipefail
-
-if [[ "${TRAVIS_JDK_VERSION}" == "openjdk8" ]]; then
-    ./mvnw -V -B -ff -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -q coveralls:report
-    exit $?
-fi
