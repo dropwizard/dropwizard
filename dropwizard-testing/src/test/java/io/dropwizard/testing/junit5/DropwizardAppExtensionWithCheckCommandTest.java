b04523139260eddf667e8110a7ae0178d946c769
Jochen Schalanda <jochen@schalanda.name>
2021-01-29 16:20:51 +0100

    Add support for testing plain Command classes
    `DropwizardTestSupport` didn't support running plain `Command` or `ConfiguredCommand<C>` classes which have been with the `commandInstantiator` parameter in the `DropwizardAppExtension` extension (JUnit 5) or `DropwizardAppRule` rule (JUnit 4).

---
+++ dropwizard-testing/src/test/java/io/dropwizard/testing/junit5/DropwizardAppExtensionWithCheckCommandTest.java
@@ -0,0 +1,34 @@
+package io.dropwizard.testing.junit5;
+
+import io.dropwizard.cli.CheckCommand;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.app.DropwizardTestApplication;
+import io.dropwizard.testing.app.TestConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+class DropwizardAppExtensionWithCheckCommandTest {
+
+    private static final DropwizardAppExtension<TestConfiguration> EXTENSION = new DropwizardAppExtension<>(
+        DropwizardTestApplication.class, "test-config.yaml", new ResourceConfigurationSourceProvider(), null, CheckCommand::new);
+
+
+    @Test
+    void returnsConfiguration() {
+        assertThat(EXTENSION.getConfiguration().getMessage()).isEqualTo("Yes, it's here");
+    }
+
+    @Test
+    void returnsApplication() {
+        assertThat(EXTENSION.<DropwizardTestApplication>getApplication()).isNotNull();
+    }
+
+    @Test
+    void environmentIsNull() {
+        assertThatNullPointerException().isThrownBy(EXTENSION::getEnvironment);
+    }
+}
---
+++ dropwizard-testing/src/test/java/io/dropwizard/testing/junit5/DropwizardAppExtensionWithConfiguredCommandTest.java
@@ -0,0 +1,42 @@
+package io.dropwizard.testing.junit5;
+
+import io.dropwizard.cli.ConfiguredCommand;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.testing.app.DropwizardTestApplication;
+import io.dropwizard.testing.app.TestConfiguration;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+class DropwizardAppExtensionWithConfiguredCommandTest {
+
+    private static final DropwizardAppExtension<TestConfiguration> EXTENSION = new DropwizardAppExtension<>(
+        DropwizardTestApplication.class, "test-config.yaml", new ResourceConfigurationSourceProvider(), null,
+        application -> new ConfiguredCommand<TestConfiguration>("test", "Test command") {
+            @Override
+            protected void run(Bootstrap<TestConfiguration> bootstrap, Namespace namespace, TestConfiguration configuration) throws Exception {
+
+            }
+        });
+
+
+    @Test
+    void returnsConfiguration() {
+        assertThat(EXTENSION.getConfiguration().getMessage()).isEqualTo("Yes, it's here");
+    }
+
+    @Test
+    void returnsApplication() {
+        assertThat(EXTENSION.<DropwizardTestApplication>getApplication()).isNotNull();
+    }
+
+    @Test
+    void environmentIsNull() {
+        assertThatNullPointerException().isThrownBy(EXTENSION::getEnvironment);
+    }
+}
