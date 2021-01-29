b59bd030e9aad31830284eb77e511b0c8d39498a
Artem Prigoda <arteamon@gmail.com>
2021-01-29 17:04:03 +0100

    Add JavaVersion unit tests

--- dropwizard-util/src/test/java/io/dropwizard/util/JavaVersionTest.java
+++ dropwizard-util/src/test/java/io/dropwizard/util/JavaVersionTest.java
@@ -1,6 +1,8 @@
 package io.dropwizard.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +23,17 @@ class JavaVersionTest {
     void isJava8_returns_true_if_specVersion_is_Java_11() {
         System.setProperty("java.specification.version", "11");
         assertThat(JavaVersion.isJava8()).isFalse();
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_15})
+    void isJava11_or_higher_returns_true_on_Java_11_15() {
+        assertThat(JavaVersion.isJava11OrHigher()).isTrue();
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void isJava11_or_higher_returns_false_on_Java_8() {
+        assertThat(JavaVersion.isJava11OrHigher()).isTrue();
     }
 }
