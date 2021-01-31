c85a7d639fb7be7cc16d269ab37ec7a448c478f2
Jochen Schalanda <jochen@schalanda.name>
2021-01-31 15:09:13 +0100

    Try making BadLogTest less flaky
    Disable test on Windows:
    ```
    java.io.IOException: Failed to delete temp directory C:\Users\appveyor\AppData\Local\Temp\1\junit7886399651463695950. The following paths could not be deleted (see suppressed exceptions for details): , example.log
    ```

--- dropwizard-e2e/src/test/java/com/example/badlog/BadLogTest.java
+++ dropwizard-e2e/src/test/java/com/example/badlog/BadLogTest.java
@@ -4,9 +4,11 @@ import io.dropwizard.Configuration;
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.DropwizardTestSupport;
 import io.dropwizard.testing.ResourceHelpers;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,52 +17,53 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
 
-public class BadLogTest {
+@DisabledOnOs(OS.WINDOWS)
+class BadLogTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(BadLogTest.class);
-    private static ByteArrayOutputStream out;
-    private static ByteArrayOutputStream err;
-    private static PrintStream oldOut = System.out;
-    private static PrintStream oldErr = System.err;
+    private static final PrintStream oldOut = System.out;
+    private static final PrintStream oldErr = System.err;
+    private ByteArrayOutputStream out;
+    private ByteArrayOutputStream err;
 
     @BeforeEach
-    public void setup() throws Exception {
+    void setup() throws Exception {
         out = new ByteArrayOutputStream();
         err = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         System.setErr(new PrintStream(err));
     }
 
-    @AfterEach
-    public void teardown() {
+    @AfterAll
+    static void teardown() {
         System.setOut(oldOut);
         System.setErr(oldErr);
     }
 
     @Test
-    public void thatLoggingIsntBrokenOnCleanup() throws Exception {
-        BadLogApp.runMe(new String[]{"server"});
+    void thatLoggingIsNotBrokenOnCleanup() throws Exception {
+        new BadLogApp().run("server");
         LOGGER.info("I'm after the test");
         Thread.sleep(100);
 
-        assertThat(new String(out.toByteArray(), UTF_8))
+        assertThat(out.toByteArray()).asString(UTF_8)
             .contains("Mayday we're going down")
             .contains("I'm after the test");
 
-        assertThat(new String(err.toByteArray(), UTF_8))
+        assertThat(err.toByteArray()).asString(UTF_8)
             .contains("I'm a bad app");
     }
 
     @Test
-    public void testSupportShouldResetLogging(@TempDir Path tempDir) throws Exception {
-        final Path logFile = tempDir.resolve("example.log");
-
+    void testSupportShouldResetLogging(@TempDir Path tempDir) throws Exception {
         // Clear out the log file
-        Files.write(logFile, new byte[]{});
+        final Path logFile = Files.write(tempDir.resolve("example.log"), new byte[0]);
 
         final ConfigOverride logOverride = ConfigOverride.config("logging.appenders[0].currentLogFilename", logFile.toString());
         final String configPath = ResourceHelpers.resourceFilePath("badlog/config.yaml");
@@ -76,19 +79,19 @@ public class BadLogTest {
         // Explicitly run the command so that the fatal error function runs
         app.getApplication().run("server", configPath);
         app.after();
+        Thread.sleep(100L);
 
         // Since our dropwizard app is only using the file appender, the console
         // appender should not contain our logging statements
-        assertThat(new String(out.toByteArray(), UTF_8))
-            .doesNotContain("Mayday we're going down");
+        assertThat(out.toByteArray()).asString(UTF_8).doesNotContain("Mayday we're going down");
         out.reset();
 
         // and the file should have our logging statements
-        final String contents = new String(Files.readAllBytes(logFile), UTF_8);
-        assertThat(contents).contains("Mayday we're going down");
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(
+                () -> assertThat(Files.readAllBytes(logFile)).asString(UTF_8).contains("Mayday we're going down"));
 
         // Clear out the log file
-        Files.write(logFile, new byte[]{});
+        Files.write(logFile, new byte[0]);
 
         final DropwizardTestSupport<Configuration> app2 = new DropwizardTestSupport<>(BadLogApp.class, new Configuration());
         assertThatThrownBy(app2::before).hasMessage("I'm a bad app");
@@ -96,10 +99,10 @@ public class BadLogTest {
         // Explicitly run the command so that the fatal error function runs
         app2.getApplication().run("server");
         app2.after();
+        Thread.sleep(100L);
 
         // Now the console appender will see the fatal error
-        assertThat(new String(out.toByteArray(), UTF_8))
-            .contains("Mayday we're going down");
+        assertThat(out.toByteArray()).asString(UTF_8).contains("Mayday we're going down");
         out.reset();
 
         // and the old log file shouldn't
@@ -109,7 +112,7 @@ public class BadLogTest {
         // And for the final set of assertions will make sure that going back to the app
         // that logs to a file is still behaviorally correct.
         // Clear out the log file
-        Files.write(logFile, new byte[]{});
+        Files.write(logFile, new byte[0]);
 
         final DropwizardTestSupport<Configuration> app3 = new DropwizardTestSupport<>(BadLogApp.class, configPath, logOverride);
         assertThatThrownBy(app3::before).hasMessage("I'm a bad app");
@@ -120,14 +123,14 @@ public class BadLogTest {
         // Explicitly run the command so that the fatal error function runs
         app3.getApplication().run("server", configPath);
         app3.after();
+        Thread.sleep(100L);
 
         // Since our dropwizard app is only using the file appender, the console
         // appender should not contain our logging statements
-        assertThat(new String(out.toByteArray(), UTF_8))
-            .doesNotContain("Mayday we're going down");
+        assertThat(out.toByteArray()).asString(UTF_8).doesNotContain("Mayday we're going down");
 
         // and the file should have our logging statements
-        final String contents3 = new String(Files.readAllBytes(logFile), UTF_8);
-        assertThat(contents3).contains("Mayday we're going down");
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(
+                () -> assertThat(Files.readAllBytes(logFile)).asString(UTF_8).contains("Mayday we're going down"));
     }
 }
