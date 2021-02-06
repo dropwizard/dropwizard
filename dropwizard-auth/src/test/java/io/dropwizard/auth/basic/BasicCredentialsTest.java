c46a863738173033630c1a04a65ba4d76645745b
Russell Howe <rhowe@siksai.co.uk>
2021-02-06 14:29:19 +0000

    Address SonarCloud concerns in dropwizard-auth (#3688)
    Sonar didn't like the accesses of static fields on a derived type, preferring
    direct access to the supertype instead.
    
    Also, some assertions in the tests can be chained and we can use a few more
    specialised AssertJ assertions to make the tests more expressive.

--- dropwizard-auth/src/test/java/io/dropwizard/auth/basic/BasicCredentialsTest.java
+++ dropwizard-auth/src/test/java/io/dropwizard/auth/basic/BasicCredentialsTest.java
@@ -8,35 +8,37 @@ public class BasicCredentialsTest {
     private final BasicCredentials credentials = new BasicCredentials("u", "p");
 
     @Test
-    public void hasAUsername() throws Exception {
+    public void hasAUsername() {
         assertThat(credentials.getUsername()).isEqualTo("u");
     }
 
     @Test
-    public void hasAPassword() throws Exception {
+    public void hasAPassword() {
         assertThat(credentials.getPassword()).isEqualTo("p");
     }
 
     @Test
-    @SuppressWarnings({ "ObjectEqualsNull", "EqualsBetweenInconvertibleTypes", "LiteralAsArgToStringEquals" })
-    public void hasAWorkingEqualsMethod() throws Exception {
-        assertThat(credentials).isEqualTo(credentials);
-        assertThat(credentials).isEqualTo(new BasicCredentials("u", "p"));
-        assertThat(credentials).isNotEqualTo(null);
-        assertThat(credentials).isNotEqualTo("string");
-        assertThat(credentials).isNotEqualTo(new BasicCredentials("u1", "p"));
-        assertThat(credentials).isNotEqualTo(new BasicCredentials("u", "p1"));
+    @SuppressWarnings({ "ObjectEqualsNull", "LiteralAsArgToStringEquals" })
+    public void hasAWorkingEqualsMethod() {
+        assertThat(credentials)
+            .isEqualTo(credentials)
+            .isEqualTo(new BasicCredentials("u", "p"))
+            .isNotEqualTo(null)
+            .isNotEqualTo("string")
+            .isNotEqualTo(new BasicCredentials("u1", "p"))
+            .isNotEqualTo(new BasicCredentials("u", "p1"));
     }
 
     @Test
-    public void hasAWorkingHashCode() throws Exception {
-        assertThat(credentials.hashCode()).isEqualTo(new BasicCredentials("u", "p").hashCode());
-        assertThat(credentials.hashCode()).isNotEqualTo(new BasicCredentials("u1", "p").hashCode());
-        assertThat(credentials.hashCode()).isNotEqualTo(new BasicCredentials("u", "p1").hashCode());
+    public void hasAWorkingHashCode() {
+        assertThat(credentials.hashCode())
+            .hasSameHashCodeAs(new BasicCredentials("u", "p"))
+            .isNotEqualTo(new BasicCredentials("u1", "p").hashCode())
+            .isNotEqualTo(new BasicCredentials("u", "p1").hashCode());
     }
 
     @Test
-    public void isHumanReadable() throws Exception {
-        assertThat(credentials.toString()).isEqualTo("BasicCredentials{username=u, password=**********}");
+    public void isHumanReadable() {
+        assertThat(credentials).hasToString("BasicCredentials{username=u, password=**********}");
     }
 }
