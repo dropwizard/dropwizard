c8136a8eafccf341ff352c5bf02f2984a1ef337c
Jochen Schalanda <jochen@schalanda.name>
2021-03-21 12:10:16 +0100

    Ensure correct TreeTraversingParser initialization in BaseConfigurationFactory (#3800)
    The `TreeTraversingParser` in `BaseConfigurationFactory#build(JsonNode node, String path)` wasn't initialized correctly so that configuration classes including properties with custom deserializers or other customizations during deserialization which require an `ObjectCodec` instance couldn't be deserialized because `ObjectCodec` was `null`.
    
    Thanks a lot to @isaki for investigating the issue and providing comprehensive tests to reproduce the bug and to @IdamkinI for providing more test cases.
    
    Fixes #1991
    Fixes #3796
    
    (cherry picked from commit c22291d5c65dfd50fe0b322b3013a7eaf7fa6e7c)

---
+++ dropwizard-configuration/src/test/java/io/dropwizard/configuration/Issue3796Test.java
@@ -0,0 +1,58 @@
+package io.dropwizard.configuration;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.TextNode;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.validation.BaseValidator;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NullAway")
+class Issue3796Test {
+    @Test
+    void configurationWithCustomDeserializerCanBeRead() throws IOException, ConfigurationException {
+        final ConfigurationFactory<CustomConfiguration> factory = new YamlConfigurationFactory<>(CustomConfiguration.class, BaseValidator.newValidator(), Jackson.newObjectMapper(), "dw");
+        final CustomConfiguration testObject = factory.build(new ResourceConfigurationSourceProvider(), "issue-3796.yml");
+
+        assertThat(testObject).isNotNull();
+        assertThat(testObject.customProperty).isNotNull();
+        assertThat(testObject.customProperty.customString).isEqualTo("hello, world");
+    }
+
+    static class CustomConfiguration {
+        @Nullable
+        public CustomProperty customProperty;
+    }
+
+    @JsonDeserialize(using = CustomDeserializer.class)
+    static class CustomProperty {
+        final String customString;
+
+        CustomProperty(String customString) {
+            this.customString = customString;
+        }
+    }
+
+    static class CustomDeserializer extends StdDeserializer<CustomProperty> {
+        public CustomDeserializer() {
+            super(CustomProperty.class);
+        }
+
+        @Override
+        public CustomProperty deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+            assertThat(parser.getCodec()).isNotNull();
+
+            TreeNode treeNode = parser.readValueAsTree();
+            final TextNode custom = (TextNode) treeNode.path("custom");
+            return new CustomProperty(custom.asText());
+        }
+    }
+}
