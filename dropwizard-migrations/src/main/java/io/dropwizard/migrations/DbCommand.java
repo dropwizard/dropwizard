ce0975d8994a4fa001847b5c5a1aea43f5b08684
Steffen Nießing <36885591+zUniQueX@users.noreply.github.com>
2021-08-19 19:18:01 +0200

    If scoped objects are provided, enter a child scope before liquibase execution and exit it afterwards

--- dropwizard-migrations/src/main/java/io/dropwizard/migrations/DbCommand.java
+++ dropwizard-migrations/src/main/java/io/dropwizard/migrations/DbCommand.java
@@ -3,9 +3,12 @@ package io.dropwizard.migrations;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DatabaseConfiguration;
 import liquibase.Liquibase;
+import liquibase.Scope;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 
+import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -14,8 +17,14 @@ import static java.util.Objects.requireNonNull;
 public class DbCommand<T extends Configuration> extends AbstractLiquibaseCommand<T> {
     private static final String COMMAND_NAME_ATTR = "subcommand";
     private final SortedMap<String, AbstractLiquibaseCommand<T>> subcommands;
+    @Nullable
+    private final Map<String, Object> scopedObjects;
 
     public DbCommand(String name, DatabaseConfiguration<T> strategy, Class<T> configurationClass, String migrationsFileName) {
+        this(name, strategy, configurationClass, migrationsFileName, null);
+    }
+
+    public DbCommand(String name, DatabaseConfiguration<T> strategy, Class<T> configurationClass, String migrationsFileName, @Nullable Map<String, Object> scopedObjects) {
         super(name, "Run database migration tasks", strategy, configurationClass, migrationsFileName);
         this.subcommands = new TreeMap<>();
         addSubcommand(new DbCalculateChecksumCommand<>(strategy, configurationClass, migrationsFileName));
@@ -31,6 +40,7 @@ public class DbCommand<T extends Configuration> extends AbstractLiquibaseCommand
         addSubcommand(new DbStatusCommand<>(strategy, configurationClass, migrationsFileName));
         addSubcommand(new DbTagCommand<>(strategy, configurationClass, migrationsFileName));
         addSubcommand(new DbTestCommand<>(strategy, configurationClass, migrationsFileName));
+        this.scopedObjects = scopedObjects;
     }
 
     private void addSubcommand(AbstractLiquibaseCommand<T> subcommand) {
@@ -50,8 +60,15 @@ public class DbCommand<T extends Configuration> extends AbstractLiquibaseCommand
 
     @Override
     public void run(Namespace namespace, Liquibase liquibase) throws Exception {
+        String scopeId = null;
+        if (scopedObjects != null) {
+            scopeId = Scope.enter(scopedObjects);
+        }
         final AbstractLiquibaseCommand<T> subcommand =
             requireNonNull(subcommands.get(namespace.getString(COMMAND_NAME_ATTR)), "Unable find the command");
         subcommand.run(namespace, liquibase);
+        if (scopeId != null) {
+            Scope.exit(scopeId);
+        }
     }
 }
--- dropwizard-migrations/src/main/java/io/dropwizard/migrations/MigrationsBundle.java
+++ dropwizard-migrations/src/main/java/io/dropwizard/migrations/MigrationsBundle.java
@@ -5,6 +5,9 @@ import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.db.DatabaseConfiguration;
 import io.dropwizard.setup.Bootstrap;
 
+import javax.annotation.Nullable;
+import java.util.Map;
+
 public abstract class MigrationsBundle<T extends Configuration> implements ConfiguredBundle<T>, DatabaseConfiguration<T> {
     private static final String DEFAULT_NAME = "db";
     private static final String DEFAULT_MIGRATIONS_FILE = "migrations.xml";
@@ -13,7 +16,7 @@ public abstract class MigrationsBundle<T extends Configuration> implements Confi
     @SuppressWarnings("unchecked")
     public final void initialize(Bootstrap<?> bootstrap) {
         final Class<T> klass = (Class<T>) bootstrap.getApplication().getConfigurationClass();
-        bootstrap.addCommand(new DbCommand<>(name(), this, klass, getMigrationsFileName()));
+        bootstrap.addCommand(new DbCommand<>(name(), this, klass, getMigrationsFileName(), getScopedObjects()));
     }
 
     public String getMigrationsFileName() {
@@ -22,5 +25,15 @@ public abstract class MigrationsBundle<T extends Configuration> implements Confi
 
     public String name() {
         return DEFAULT_NAME;
+    }
+
+    /**
+     * If overridden, enters a new {@link liquibase.Scope}, in which the provided objects are available.
+     *
+     * @return the objects introduced in the created child {@link liquibase.Scope}
+     */
+    @Nullable
+    public Map<String, Object> getScopedObjects() {
+        return null;
     }
 }
