3bb4c7feab76662077cd696bd757b5541fbb7782
Russell Howe <rhowe@siksai.co.uk>
2022-02-15 20:52:11 +0000

    Add a test for `SslReloadTask`
    Validate the behaviour of `SslReloadTask`, i.e. that it performs a
    dry-run of all `SslReload`ers and only performs the reload if they all
    succeed their dry run.
    
    Move the dry run logic into `SslReload` in order to simplify testing.
    
    Deprecate `SslReload#reload(SslContextFactory)` as it is no longer
    required.

--- dropwizard-jetty/src/main/java/io/dropwizard/jetty/SslReload.java
+++ dropwizard-jetty/src/main/java/io/dropwizard/jetty/SslReload.java
@@ -4,7 +4,7 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import java.util.function.Consumer;
 
-/** Provides the ability to modify an existing ssl factory with new configuration options. */
+/** Provides the ability to modify an existing SSL factory with new configuration options. */
 public class SslReload {
     private final SslContextFactory factory;
     private final Consumer<SslContextFactory> configurer;
@@ -18,8 +18,24 @@ public class SslReload {
         getFactory().reload(configurer);
     }
 
+    /**
+     * Reload a given {@link SslContextFactory}'s configuration
+     *
+     * @deprecated
+     */
+    @Deprecated
     public void reload(SslContextFactory factory) throws Exception {
         factory.reload(configurer);
+    }
+
+    /**
+     * Perform a mock configuration reload
+     *
+	  * @since 2.1.0
+     * @throws Exception if the reload failed, e.g. due to invalid configuration
+     */
+    public void reloadDryRun() throws Exception {
+        new SslContextFactory.Server().reload(configurer);
     }
 
     public SslContextFactory getFactory() {
