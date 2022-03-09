ab2be2d342a99e8288db878d4bdcb6a246652eed
Russell Howe <rhowe@siksai.co.uk>
2022-03-09 19:16:40 +0000

    Remove deprecated `Authorizer#authorize(P, String)`

--- dropwizard-example/src/main/java/com/example/helloworld/auth/ExampleAuthorizer.java
+++ dropwizard-example/src/main/java/com/example/helloworld/auth/ExampleAuthorizer.java
@@ -3,10 +3,13 @@ package com.example.helloworld.auth;
 import com.example.helloworld.core.User;
 import io.dropwizard.auth.Authorizer;
 
+import javax.annotation.Nullable;
+import javax.ws.rs.container.ContainerRequestContext;
+
 public class ExampleAuthorizer implements Authorizer<User> {
 
     @Override
-    public boolean authorize(User user, String role) {
+    public boolean authorize(User user, String role, @Nullable ContainerRequestContext ctx) {
         return user.getRoles() != null && user.getRoles().contains(role);
     }
 }
