4d42493c2f6b324206d227ba615bcc9c2e26e32c
Steffen Nießing <zuniquex@protonmail.com>
2022-04-05 21:49:23 +0200

    Implement LifeCycle in LogbackAccessRequestLog to get a properly managed bean

--- dropwizard-request-logging/src/main/java/io/dropwizard/request/logging/LogbackAccessRequestLog.java
+++ dropwizard-request-logging/src/main/java/io/dropwizard/request/logging/LogbackAccessRequestLog.java
@@ -1,12 +1,13 @@
 package io.dropwizard.request.logging;
 
 import ch.qos.logback.access.jetty.RequestLogImpl;
+import org.eclipse.jetty.util.component.LifeCycle;
 
 /**
  * The Dropwizard request log uses logback-access, but we override it to remove the requirement for logback-access.xml
  * based configuration.
  */
-public class LogbackAccessRequestLog extends RequestLogImpl {
+public class LogbackAccessRequestLog extends RequestLogImpl implements LifeCycle {
     @Override
     public void configure() {
         setName("LogbackAccessRequestLog");
