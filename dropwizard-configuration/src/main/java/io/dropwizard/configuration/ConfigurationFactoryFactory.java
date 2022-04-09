78d7f21f5f3a1a489beed26fe76e9ad00a2d663c
Glenn Wallace <glenn.wallace@synacor.com>
2022-04-09 12:37:12 -0400

    Make ConfigurationFactoryFactory#create public

--- dropwizard-configuration/src/main/java/io/dropwizard/configuration/ConfigurationFactoryFactory.java
+++ dropwizard-configuration/src/main/java/io/dropwizard/configuration/ConfigurationFactoryFactory.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.validation.Validator;
 
 public interface ConfigurationFactoryFactory<T> {
-    ConfigurationFactory<T> create(Class<T> klass,
+    public ConfigurationFactory<T> create(Class<T> klass,
             Validator validator,
             ObjectMapper objectMapper,
             String propertyPrefix);
