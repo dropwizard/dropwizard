2bf066b8146db70c70911c8571f9247967f8fac0
Jochen Schalanda <jochen@schalanda.name>
2022-04-17 22:13:47 +0200

    Update EnvironmentVariableLookup.java

--- dropwizard-configuration/src/main/java/io/dropwizard/configuration/EnvironmentVariableLookup.java
+++ dropwizard-configuration/src/main/java/io/dropwizard/configuration/EnvironmentVariableLookup.java
@@ -5,9 +5,8 @@ import org.apache.commons.text.lookup.StringLookup;
 /**
  * A custom {@link StringLookup} implementation using environment variables as lookup source.
  *
- * @deprecated Use a method reference to @link{System#getenv()} directly instead.
+ * @deprecated Use a method reference to {@link System#getenv()} directly instead.
  */
-
 @Deprecated
 public class EnvironmentVariableLookup implements StringLookup {
     /**
