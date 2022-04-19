85016fc6dd7dcffa5dab29c15de927d9d294cec6
Steffen Nießing <zuniquex@protonmail.com>
2022-04-19 23:16:15 +0200

    Allow arbitrary `ContainerRequestFilter` instances in the `InjectingFilter`

--- dropwizard-auth/src/main/java/io/dropwizard/auth/InjectingFilter.java
+++ dropwizard-auth/src/main/java/io/dropwizard/auth/InjectingFilter.java
@@ -11,41 +11,38 @@ import java.io.IOException;
 
 /**
  * A {@link ContainerRequestFilter} decorator providing dependency injection.
- * Thereby the filter must be of type {@link AuthFilter} or {@link WebApplicationExceptionCatchingFilter},
- * from which an {@link AuthFilter} can be obtained.
  *
- * If {@link Authenticator} and {@link Authorizer} instances of this {@link AuthFilter} are not {@code null}, dependency
+ * If an {@link AuthFilter} instance can be obtained from the {@link ContainerRequestFilter} and the
+ * {@link Authenticator} and {@link Authorizer} instances of this {@link AuthFilter} are not {@code null}, dependency
  * injection is provided for these too.
  */
 @Priority(Priorities.AUTHENTICATION)
 class InjectingFilter implements ContainerRequestFilter {
     @Nullable
     private final InjectionManager injectionManager;
-    private final AuthFilter<?, ?> filter;
     private final ContainerRequestFilter rawAuthFilter;
 
     public InjectingFilter(@Nullable InjectionManager injectionManager, ContainerRequestFilter filter) {
         this.injectionManager = injectionManager;
-        ContainerRequestFilter tempAuthFilter = filter;
-        if (filter instanceof WebApplicationExceptionCatchingFilter) {
-            tempAuthFilter = ((WebApplicationExceptionCatchingFilter) filter).getUnderlying();
-        }
-        if (!(tempAuthFilter instanceof AuthFilter)) {
-            throw new IllegalArgumentException("The filter must be an instance of AuthFilter");
-        }
-        this.filter = (AuthFilter<?, ?>) tempAuthFilter;
         this.rawAuthFilter = filter;
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         if (injectionManager != null) {
-            injectionManager.inject(filter);
-            if (filter.authenticator != null) {
-                injectionManager.inject(filter.authenticator);
+            ContainerRequestFilter tempAuthFilter = rawAuthFilter;
+            if (tempAuthFilter instanceof WebApplicationExceptionCatchingFilter) {
+                tempAuthFilter = ((WebApplicationExceptionCatchingFilter) tempAuthFilter).getUnderlying();
             }
-            if (filter.authorizer != null) {
-                injectionManager.inject(filter.authorizer);
+            injectionManager.inject(tempAuthFilter);
+            if (tempAuthFilter instanceof AuthFilter<?, ?>) {
+                AuthFilter<?, ?> filter = (AuthFilter<?, ?>) tempAuthFilter;
+                if (filter.authenticator != null) {
+                    injectionManager.inject(filter.authenticator);
+                }
+                if (filter.authorizer != null) {
+                    injectionManager.inject(filter.authorizer);
+                }
             }
         }
         rawAuthFilter.filter(requestContext);
