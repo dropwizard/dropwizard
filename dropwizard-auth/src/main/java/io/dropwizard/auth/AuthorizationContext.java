9c7e00fbb83c9f0c63d935404e1d8d18ffd1e731
Timothée Peignier <timothee.peignier@icloud.com>
2022-05-02 11:07:59 -0700

    Change access modifier to methods of AuthorizationContext

--- dropwizard-auth/src/main/java/io/dropwizard/auth/AuthorizationContext.java
+++ dropwizard-auth/src/main/java/io/dropwizard/auth/AuthorizationContext.java
@@ -12,21 +12,21 @@ public abstract class AuthorizationContext<P extends Principal> {
     private final @Nullable
     ContainerRequestContext requestContext;
 
-    protected AuthorizationContext(P principal, String role, @Nullable ContainerRequestContext requestContext) {
+    public AuthorizationContext(P principal, String role, @Nullable ContainerRequestContext requestContext) {
         this.principal = principal;
         this.role = role;
         this.requestContext = requestContext;
     }
 
-    protected P getPrincipal() {
+    public P getPrincipal() {
         return principal;
     }
 
-    protected String getRole() {
+    public String getRole() {
         return role;
     }
 
-    @Nullable protected ContainerRequestContext getRequestContext() {
+    @Nullable public ContainerRequestContext getRequestContext() {
         return requestContext;
     }
 
