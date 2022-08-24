ea10b91ed29d6783ab4b9f985f9ffe97a48b026e
Steffen Nießing <zuniquex@protonmail.com>
2022-08-24 22:44:48 +0200

    Fix JavaDoc warnings in `dropwizard-validation`

--- dropwizard-validation/src/main/java/io/dropwizard/validation/InterpolationHelper.java
+++ dropwizard-validation/src/main/java/io/dropwizard/validation/InterpolationHelper.java
@@ -18,9 +18,21 @@ import java.util.regex.Pattern;
  */
 public final class InterpolationHelper {
 
+    /**
+     * A constant representing the '&#123;' character.
+     */
     public static final char BEGIN_TERM = '{';
+    /**
+     * A constant representing the '&#125;' character.
+     */
     public static final char END_TERM = '}';
+    /**
+     * A constant representing the '&#36;' character.
+     */
     public static final char EL_DESIGNATOR = '$';
+    /**
+     * A constant representing the '&#92;' character.
+     */
     public static final char ESCAPE_CHARACTER = '\\';
 
     private static final Pattern ESCAPE_MESSAGE_PARAMETER_PATTERN = Pattern.compile("([\\" + ESCAPE_CHARACTER + BEGIN_TERM + END_TERM + EL_DESIGNATOR + "])");
@@ -28,6 +40,12 @@ public final class InterpolationHelper {
     private InterpolationHelper() {
     }
 
+    /**
+     * Escapes a string with the {@link #ESCAPE_MESSAGE_PARAMETER_PATTERN}.
+     *
+     * @param messageParameter the string to escape
+     * @return the escaped string or {@code null}, if the input was {@code null}
+     */
     @Nullable
     public static String escapeMessageParameter(@Nullable String messageParameter) {
         if (messageParameter == null) {
