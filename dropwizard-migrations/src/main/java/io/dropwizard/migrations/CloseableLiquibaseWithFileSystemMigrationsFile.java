2bb76ac538602fdc1513f0ce4da331fdfc417774
Jochen Schalanda <jochen@schalanda.name>
2022-10-11 11:43:28 +0200

    Fix CloseableLiquibaseWithFileSystemMigrationsFile

--- dropwizard-migrations/src/main/java/io/dropwizard/migrations/CloseableLiquibaseWithFileSystemMigrationsFile.java
+++ dropwizard-migrations/src/main/java/io/dropwizard/migrations/CloseableLiquibaseWithFileSystemMigrationsFile.java
@@ -5,14 +5,18 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
-import liquibase.resource.FileSystemResourceAccessor;
+import liquibase.resource.CompositeResourceAccessor;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 
-import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class CloseableLiquibaseWithFileSystemMigrationsFile extends CloseableLiquibase implements AutoCloseable {
     CloseableLiquibaseWithFileSystemMigrationsFile(
@@ -33,15 +37,22 @@ public class CloseableLiquibaseWithFileSystemMigrationsFile extends CloseableLiq
         FileSystem fileSystem
     ) throws LiquibaseException, SQLException {
         super(file,
-            new FileSystemResourceAccessor(getRootPaths(fileSystem).toArray(new File[0])),
+            new CompositeResourceAccessor(directoryResourceAccessors(fileSystem)),
             database,
             dataSource);
     }
 
-    private static List<File> getRootPaths(FileSystem fileSystem) {
-        List<File> rootPaths = new ArrayList<>();
-        fileSystem.getRootDirectories().forEach(path -> rootPaths.add(path.toFile()));
-        return rootPaths;
+    private static Collection<ResourceAccessor> directoryResourceAccessors(FileSystem fileSystem) {
+        return StreamSupport.stream(fileSystem.getRootDirectories().spliterator(), false)
+                .map(path -> {
+                    try {
+                        return new DirectoryResourceAccessor(path);
+                    } catch (IOException e) {
+                        return null;
+                    }
+                })
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
     }
 
     public CloseableLiquibaseWithFileSystemMigrationsFile(
