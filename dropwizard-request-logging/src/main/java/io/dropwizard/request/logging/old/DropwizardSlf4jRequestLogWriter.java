7d866c5ee1f704ddc24ac12f604e234f5d8235da
Jochen Schalanda <jochen@schalanda.name>
2023-11-04 13:08:19 +0100

    Set `LoggerContext` in `LoggingEvent` (#7835) (#7837)
    Co-authored-by: Steffen Nießing <36885591+zUniQueX@users.noreply.github.com>
    
    Logback 1.4.8 refactored the handling of MDC and now requires a LoggerContext to be set in a LoggingEvent, otherwise an NPE is thrown.
    
    Fixes #7830
    Refs #7835

--- dropwizard-request-logging/src/main/java/io/dropwizard/request/logging/old/DropwizardSlf4jRequestLogWriter.java
+++ dropwizard-request-logging/src/main/java/io/dropwizard/request/logging/old/DropwizardSlf4jRequestLogWriter.java
@@ -1,11 +1,13 @@
 package io.dropwizard.request.logging.old;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.spi.AppenderAttachableImpl;
 import org.eclipse.jetty.server.RequestLog;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -16,9 +18,11 @@ import java.io.IOException;
  */
 public class DropwizardSlf4jRequestLogWriter extends AbstractLifeCycle implements RequestLog.Writer {
     private AppenderAttachableImpl<ILoggingEvent> appenders;
+    private final LoggerContext loggerContext;
 
     DropwizardSlf4jRequestLogWriter(AppenderAttachableImpl<ILoggingEvent> appenders) {
         this.appenders = appenders;
+        this.loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
     }
 
     @Override
@@ -28,6 +32,7 @@ public class DropwizardSlf4jRequestLogWriter extends AbstractLifeCycle implement
         event.setLoggerName("http.request");
         event.setMessage(entry);
         event.setTimeStamp(System.currentTimeMillis());
+        event.setLoggerContext(loggerContext);
 
         appenders.appendLoopOnAppenders(event);
     }
