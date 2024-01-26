1de3eef39871859804dd03b61a3cb1c1488b308c
Natan Abolafya <natan.abolafya@appgate.com>
2024-01-26 10:51:23 +0100

    Unix domain socket connector (#7614)
    Co-authored-by: Steffen Nießing <zuniquex@protonmail.com>
    
    ###### Problem:
    Since Java 16, Unix Domain Sockets are supported natively. And since Jetty 10, there is a ready made connector for it and it is not considered experimental anymore.
    
    The typical use case is a reverse proxy connecting to UNIX socket instead of TCP which has various advantages.
    
    ###### Solution:
    Created a new connector type `unix-socket`.
    
    I've reimplemented #1893 but it needed some adjustments considering how much Dropwizard changed since then.
    
    The test was also a little tricky (and not very pretty) considering there is no UNIX socket support in Apache HTTP client. Or at least I couldn't find any but please correct me otherwise.
    
    ###### Result:
    It's pretty simple to listen on unix sockets now. That was something we wanted in our product for a long time.
    Also this works on Windows since Windows now also supports UNIX domain sockets.
    
    Refs #1893

---
+++ dropwizard-unix-socket/src/test/java/io/dropwizard/unixsocket/UnixSocketConnectorFactoryTest.java
@@ -0,0 +1,81 @@
+package io.dropwizard.unixsocket;
+
+import io.dropwizard.core.Application;
+import io.dropwizard.core.Configuration;
+import io.dropwizard.core.setup.Environment;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.jetty.client.ContentResponse;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.transport.HttpClientTransportOverHTTP;
+import org.eclipse.jetty.io.ClientConnector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisabledOnOs(OS.WINDOWS) // Windows has Unix Socket support but not on all versions.
+@ExtendWith(DropwizardExtensionsSupport.class)
+class UnixSocketConnectorFactoryTest {
+
+    public DropwizardAppExtension<Configuration> ext = new DropwizardAppExtension<>(HelloWorldApplication.class,
+        ResourceHelpers.resourceFilePath("yaml/usock-server.yml"));
+
+    private HttpClient httpClient;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        ClientConnector clientConnector = ClientConnector.forUnixDomain(Paths.get("/tmp", "dropwizard.sock"));
+        httpClient = new HttpClient(new HttpClientTransportOverHTTP(clientConnector));
+        httpClient.start();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        httpClient.stop();
+    }
+
+    @Test
+    void testClient() throws Exception {
+        ContentResponse contentResponse = httpClient.GET("http://localhost:0/app/hello");
+        assertThat(contentResponse).isNotNull().satisfies(response -> {
+            assertThat(response.getContentAsString()).isEqualTo("{\"hello\": \"World\"}");
+        });
+    }
+
+    @Test
+    void testManyCalls() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            testClient();
+        }
+    }
+
+    public static class HelloWorldApplication extends Application<Configuration> {
+
+        @Override
+        public void run(Configuration configuration, Environment environment) {
+            environment.jersey().register(new FakeResource());
+        }
+
+        @Path("/hello")
+        @Produces(MediaType.APPLICATION_JSON)
+        public static class FakeResource {
+
+            @GET
+            public String get() {
+                return "{\"hello\": \"World\"}";
+            }
+        }
+    }
+}
