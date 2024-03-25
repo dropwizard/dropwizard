07e3c29b07494205cec19f01df238f43e730f1bb
bontapalle <rh364338@dal.ca>
2024-03-25 11:28:04 -0300

    refactor: extract class TimeBoundHealthCheck to HealthCheckResultCreator

--- dropwizard-db/src/main/java/io/dropwizard/db/TimeBoundHealthCheck.java
+++ dropwizard-db/src/main/java/io/dropwizard/db/TimeBoundHealthCheck.java
@@ -7,25 +7,24 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
 public class TimeBoundHealthCheck {
-    private final ExecutorService executorService; 
+    private final ExecutorService executorService;
     private final Duration duration;
-     
+    private final HealthCheckResultCreator resultCreator = new HealthCheckResultCreator();
+
     public TimeBoundHealthCheck(ExecutorService executorService, Duration duration) {
         this.executorService = executorService;
         this.duration = duration;
     }
-    
-    public HealthCheck.Result check(Callable<HealthCheck.Result> c) {
+
+    public HealthCheck.Result check(Callable<HealthCheck.Result> callable) {
         try {
-            return executorService.submit(c).get(duration.getQuantity(), duration.getUnit());
+            return executorService.submit(callable).get(duration.getQuantity(), duration.getUnit());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            return resultCreator.unhealthy(duration.toString());
         } catch (Exception ignored) {
+            return resultCreator.unhealthy(duration.toString());
         }
-        return unhealthy();
-    }
-
-    private HealthCheck.Result unhealthy() {
-        return HealthCheck.Result.unhealthy("Unable to successfully check in %s", duration);
     }
 }
+
