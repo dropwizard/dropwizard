cec1dfad7cd8b460c3abbf7b653304d93c1cd66f
Jochen Schalanda <jochen@schalanda.name>
2024-10-21 09:17:37 +0200

    Revert "fix(deps): update dependency com.fasterxml.jackson:jackson-bom to v2.…"
    This reverts commit f7f5c96561fff3a4cfbd92caeeeef9ebb25f266f.

--- dropwizard-e2e/src/main/java/com/example/app1/CustomJsonProvider.java
+++ dropwizard-e2e/src/main/java/com/example/app1/CustomJsonProvider.java
@@ -2,9 +2,7 @@ package com.example.app1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jakarta.rs.json.JacksonXmlBindJsonProvider;
-import jakarta.annotation.Priority;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
@@ -21,7 +19,6 @@ import java.util.Objects;
 /** Custom JSON reader and writer that will write a leading HEADER to the JSON output */
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Priority(Priorities.ENTITY_CODER)
 public class CustomJsonProvider extends JacksonXmlBindJsonProvider {
     private static final String HEADER = "/** A Dropwizard specialty */\n";
     private static final byte[] HEADER_BYTES = HEADER.getBytes(StandardCharsets.UTF_8);
