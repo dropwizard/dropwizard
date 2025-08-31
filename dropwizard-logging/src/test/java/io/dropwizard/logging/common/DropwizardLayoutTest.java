526fbf703dfb0161ab3ec893ae74232ecd021122
Jochen Schalanda <jochen@schalanda.name>
2025-08-31 10:47:09 +0200

    Replace call to deprecated logback PatternLayout#getDefaultConverterMap (#10447)
    This was deprecated in logback 1.5.13 and replaced by the more typesafe
    getDefaultConverterSupplierMap
    
    Refs #10446
    
    Co-authored-by: Russell Howe <rhowe@siksai.co.uk>

--- dropwizard-logging/src/test/java/io/dropwizard/logging/common/DropwizardLayoutTest.java
+++ dropwizard-logging/src/test/java/io/dropwizard/logging/common/DropwizardLayoutTest.java
@@ -9,30 +9,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class DropwizardLayoutTest {
-    private final LoggerContext context = mock(LoggerContext.class);
-    private final TimeZone timeZone = TimeZone.getTimeZone("UTC");
-    private final DropwizardLayout layout = new DropwizardLayout(context, timeZone);
+    private final LoggerContext context = mock();
+    private final DropwizardLayout layout = new DropwizardLayout(context, TimeZone.getTimeZone("UTC"));
 
     @Test
-    void prefixesThrowables() throws Exception {
-        assertThat(layout.getDefaultConverterMap())
-                .containsEntry("dwEx", PrefixedThrowableProxyConverter.class.getName());
+    void prefixesThrowables() {
+        assertThat(layout.getDefaultConverterSupplierMap())
+                .hasEntrySatisfying("dwEx", supplier ->
+                    assertThat(supplier.get()).isInstanceOf(PrefixedThrowableProxyConverter.class));
     }
 
     @Test
-    void prefixesExtendedThrowables() throws Exception {
-        assertThat(layout.getDefaultConverterMap())
-                .containsEntry("dwXEx", PrefixedExtendedThrowableProxyConverter.class.getName());
+    void prefixesExtendedThrowables() {
+        assertThat(layout.getDefaultConverterSupplierMap())
+                .hasEntrySatisfying("dwXEx", supplier ->
+                    assertThat(supplier.get()).isInstanceOf(PrefixedExtendedThrowableProxyConverter.class));
     }
 
     @Test
-    void hasAContext() throws Exception {
+    void hasAContext() {
         assertThat(layout.getContext())
                 .isEqualTo(context);
     }
 
     @Test
-    void hasAPatternWithATimeZoneAndExtendedThrowables() throws Exception {
+    void hasAPatternWithATimeZoneAndExtendedThrowables() {
         assertThat(layout.getPattern())
                 .isEqualTo("%-5p [%d{ISO8601,UTC}] %c: %m%n%dwREx");
     }
