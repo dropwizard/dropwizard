229c6ecb96a2a0b925cc01297a44b6b1edb27f4f
Russell Howe <rhowe@siksai.co.uk>
2025-08-31 18:00:43 +0100

    Simplify dropwizard-db TimeBoundHealthCheckTest

--- dropwizard-db/src/test/java/io/dropwizard/db/TimeBoundHealthCheckTest.java
+++ dropwizard-db/src/test/java/io/dropwizard/db/TimeBoundHealthCheckTest.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static org.mockito.Mockito.mock;
@@ -17,17 +16,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class TimeBoundHealthCheckTest {
-
     @Test
-    @SuppressWarnings("unchecked")
     void testCheck() throws InterruptedException, ExecutionException, TimeoutException {
-        final ExecutorService executorService = mock(ExecutorService.class);
-        final Duration duration = mock(Duration.class);
-        when(duration.getQuantity()).thenReturn(5L);
-        when(duration.getUnit()).thenReturn(TimeUnit.SECONDS);
+        final ExecutorService executorService = mock();
+        final Duration duration = Duration.seconds(5);
 
-        final Callable<HealthCheck.Result> callable = mock(Callable.class);
-        final Future<HealthCheck.Result> future = mock(Future.class);
+        final Callable<HealthCheck.Result> callable = mock();
+        final Future<HealthCheck.Result> future = mock();
         when(executorService.submit(callable)).thenReturn(future);
 
         new TimeBoundHealthCheck(executorService, duration).check(callable);
