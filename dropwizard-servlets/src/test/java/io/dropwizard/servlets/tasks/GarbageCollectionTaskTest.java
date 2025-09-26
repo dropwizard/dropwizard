14c0281dcb6c70e75021af13ceddb03ce2b781c4
Jochen Schalanda <jochen@schalanda.name>
2025-09-26 22:32:33 +0200

    ci: build with Java 25 (#10603)
    * ci: build with Java 25
    
    * build: use ErrorProne 2.42.0 with JDK 17 and later
    
    * chore: rework GarbageCollectionTask to avoid mocking in tests

--- dropwizard-servlets/src/test/java/io/dropwizard/servlets/tasks/GarbageCollectionTaskTest.java
+++ dropwizard-servlets/src/test/java/io/dropwizard/servlets/tasks/GarbageCollectionTaskTest.java
@@ -7,36 +7,39 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
-@SuppressWarnings("CallToSystemGC")
 class GarbageCollectionTaskTest {
-    private final Runtime runtime = mock(Runtime.class);
     private final PrintWriter output = mock(PrintWriter.class);
-    private final GarbageCollectionTask task = new GarbageCollectionTask(runtime);
 
     @Test
     void runsOnceWithNoParameters() throws Exception {
+        final AtomicInteger count = new AtomicInteger();
+        final GarbageCollectionTask task = new GarbageCollectionTask(count::incrementAndGet);
         task.execute(Collections.emptyMap(), output);
 
-        verify(runtime, times(1)).gc();
+        assertThat(count).hasValue(1);
     }
 
     @Test
     void usesTheFirstRunsParameter() throws Exception {
+        final AtomicInteger count = new AtomicInteger();
+        final GarbageCollectionTask task = new GarbageCollectionTask(count::incrementAndGet);
         final Map<String, List<String>> parameters = Collections.singletonMap("runs", Arrays.asList("3", "2"));
         task.execute(parameters, output);
 
-        verify(runtime, times(3)).gc();
+        assertThat(count).hasValue(3);
     }
 
     @Test
     void defaultsToOneRunIfTheQueryParamDoesNotParse() throws Exception {
+        final AtomicInteger count = new AtomicInteger();
+        final GarbageCollectionTask task = new GarbageCollectionTask(count::incrementAndGet);
         task.execute(Collections.singletonMap("runs", Collections.singletonList("$")), output);
 
-        verify(runtime, times(1)).gc();
+        assertThat(count).hasValue(1);
     }
 }
